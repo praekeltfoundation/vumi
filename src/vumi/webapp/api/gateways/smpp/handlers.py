5bfb704bde81496e75dd9493cc112cd2ba60c7d5
dmaclay <dmaclay@gmail.com>
2011-01-17 20:28:20 +0200

    smpp webapp api

---
+++ src/vumi/webapp/api/gateways/smpp/handlers.py
@@ -0,0 +1,48 @@
+import re, yaml, logging
+from datetime import datetime, timedelta
+
+from piston.handler import BaseHandler
+from piston.utils import rc, throttle, require_mime, validate
+from piston.utils import Mimer, FormValidationError
+
+from vumi.webapp.api.models import SentSMS, ReceivedSMS, URLCallback
+from vumi.webapp.api import forms
+from vumi.webapp.api import signals
+from vumi.webapp.api.utils import specify_fields
+
+from alexandria.loader.base import YAMLLoader
+from alexandria.dsl.utils import dump_menu
+
+import pystache
+
+
+class SendSMPPHandler(BaseHandler):
+    print "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 1"
+    allowed_methods = ('GET', 'POST',)
+    exclude, fields = specify_fields(SentSMS, 
+        include=['transport_status_display'],
+        exclude=['user'])
+    
+    def _send_one(self, **kwargs):
+        kwargs.update({
+            'transport_name': 'smpp'
+        })
+        print "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 3"
+        form = forms.SentSMSForm(kwargs)
+        if not form.is_valid():
+            raise FormValidationError(form)
+        send_sms = form.save()
+        logging.debug('Scheduling an SMPP to: %s' % kwargs['to_msisdn'])
+        #signals.sms_scheduled.send(sender=SentSMS, instance=send_sms,
+                                    #pk=send_sms.pk)
+        return send_sms
+    
+    @throttle(6000, 60) # allow for 100 a second
+    def create(self, request):
+        print "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 2"
+        return [self._send_one(user=request.user.pk, 
+                                to_msisdn=msisdn,
+                                from_msisdn=request.POST.get('from_msisdn'),
+                                message=request.POST.get('message'))
+                    for msisdn in request.POST.getlist('to_msisdn')]
+    
