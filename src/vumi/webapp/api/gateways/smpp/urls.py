514e469ad46d63d1c757680365ab94cef45e9c68
dmaclay <dmaclay@gmail.com>
2011-01-17 20:09:57 +0200

    smpp webapp api

---
+++ src/vumi/webapp/api/gateways/smpp/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import *
+from piston.resource import Resource
+from piston.authentication import HttpBasicAuthentication
+from vumi.webapp.api.gateways.smpp import handlers
+
+ad = {'authentication': HttpBasicAuthentication(realm="Vumi")}
+
+smpp_send_resource = Resource(handler=handlers.SendSMPPHandler, **ad)
+
+urlpatterns = patterns(
+    '',
+    (r'^send\.json$', smpp_send_resource, {}, 'smpp-send'),
+)
