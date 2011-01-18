9721091bb073bf027652339446e721234eae7d1c
dmaclay <dmaclay@gmail.com>
2011-01-18 22:14:20 +0200

    just old msg in queue

--- src/vumi/webapp/api/signals.py
+++ src/vumi/webapp/api/signals.py
@@ -10,13 +10,12 @@ sms_received = Signal(providing_args=['instance', 'pk'])
 sms_receipt = Signal(providing_args=['instance', 'pk', 'receipt'])
 
 def sms_scheduled_handler(*args, **kwargs):
-    print "PPPPPPPPPPPPPPPPPPPPPP", kwargs.get('payload')
+    print kwargs.get('payload')
     sms_scheduled_worker(kwargs['instance'], kwargs.get('payload'))
 
 def sms_scheduled_worker(sent_sms, payload):
     """Responsibile for delivering of SMSs"""
-    print "PPPPPPPPPPPPPPPPPPPPPP", payload
-    SendSMSTask.delay(pk=sent_sms.pk, payload="woohoo")
+    SendSMSTask.delay(pk=sent_sms.pk, payload=payload)
 
 def sms_received_handler(*args, **kwargs):
     sms_received_worker(kwargs['instance'])
