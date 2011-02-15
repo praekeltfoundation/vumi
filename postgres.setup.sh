857a227141df19b4184f051e99c8afa6c39f8d5c
dmaclay <dmaclay@gmail.com>
2011-02-15 13:51:20 +0200

    script to setup postgress for production & staging

---
+++ postgres.setup.sh
@@ -0,0 +1,9 @@
+
+sudo -u postgres createuser --superuser --pwprompt vumi
+
+createdb -W -U vumi -h localhost -E UNICODE staging
+
+createdb -W -U vumi -h localhost -E UNICODE production
+
+pip -E ve install psycopg2
+
