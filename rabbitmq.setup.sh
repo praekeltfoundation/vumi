a1b3aa5488986cb07c6edd53c1a8fd6a6b1be92a
dmaclay <dmaclay@gmail.com>
2011-02-15 13:54:26 +0200

    script to setup rabbitmq for production & staging

---
+++ rabbitmq.setup.sh
@@ -0,0 +1,9 @@
+
+rabbitmqctl add_user vumi vumi
+
+rabbitmqctl add_vhost /staging
+rabbitmqctl set_permissions -p /staging vumi '.*' '.*' '.*'
+
+rabbitmqctl add_vhost /production
+rabbitmqctl set_permissions -p /production vumi '.*' '.*' '.*'
+
