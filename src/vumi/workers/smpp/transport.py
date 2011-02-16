57db1dec60b8101ca7151e4664fb8ef2d1c60d60
dmaclay <dmaclay@gmail.com>
2011-02-16 22:52:54 +0200

    ready for production ?

--- src/vumi/workers/smpp/transport.py
+++ src/vumi/workers/smpp/transport.py
@@ -13,6 +13,7 @@ import re
 #os.environ['DJANGO_SETTINGS_MODULE'] = 'vumi.webapp.settings'
 from vumi.webapp.api import models
 from vumi.webapp.api import forms
+from vumi.webapp.api import utils
 from vumi.utils import *
 
 import urllib
@@ -80,7 +81,6 @@ class SmppTransport(Worker):
         factory = EsmeTransceiverFactory(
                 int(self.config['smpp_increment']),
                 int(self.config['smpp_offset']))
-        print "#@#@#@#@#@#@#@#@#@#@#@#@@#@#", self.config
         factory.loadDefaults(self.config)
         factory.setLatestSequenceNumber(self.getLatestSequenceNumber())
         factory.setConnectCallback(self.esme_connected)
@@ -136,22 +136,38 @@ class SmppTransport(Worker):
 
     @inlineCallbacks
     def deliver_sm(self, *args, **kwargs):
-        profile = models.User.objects.get(username='vumi').get_profile()
-        urlcallback_set = profile.urlcallback_set.filter(name='sms_received')
-        for urlcallback in urlcallback_set:
-            try:
-                #url = "http://localhost:8080/"
-                url = urlcallback.url
-                params = {'json' : '{"route":"%s", "msisdn":"%s", "message":"%s"}' % (
-                    kwargs.get('destination_addr'),
-                    kwargs.get('source_addr'),
-                    kwargs.get('short_message')
-                    )}
-                data = urllib.urlencode(params)
-                req = urllib2.Request(url, data)
-                resp = urllib2.urlopen(req)
-            except Exception, e:
-                log.error(e)
+        message = kwargs.get('short_message')
+        head = message.split(' ')[0]
+        try:
+            user = models.User.objects.get(username=head)
+        except:
+            user= None
+        if user:
+            profile = user.get_profile()
+            urlcallback_set = profile.urlcallback_set.filter(name='sms_received')
+            for urlcallback in urlcallback_set:
+                try:
+                    #url = "http://localhost:8080/"
+                    url = urlcallback.url
+                    params = [("json",
+                        '{"route":"%s", "msisdn":"%s", "message":"%s"}' % (
+                        kwargs.get('destination_addr'),
+                        kwargs.get('source_addr'),
+                        kwargs.get('short_message'))
+                        )]
+                    utils.callback(url, params)
+                    #params = {'json' : '{"route":"%s", "msisdn":"%s", "message":"%s"}' % (
+                        #kwargs.get('destination_addr'),
+                        #kwargs.get('source_addr'),
+                        #kwargs.get('short_message')
+                        #)}
+                    #data = urllib.urlencode(params)
+                    #req = urllib2.Request(url, data)
+                    #resp = urllib2.urlopen(req)
+                except Exception, e:
+                    log.error(e)
+        else:
+            log.msg("Couldn't find user for message: %s" % message)
         yield log.msg("DELIVER SM %s" % (json.dumps(kwargs)))
 
     @inlineCallbacks
@@ -199,10 +215,10 @@ class SmppTransport(Worker):
                 destination_addr = str(to_msisdn),
                 source_addr = route,
                 )
-        self.deliver_sm(
-                short_message=str(message),
-                destination_addr=str(to_msisdn),
-                source_addr=route)
+        #self.deliver_sm(
+                #short_message=str(message),
+                #destination_addr=str(to_msisdn),
+                #source_addr=route)
         return sequence_number
 
 
