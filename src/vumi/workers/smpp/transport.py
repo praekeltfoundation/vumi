3804aca27af319b6315814cf646a5b860ab82d12
dmaclay <dmaclay@gmail.com>
2011-02-25 03:54:56 +0200

    find delivered sms

--- src/vumi/workers/smpp/transport.py
+++ src/vumi/workers/smpp/transport.py
@@ -167,6 +167,11 @@ class SmppTransport(Worker):
 
     @inlineCallbacks
     def delivery_report(self, *args, **kwargs):
+        if len(kwargs['id']):
+            resp = models.SMPPResp.objects \
+                    .filter(message_id=kwargs['id'])
+                    .order_by('-created_at')[:1]
+            print "ORIG_ID", resp.sent_sms_id
         yield log.msg("DELIVERY REPORT %s" % (json.dumps(kwargs)))
 
 
