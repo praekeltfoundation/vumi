45e8ad3a34449532750c109955f7f574573f86b8
dmaclay <dmaclay@gmail.com>
2011-03-11 12:49:34 +0200

    unstable schema

--- postgres.setup.sh
+++ postgres.setup.sh
@@ -5,6 +5,8 @@ createdb -W -U vumi -h localhost -E UNICODE staging
 
 createdb -W -U vumi -h localhost -E UNICODE production
 
+createdb -W -U vumi -h localhost -E UNICODE unstable
+
 pip -E ve install psycopg2
 
 
