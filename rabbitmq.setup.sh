1680472c5e2cf79b6e4771788ea4a40d24af7a4c
dmaclay <dmaclay@gmail.com>
2011-03-11 12:50:39 +0200

    unstable vhost

--- rabbitmq.setup.sh
+++ rabbitmq.setup.sh
@@ -7,4 +7,6 @@ rabbitmqctl set_permissions -p /staging vumi '.*' '.*' '.*'
 rabbitmqctl add_vhost /production
 rabbitmqctl set_permissions -p /production vumi '.*' '.*' '.*'
 
+rabbitmqctl add_vhost /unstable
+rabbitmqctl set_permissions -p /unstable vumi '.*' '.*' '.*'
 
