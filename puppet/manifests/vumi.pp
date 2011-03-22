ed634bebe7910514231021334b8f890131bc4447
Simon de Haan <simon@praekeltfoundation.org>
2011-03-22 20:58:00 +0200

    removing python-clickatell from requirements improved puppet execution order puppet now more verbose

--- puppet/manifests/vumi.pp
+++ puppet/manifests/vumi.pp
@@ -22,10 +22,11 @@ define apt::package($ensure='latest') {
 
 # Install these packages
 class vumi::packages {
-    apt::package { "build-essential": }
-    apt::package { "python": }
-    apt::package { "python-dev": }
-    apt::package { "python-setuptools": }
+    apt::package { "build-essential": ensure => "11.4build1" }
+    apt::package { "python": ensure => "2.6.5-0ubuntu1" }
+    apt::package { "python-dev": ensure => "2.6.5-0ubuntu1" }
+    apt::package { "python-setuptools": ensure => "0.6.10-4ubuntu1" }
+    apt::package { "python-pip": ensure => "0.3.1-1ubuntu2" }
     apt::package { "python-virtualenv": ensure => "1.4.5-1ubuntu1" }
     apt::package { "postgresql-8.4": ensure => "8.4.3-1" }
     apt::package { "libpq-dev": ensure => "8.4.7-0ubuntu0.10.04" }
@@ -40,35 +41,23 @@ class vumi::accounts {
     
     rabbitmq::vhost { "/development":
         ensure => present,
-        require => Class['vumi::packages']
     }
     
     rabbitmq::user { "vumi":
         ensure => present,
         password => "vumi",
         vhost => '/development',
-        require => Class['vumi::packages']
     }
     
     postgres::role { "vumi":
         ensure => present,
         password => "vumi",
-        require => Class["vumi::packages"],
     }
     postgres::database { "vumi":
         ensure => present,
         owner => vumi,
         template => "template0",
-        require => Class["vumi::packages"],
     }
-}
-
-# We use pip for installing python software.
-exec { "easy_install pip":
-    command => "easy_install pip",
-    refreshonly => true, 
-    user => "root",
-    require => Class["vumi::packages"], 
 }
 
 file {
@@ -80,45 +69,24 @@ file {
 exec { "Clone git repository":
     command => "git clone http://github.com/praekelt/vumi.git",
     cwd => "/var/praekelt",
-    require => [
-        Class['vumi::packages'],
-        File['/var/praekelt']
-    ],
-    notify => "/var/praekelt/vumi repository ready",
     unless => "test -d /var/praekelt/vumi/.git"
 }
 
 exec { "Checkout development branch":
     command => "git checkout -b develop origin/develop",
     cwd => "/var/praekelt/vumi",
-    require => [
-        Class['vumi::packages'],
-        File['/var/praekelt'],
-    ],
-    subscribe => "/var/praekelt/vumi repository ready",
-    notify => "vumi development branch ready",
     unless => "git branch | grep '* develop'"
 }
 
 exec { "Update git repository":
     command => "git pull",
     cwd => "/var/praekelt/vumi",
-    require => [
-        Package['git-core'],
-        File['/var/praekelt']
-    ],
-    subscribe => "/var/praekelt/vumi repository ready",
-    notify => "vumi development branch ready",
     onlyif => "test -d /var/praekelt/vumi/.git"
 }
 
 exec { "Create virtualenv":
     command => "virtualenv --no-site-packages ve",
     cwd => "/var/praekelt/vumi",
-    require => [
-        Class["vumi::packages"]
-    ],
-    subscribe => "vumi development branch ready",
     unless => "test -d ve"
 }
 
@@ -127,10 +95,6 @@ exec { "Install requirements":
                     pip install -r config/requirements.pip && \
                 deactivate",
     cwd => "/var/praekelt/vumi",
-    require => [
-        Class["vumi::packages"]
-    ],
-    subscribe => "vumi development branch ready",
     timeout => "-1", # disable timeout
     onlyif => "test -d ve"
 }
@@ -140,20 +104,12 @@ exec { "Install Vumi package":
                     python setup.py develop && \
                 deactivate",
     cwd => "/var/praekelt/vumi",
-    require => [
-        Class["vumi::packages"]
-    ],
-    subscribe => "vumi development branch ready",
     onlyif => "test -d ve"
 }
 
 exec { "Install Selenium SMPPSim":
     command => "sh install_smpp_simulator.sh",
     cwd => "/var/praekelt/vumi/utils",
-    require => [
-        Class["vumi::packages"]
-    ],
-    subscribe => "vumi development branch ready",
     timeout => "-1",
     unless => "test -d /var/praekelt/vumi/utils/smppsim"
 }
@@ -165,10 +121,6 @@ class vumi::database {
                     deactivate
                     ",
         cwd => "/var/praekelt/vumi",
-        require => [
-            Class["vumi::packages"]
-        ],
-        subscribe => "vumi development branch ready",
     }
     exec { "Migrate":
         command => ". ve/bin/activate && \
@@ -176,10 +128,6 @@ class vumi::database {
                     deactivate
                     ",
         cwd => "/var/praekelt/vumi",
-        require => [
-            Class["vumi::packages"]
-        ],
-        subscribe => "vumi development branch ready",
     }
 }
 
@@ -188,9 +136,6 @@ exec { "Create Vumi Django user":
         echo \"from django.contrib.auth.models import *; u, created = User.objects.get_or_create(username='vumi', is_superuser=True, is_staff=True); u.set_password('vumi'); u.save()\" | ./manage.py shell && \
     deactivate",
     cwd => "/var/praekelt/vumi",
-    require => [
-        Class["vumi::database"],
-    ],
 }
 
 exec { "Restart Vumi":
@@ -198,9 +143,6 @@ exec { "Restart Vumi":
                     supervisorctl reload && \
                 deactivate",
     cwd => "/var/praekelt/vumi",
-    require => [
-        Class["vumi::database"],
-    ],
     onlyif => "ps -p `cat tmp/pids/supervisord.pid`"
 }
 exec { "Start Vumi":
@@ -208,24 +150,26 @@ exec { "Start Vumi":
                     supervisord && \
                 deactivate",
     cwd => "/var/praekelt/vumi",
-    require => [
-        Class["vumi::database"],
-    ],
     unless => "ps -p `cat tmp/pids/supervisord.pid`"
 }
 
-# class vumi {
-#     include apt::update,
-#                 vumi::packages, 
-#                 vumi::accounts,
-#                 vumi::python_tools, 
-#                 vumi::layout, 
-#                 vumi::clone_repo, 
-#                 vumi::virtualenv, 
-#                 vumi::syncdb,
-#                 vumi::createadmin,
-#                 vumi::install_smpp_simulator, 
-#                 vumi::startup
-# }
-# 
-# include vumi
+class vumi {
+    include apt::update,
+                vumi::accounts,
+                vumi::packages, 
+                vumi::database
+}
+
+Exec["Resynchronize apt package index"] ~> Class["vumi::packages"] ~> Class["vumi::accounts"]
+
+File["/var/praekelt"] ~> Exec["Clone git repository"] ~> Exec["Checkout development branch"] ~> Exec["Create virtualenv"] ~> Exec["Install requirements"] ~> Class["vumi::database"]
+File["/var/praekelt"] ~> Exec["Update git repository"] ~> Exec["Install requirements"] ~> Class["vumi::database"]
+
+Exec["Install requirements"] ~> Exec["Install Vumi package"]
+Exec["Install requirements"] ~> Exec["Install Selenium SMPPSim"]
+
+Class["vumi::database"] ~> Exec["Create Vumi Django user"]
+Exec["Create Vumi Django user"] ~> Exec["Start Vumi"]
+Exec["Create Vumi Django user"] ~> Exec["Restart Vumi"]
+
+include vumi
