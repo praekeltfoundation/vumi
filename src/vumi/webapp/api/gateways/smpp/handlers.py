000da672ae6a8a65398e3a1a4f267544b859ee82
dmaclay <dmaclay@gmail.com>
2011-03-24 21:48:03 +0200

    import SentSMSBatch

--- src/vumi/webapp/api/gateways/smpp/handlers.py
+++ src/vumi/webapp/api/gateways/smpp/handlers.py
@@ -5,7 +5,7 @@ from piston.handler import BaseHandler
 from piston.utils import rc, throttle, require_mime, validate
 from piston.utils import Mimer, FormValidationError
 
-from vumi.webapp.api.models import SentSMS, ReceivedSMS, URLCallback
+from vumi.webapp.api.models import SentSMS, SentSMSBatch, ReceivedSMS, URLCallback
 from vumi.webapp.api import forms
 from vumi.webapp.api import signals
 from vumi.webapp.api.utils import specify_fields
