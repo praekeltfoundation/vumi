115d4658a0296ad3ac6fe7aea6bc444929835360
Simon de Haan <simon@praekeltfoundation.org>
2011-03-29 11:32:56 +0200

    typo spinx -> sphinx, by default use the develop database

--- src/vumi/webapp/settings.py
+++ src/vumi/webapp/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE':   'django.db.backends.postgresql_psycopg2',  # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME':     'develop',                 # Or path to database file if using sqlite3.
+        'NAME':     'vumi',                 # Or path to database file if using sqlite3.
         'USER':     'vumi',                 # Not used with sqlite3.
         'PASSWORD': 'vumi',                 # Not used with sqlite3.
         'HOST':     'localhost',            # Set to empty string for localhost. Not used with sqlite3.
