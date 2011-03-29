874b2650e019193bf40aa6129b689df40cfbe33a
Simon de Haan <simon@praekeltfoundation.org>
2011-03-29 15:50:57 +0200

    using supervisord to start smppsim, not needing the sed & chmod stuff

--- utils/install_smpp_simulator.sh
+++ utils/install_smpp_simulator.sh
@@ -1,7 +1,4 @@
 mkdir -p smppsim && \
 cd smppsim && \
 wget http://www.seleniumsoftware.com/downloads/SMPPSim.tar.gz && \
-tar zxf SMPPSim.tar.gz && \
-cd SMPPSim && \
-sed -i.original -e s/HTTP_PORT=88/HTTP_PORT=8080/ conf/smppsim.props && \
-chmod +x startsmppsim.sh
+tar zxf SMPPSim.tar.gz
