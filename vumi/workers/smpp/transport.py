135f3a469c62e81003dd888676dc7c5ac216eeb5
dmaclay <dmaclay@gmail.com>
2011-03-31 16:28:31 +0200

    it can't be this simple

--- vumi/workers/smpp/transport.py
+++ vumi/workers/smpp/transport.py
@@ -198,7 +198,7 @@ class SmppTransport(Worker):
                 self.config.get('OPERATOR_PREFIX',{}),
                 self.config.get('OPERATOR_NUMBER',{}))
         sequence_number = self.esme_client.submit_sm(
-                short_message = str(message),
+                short_message = message,
                 destination_addr = str(to_msisdn),
                 source_addr = route,
                 )
