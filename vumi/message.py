f5d5cb47c1b278eb4cf057d5aecc596d1e7781c6
Simon de Haan <simon@praekeltfoundation.org>
2011-04-04 08:05:18 +0200

    adding unified message object for Vumi

---
+++ vumi/message.py
@@ -0,0 +1,40 @@
+import json, datetime
+
+class Message(object):
+    """
+    Start of a somewhat unified message object to be
+    used internally in Vumi and while being in transit
+    over AMQP
+
+    scary transport format -> Vumi Tansport -> Unified Message -> Vumi Worker
+    
+    """
+
+    def __init__(self, **kwargs):
+        self.payload = kwargs
+
+    def to_json(self):
+        return json.dumps(self.payload, cls=JSONMessageEncoder)
+    
+    @classmethod
+    def from_json(klass, json_string):
+        dictionary = json.loads(json_string, cls=JSONMessageDecoder)
+        return klass(**dictionary)
+
+class JSONMessageDecoder(json.JSONDecoder):
+    """A JSON decoder that is ablo to read datetime values"""
+    def decode(self, s):
+        try:
+            d = datetime.datetime.strptime(d, '%Y-%m-%dT%H:%M:%S.%f')
+        except ValueError, e:
+            pass
+    
+
+class JSONMessageEncoder(json.JSONEncoder):
+    """A JSON encoder that is able to serialize datetime"""
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+        return super(JSONEncoder, self).default(obj)
+    
+
