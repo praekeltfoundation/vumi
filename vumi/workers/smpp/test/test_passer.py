78af08c947898c84888e8cc85d348612129624c8
dmaclay <dmaclay@gmail.com>
2011-04-11 17:12:31 +0200

    ..

--- vumi/workers/smpp/test/test_passer.py
+++ vumi/workers/smpp/test/test_passer.py
@@ -4,5 +4,5 @@ from twisted.trial import unittest
 class PassingTest(unittest.TestCase):
     def testPasser(self):
         """A test that should always pass"""
-        self.failUnlessEqual(0, 0)
+        self.assertEqual(0, 0)
 
