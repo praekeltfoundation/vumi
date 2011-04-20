99a6971e900a983a1d2dbff1537080f0094eaa11
Simon de Haan <simon@praekeltfoundation.org>
2011-04-20 11:17:36 +0200

    removing need for publisher & consumer classes

--- vumi/workers/xmpp/worker.py
+++ vumi/workers/xmpp/worker.py
@@ -2,15 +2,8 @@ from twisted.python import log
 from twisted.internet.defer import inlineCallbacks, returnValue
 from twisted.internet import reactor
 
-from vumi.service import Worker, Publisher
+from vumi.service import Worker
 from vumi.message import Message
-
-class XMPPPublisher(Publisher):
-    exchange_name = "vumi"
-    exchange_type = "direct"
-    durable = True
-    routing_key = "xmpp.outbound.gtalk"
-    
 
 class XMPPWorker(Worker):
     
@@ -22,18 +15,16 @@ class XMPPWorker(Worker):
     def startWorker(self):
         log.msg("Starting the XMPPWorker config: %s" % self.config)
         # create the publisher
-        self.publisher = yield self.start_publisher(XMPPPublisher)
+        self.publisher = yield self.publish_to('xmpp.outbound.gtalk.%s' %
+                                                self.config['username'])
         # when it's done, create the consumer and pass it the publisher
-        # self.consumer = yield self.start_consumer(XMPPConsumer, self.publisher)
         self.consume("xmpp.inbound.gtalk.%s" % self.config['username'], 
                         self.consume_message)
     
     def consume_message(self, message):
         recipient = message.payload['sender']
         message = "You said: %s " % message.payload['message']
-        self.publisher.publish_message(Message(recipient=recipient, message=message), 
-            routing_key = "%s.%s" % (self.publisher.routing_key, self.config['username'])
-        )
+        self.publisher.publish_message(Message(recipient=recipient, message=message))
     
     def stopWorker(self):
         log.msg("Stopping the XMPPWorker")
