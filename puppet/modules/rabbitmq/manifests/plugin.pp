a7d91ac2b522707ba28727a3cf2d29a304b0ddf0
dmaclay <dmaclay@gmail.com>
2011-04-28 10:48:13 +0200

    ..

--- puppet/modules/rabbitmq/manifests/plugin.pp
+++ puppet/modules/rabbitmq/manifests/plugin.pp
@@ -1,14 +1,7 @@
-define rabbitmq::vhost($ensure) {
-    case $ensure {
-        present: {
-            exec { "Install $name-$version rabbitmq plugin":
-                command => "/usr/sbin/rabbitmqctl add_vhost $name",
-                user => "root",
-                unless => "/usr/sbin/rabbitmqctl list_vhosts | grep $name"
-            }
-        }
-        default: {
-            fail "Invalid 'ensure' value '$ensure' for rabbitmq::plugin"
-        }
+define rabbitmq::plugin($ensure) {
+    exec { "Install $name-$version rabbitmq plugin":
+        command => "/usr/sbin/rabbitmqctl add_vhost $name",
+        user => "root",
+        unless => "/usr/sbin/rabbitmqctl list_vhosts | grep $name"
     }
 }
