3b846bdbbf0a2d11a3b156ba70321ebe234ac6e4
dmaclay <dmaclay@gmail.com>
2011-04-28 13:11:07 +0200

    curl -C - -O <URL> is equivalent to wget -cN <URL>

--- puppet/modules/rabbitmq/manifests/plugin.pp
+++ puppet/modules/rabbitmq/manifests/plugin.pp
@@ -1,7 +1,7 @@
 define rabbitmq::plugin($ensure) {
     exec { "Install $name-$ensure rabbitmq plugin":
         cwd => "/usr/lib/rabbitmq/lib/rabbitmq_server-$ensure/plugins",
-        command => "wget -cN http://www.rabbitmq.com/releases/plugins/v$ensure/$name-$ensure.ez",
+        command => "curl -C - -O http://www.rabbitmq.com/releases/plugins/v$ensure/$name-$ensure.ez",
         user => "root"
     }
 }
