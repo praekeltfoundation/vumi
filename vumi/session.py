dc63abb92c553de3427a67d8e3d1c405d89c3be5
dmaclay <dmaclay@gmail.com>
2011-05-05 15:38:49 +0200

    decision tree

--- vumi/session.py
+++ vumi/session.py
@@ -2,7 +2,46 @@
 # with hist & expiry & to_str & to_json etc
 
 class VumiSession():
+    key = None
+    decision_tree = None
 
     def __init__(self, **kwargs):
         pass
+
+    def set_decision_tree(self, decision_tree):
+        self.decision_tree = decision_tree
+
+    def get_decision_tree(self):
+        return self.decision_tree
+
+
+
+
+class DecisionTree():
+    pass
+
+
+class TemplatedDecisionTree(DecisionTree):
+
+    def load_yaml(self, yaml):
+        pass
+
+
+class PopulatedDesicionTree(TemplatedDecisionTree):
+
+    def load_json(self, json):
+        pass
+
+
+class TraversedDecisionTree(PopulatedDesicionTree):
+    completed = False
+
+
+#a = VumiSession()
+#b = VumiSession()
+#a.set_decision_tree(TraversedDecisionTree())
+#b.set_decision_tree(PopulatedDesicionTree())
+#print a.get_decision_tree()
+#print b.get_decision_tree()
+
 
