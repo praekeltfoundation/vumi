e5de3b44da10a496e51cb1ff1b4792ab54f29bbb
Simon de Haan <simon@praekeltfoundation.org>
2011-05-16 19:27:30 +0200

    XMPPs not happy

--- vumi/workers/irc/transport.py
+++ vumi/workers/irc/transport.py
@@ -31,6 +31,8 @@ class MessageLogger:
         
         self.publisher.publish_message(Message(
             recipient='ircarchive@appspot.com', message=json.dumps(payload)))
+        self.publisher.publish_message(Message(
+            recipient='simon@praekeltfoundation.org', message=json.dumps(payload)))
 
 class LogBot(irc.IRCClient):
     """A logging IRC bot."""
