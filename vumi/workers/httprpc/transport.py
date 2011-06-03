b2381a9352d17cfae6f3616ecfe95122df596dcb
dmaclay <dmaclay@gmail.com>
2011-06-03 22:31:56 +0200

    cleanup

--- vumi/workers/httprpc/transport.py
+++ vumi/workers/httprpc/transport.py
@@ -1,42 +1,32 @@
-# -*- coding: utf-8 -*-
-from twisted.python import log
-from twisted.web import xmlrpc, http
-from twisted.web.resource import Resource
-from twisted.internet import defer
-from twisted.internet.defer import inlineCallbacks, returnValue
-from twisted.internet import reactor
-from datetime import datetime, timedelta
-from vumi.webapp.api.models import SentSMS, ReceivedSMS, Keyword
-from vumi.webapp.api.gateways.opera import utils
-from vumi.utils import safe_routing_key
-from vumi.webapp.api import forms
-from vumi.message import Message, JSONMessageEncoder
-from vumi.service import Worker, Consumer, Publisher
-import cgi, json, iso8601, base64, xmlrpclib
-from time import sleep
-from txamqp.queue import TimeoutDeferredQueue, Closed
-from twisted.web.server import NOT_DONE_YET
-from twisted.internet.task import deferLater
 import uuid
+
+from twisted.internet.defer import inlineCallbacks
+from twisted.python import log
+from twisted.web.resource import Resource
+from twisted.web.server import NOT_DONE_YET
 from vumi.message import Message
+from vumi.service import Worker
 
 
 class HttpRpcResource(Resource):
+    isLeaf = True
 
     def __init__(self, transport):
         self.transport = transport
-        self.publisher = transport.publisher
         Resource.__init__(self)
 
+    def render_GET(self, request):
+        log.msg("HttpRpcResource.render_GET args:", repr(request.args))
+        return "OK\n"
 
     def render_POST(self, request):
         content = request.content.read()
         uu = str(uuid.uuid4())
         self.transport.requests[uu] = request
-        mess = Message(message=content,
-                uuid=uu,
-                transport_key=self.transport.consume_key)
-        self.transport.publisher.publish_message(mess, routing_key=self.transport.consume_key)
+        mess = Message(message = content,
+                uuid = uu,
+                return_path = [self.transport.consume_key])
+        self.transport.publisher.publish_message(mess)
         return NOT_DONE_YET
 
 
@@ -57,11 +47,8 @@ class HttpRpcTransport(Worker):
 
         # start receipt web resource
         self.receipt_resource = yield self.start_web_resources(
-            [
-                (HttpRpcResource(self), self.config['web_path']),
-            ],
-            self.config['web_port']
-        )
+            [(HttpRpcResource(self), self.config['web_path']),],
+            self.config['web_port'])
 
     def consume_message(self, message):
         log.msg("HttpRpcTransport consuming on %s: %s" % (
@@ -72,10 +59,9 @@ class HttpRpcTransport(Worker):
                     message.payload['uuid'],
                     message.payload['message'])
 
-
-    def finishRequest(self, uuid, data):
-        log.msg("finishRequest data:", repr(data))
-        #log.msg(repr(self.requests))
+    def finishRequest(self, uuid, data=''):
+        log.msg("HttpRpcTransport.finishRequest with data:", repr(data))
+        log.msg(repr(self.requests))
         request = self.requests.get(uuid)
         if request:
             request.write(str(data))
