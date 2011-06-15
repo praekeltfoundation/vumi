8af21e405945d7d3a176691a68df62e500158708
dmaclay <dmaclay@gmail.com>
2011-06-15 02:05:59 +0200

    ..

--- vumi/tests/test_multipart.py
+++ vumi/tests/test_multipart.py
@@ -2,29 +2,56 @@
 from smpp.pdu_builder import *
 from twisted.trial.unittest import TestCase
 
-p = DeliverSM(1, short_message='\x00\x03\x11\x02\x01iiiiii')
+tlv = DeliverSM(1, short_message='the first message part')
+sar = DeliverSM(1, short_message='\x00\x03\xff\x02\x01the first message part')
+csm = DeliverSM(1, short_message='\x05\x00\x03\xff\x02\x01the first message part')
+csm16 = DeliverSM(1, short_message='\x06\x00\x04\xff\xff\x02\x01the first message part')
 
-print unpack_pdu(p.get_bin())
 
 
 def detect_multipart(pdu):
-
     short_message = pdu.get_obj()['body']['mandatory_parameters']['short_message']
-    print repr(short_message.split(''))
+
+    print repr(pdu.get_obj())
+
     if (short_message[0:1] == '\x00'
-        and short_message[1:2] == '\x03'):
-        print "DDDDD"
-
-    _2 = short_message[1:2]
-    _3 = short_message[2:3]
-    _4 = short_message[3:4]
-    _5 = short_message[4:5]
-
-def total_parts(pdu):
-    pass
-
-def part_number(pdu):
-    pass
+    and short_message[1:2] == '\x03'
+    and len(short_message) >= 5):
+        mdict = {'multipart_type':'SAR'}
+        mdict['reference_number'] = int(binascii.b2a_hex(short_message[2:3]), 16)
+        mdict['total_number'] = int(binascii.b2a_hex(short_message[3:4]), 16)
+        mdict['part_number'] = int(binascii.b2a_hex(short_message[4:5]), 16)
+        mdict['part_message'] = short_message[5:]
+        return mdict
 
 
-detect_multipart(p)
+    if (short_message[0:1] == '\x05'
+    and short_message[1:2] == '\x00'
+    and short_message[2:3] == '\x03'
+    and len(short_message) >= 6):
+        mdict = {'multipart_type':'CSM'}
+        mdict['reference_number'] = int(binascii.b2a_hex(short_message[3:4]), 16)
+        mdict['total_number'] = int(binascii.b2a_hex(short_message[4:5]), 16)
+        mdict['part_number'] = int(binascii.b2a_hex(short_message[5:6]), 16)
+        mdict['part_message'] = short_message[6:]
+        return mdict
+
+    if (short_message[0:1] == '\x06'
+    and short_message[1:2] == '\x00'
+    and short_message[2:3] == '\x04'
+    and len(short_message) >= 7):
+        mdict = {'multipart_type':'CSM16'}
+        mdict['reference_number'] = int(binascii.b2a_hex(short_message[3:5]), 16)
+        mdict['total_number'] = int(binascii.b2a_hex(short_message[5:6]), 16)
+        mdict['part_number'] = int(binascii.b2a_hex(short_message[6:7]), 16)
+        mdict['part_message'] = short_message[7:]
+        return mdict
+
+    return None
+
+
+
+
+print '\n', detect_multipart(sar)
+print '\n', detect_multipart(csm)
+print '\n', detect_multipart(csm16)
