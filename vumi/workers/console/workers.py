a67c3bb0100a053b5265da2a0f1e1484369204cc
Jeremy Thurgood <firxen@gmail.com>
2011-07-06 11:34:10 +0200

    Added a telnet-based console transport and some toy workers.

---
+++ vumi/workers/console/workers.py
@@ -0,0 +1,52 @@
+from twisted.internet.defer import inlineCallbacks
+from twisted.python import log
+
+from vumi.service import Worker
+from vumi.message import Message
+
+
+class TelnetConsoleWorker(Worker):
+    name = None
+
+    @inlineCallbacks
+    def startWorker(self):
+        log.msg("Starting console worker.")
+        self.publisher = yield self.publish_to('console.outbound')
+        self.consume('console.inbound', self.consume_message, 'console.inbound.%s' % (self.name,))
+        log.msg("Started service")
+
+    def consume_message(self, message):
+        log.msg("Consumed message %s" % message)
+        data = self.process_message(message.payload['message'])
+        if data:
+            self.publisher.publish_message(Message(recipient=self.name, message=data))
+
+    def process_message(self, data):
+        return None
+
+    def stopWorker(self):
+        log.msg("Stopping console worker.")
+
+
+class TelnetConsoleEcho(TelnetConsoleWorker):
+    name = 'echo'
+
+    def process_message(self, data):
+        return data
+
+
+class TelnetConsoleReverse(TelnetConsoleWorker):
+    name = 'reverse'
+
+    def process_message(self, data):
+        return data[::-1]
+
+
+class TelnetConsoleCounts(TelnetConsoleWorker):
+    name = 'counts'
+
+    def process_message(self, data):
+        words = len(data.split())
+        chars = len(data)
+        return "%s word%s, %s char%s" % (words, "s" * (words != 1), len(data), "s" * (chars != 1))
+
