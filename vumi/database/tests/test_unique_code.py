a10644c05fe5212edfd6062e1a7749fdeec18509
Jeremy Thurgood <jeremy@praekelt.com>
2011-07-14 10:37:00 +0200

    The rest of the changes for the previous commit.

--- vumi/database/tests/test_unique_code.py
+++ vumi/database/tests/test_unique_code.py
@@ -1,33 +1,33 @@
 from twisted.internet.defer import inlineCallbacks
 
 from vumi.database.tests.test_base import UglyModelTestCase
-from vumi.database.unique_code import UniqueCode2
+from vumi.database.unique_code import UniqueCode
 
 class UniqueCodeTestCase(UglyModelTestCase):
 
     def setUp(self):
         @inlineCallbacks
         def _cb(_):
-            yield UniqueCode2.drop_table(self.db)
-            yield UniqueCode2.create_table(self.db)
+            yield UniqueCode.drop_table(self.db)
+            yield UniqueCode.create_table(self.db)
         return self.setup_db(_cb)
 
     @inlineCallbacks
     def test_uc(self):
-        yield self.ri(UniqueCode2.load_codes, ['abc', '123', 'useme'])
-        yield self.ri(UniqueCode2.modify_code, '123', used=True)
+        yield self.ri(UniqueCode.load_codes, ['abc', '123', 'useme'])
+        yield self.ri(UniqueCode.modify_code, '123', used=True)
 
         def _check_status(txn):
-            self.assertEquals('unused', UniqueCode2.get_code_status(txn, 'abc'))
-            self.assertEquals('used', UniqueCode2.get_code_status(txn, '123'))
-            self.assertEquals('invalid', UniqueCode2.get_code_status(txn, 'xyz'))
+            self.assertEquals('unused', UniqueCode.get_code_status(txn, 'abc'))
+            self.assertEquals('used', UniqueCode.get_code_status(txn, '123'))
+            self.assertEquals('invalid', UniqueCode.get_code_status(txn, 'xyz'))
 
         yield self.ri(_check_status)
 
         def _bad_burn(txn):
-            self.assertEquals('unused', UniqueCode2.get_code_status(txn, 'useme'))
-            self.assertEquals((True, 'valid'), UniqueCode2.burn_code(txn, 'useme'))
-            self.assertEquals('used', UniqueCode2.get_code_status(txn, 'useme'))
+            self.assertEquals('unused', UniqueCode.get_code_status(txn, 'useme'))
+            self.assertEquals((True, 'valid'), UniqueCode.burn_code(txn, 'useme'))
+            self.assertEquals('used', UniqueCode.get_code_status(txn, 'useme'))
             raise ValueError("foo")
 
         try:
@@ -36,15 +36,15 @@ class UniqueCodeTestCase(UglyModelTestCase):
             pass
 
         def _check_burn(txn):
-            self.assertEquals('unused', UniqueCode2.get_code_status(txn, 'useme'))
-            self.assertEquals((True, 'valid'), UniqueCode2.burn_code(txn, 'useme'))
-            self.assertEquals('used', UniqueCode2.get_code_status(txn, 'useme'))
-            self.assertEquals((False, 'used'), UniqueCode2.burn_code(txn, 'useme'))
+            self.assertEquals('unused', UniqueCode.get_code_status(txn, 'useme'))
+            self.assertEquals((True, 'valid'), UniqueCode.burn_code(txn, 'useme'))
+            self.assertEquals('used', UniqueCode.get_code_status(txn, 'useme'))
+            self.assertEquals((False, 'used'), UniqueCode.burn_code(txn, 'useme'))
 
         yield self.ri(_check_burn)
 
         def _recheck_burn(txn):
-            self.assertEquals('used', UniqueCode2.get_code_status(txn, 'useme'))
+            self.assertEquals('used', UniqueCode.get_code_status(txn, 'useme'))
 
         yield self.ri(_recheck_burn)
 
--- vumi/database/unique_code.py
+++ vumi/database/unique_code.py
@@ -1,71 +1,9 @@
 # -*- test-case-name: vumi.database.tests.test_unique_code -*-
 
-from vumi.database.base import UglyModel, UglyNoDbModel
+from vumi.database.base import UglyModel
+
 
 class UniqueCode(UglyModel):
-    db_name = 'test'
-    table_name = 'unique_codes'
-    fields = (
-        ('code', 'varchar(10) UNIQUE NOT NULL'),
-        ('used', 'boolean DEFAULT false'),
-        ('created', 'timestamp DEFAULT current_timestamp'),
-        ('modified', 'timestamp DEFAULT current_timestamp'),
-        )
-
-    @classmethod
-    def get_code(cls, code):
-        def _cb(ucs):
-            if ucs:
-                return ucs[0]
-            return None
-        return cls.run_select("WHERE code=%(code)s", {'code': code}).addCallback(_cb)
-
-    @classmethod
-    def get_code_status(cls, code):
-        def _cb(uc):
-            if not uc:
-                return 'invalid'
-            if uc.used:
-                return 'used'
-            return 'unused'
-        return cls.get_code(code).addCallback(_cb)
-
-    @classmethod
-    def load_codes(cls, codes):
-        def _lc(txn, cs):
-            for code in cs:
-                txn.execute(cls._insert_values_query(code=code), {'code': code})
-        return cls.runInteraction(_lc, codes)
-
-    @classmethod
-    def modify_code(cls, code, **kw):
-        kw.pop('modified', None)
-        setchunks = ["modified=current_timestamp"]
-        setchunks.extend(["%s=%%(%s)s" % (f, f) for f in kw.keys()])
-        setchunks = ", ".join(setchunks)
-        kw['_code'] = code
-        query = "UPDATE %s SET %s WHERE code=%%(_code)s" % (cls.table_name, setchunks)
-        return cls.runOperation(query, kw)
-
-    @classmethod
-    def burn_code(cls, code):
-        def _cb(txn, code):
-            query = cls._select_query("WHERE code=%(code)s")
-            txn.execute(query, {'code': code})
-            codes = cls._wrap_results(txn.fetchall())
-            if not codes:
-                return False, 'invalid'
-            if codes[0].used:
-                return False, 'used'
-            query = "UPDATE %s SET used=%%(used)s WHERE code=%%(code)s" % (cls.table_name)
-            txn.execute(query, {'code': code, 'used': True})
-            return True, 'valid'
-        return cls.runInteraction(_cb, code)
-
-
-
-
-class UniqueCode2(UglyNoDbModel):
     table_name = 'unique_codes'
     fields = (
         ('code', 'varchar(10) UNIQUE NOT NULL'),
