eb92f7f69d060b3de3de1213de014b782dac2e0a
Simon de Haan <simon@praekeltfoundation.org>
2011-07-15 22:51:04 +0200

    testing delivery receipts

--- vumi/workers/vas2nets/transport.py
+++ vumi/workers/vas2nets/transport.py
@@ -7,16 +7,20 @@ from twisted.web.server import NOT_DONE_YET
 from vumi.message import Message
 from vumi.service import Worker
 
-class Vas2NetsResource(Resource):
+class ReceiveSMSResource(Resource):
     isLeaf = True
     def __init__(self, config, publisher):
         self.config = config
         self.publisher = publisher
     
     def render_POST(self, request):
+        
         request.setResponseCode(http.OK)
+        request.setHeader('Content-Type', 'text/plain')
+        
         self.publisher.publish_message(Message(**{
             'transport_message_id': request.args['messageid'],
+            'transport_timestamp': request.args['time'],
             'to_msisdn': request.args['destination'],
             'from_msisdn': request.args['sender'],
             'message': request.args['text']
@@ -26,6 +30,23 @@ class Vas2NetsResource(Resource):
         ))
         return ''
     
+
+class DeliveryReceiptResource(Resource):
+    isLeaf = True
+    def __init__(self, config, publisher):
+        self.config = config
+        self.publisher = publisher
+    
+    def render_POST(self, request):
+        request.setResponseCode(http.OK)
+        request.setHeader('Content-Type', 'text/plain')
+        self.publisher.publish_message(Message(**{
+            'transport_message_id': request.args['smsid'],
+            'transport_status': request.args['status'],
+            'transport_status_message': request.args['text'],
+            'transport_timestamp': request.args['time'],
+        }), routing_key='sms.receipt.%(transport_name)s' % self.config)
+        return ''
 
 class Vas2NetsTransport(Worker):
     
