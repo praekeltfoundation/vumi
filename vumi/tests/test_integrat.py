36be21aa9c765c4c0f03e0ed0c651df53383d7f7
dmaclay <dmaclay@gmail.com>
2011-07-20 02:15:40 +0200

    sep funcs per xml test

--- vumi/tests/test_integrat.py
+++ vumi/tests/test_integrat.py
@@ -1,42 +1,45 @@
 import json
 
+from twisted.python import log
 from twisted.trial.unittest import TestCase
 from vumi.workers.integrat.utils import *
 
 
-class HigateTestCase(TestCase):
+class HigateXMLTestCases(TestCase):
 
     def setUp(self):
-        pass
+        self.dolog = True
+        self.hxp = HigateXMLParser()
 
     def tearDown(self):
-        pass
+        del self.hxp
 
-    def test_higate_xml_parser(self):
-        hxp = HigateXMLParser()
-
-        OnUSSEvent_xml = '''
+    def testOnResult(self):
+        OnResult_xml = '''
         <Message>
-            <Version Version="1.0"/>
-            <Response Type="OnUSSEvent">
+            <Version Version="1.0" />
+            <Response Type="OnResult"
+                      TOC="SMS"
+                      RefNo="2313344"
+                      SeqNo="8199250">
                 <SystemID>Higate</SystemID>
-                <UserID>LoginName</UserID>
-                <Service>SERVICECODE</Service>
-                <Network ID="1" MCC="655" MNC="001"/>
-                <OnUSSEvent Type="Request">
-                    <USSContext SessionID="16502" NetworkSID="310941653" MSISDN="27821234567" Script="testscript" ConnStr="*120*99*123#"/>
-                    <USSText Type="TEXT">REQ</USSText>
-                </OnUSSEvent>
+                <UserID>Http001</UserID>
+                <Service>HC001</Service>
+                <NetworkID>1</NetworkID>
+                <OnResult Flags="0"
+                          Code="3"
+                          SubCode="0"
+                          Text="Acknowledged" />
             </Response>
         </Message>
         '''
+        OnResult_dict = {'Code': '3', 'Text': 'Acknowledged', 'SubCode': '0', 'RefNo': '2313344', 'Flags': '0', 'SeqNo': '8199250', 'TOC': 'SMS', 'Type': 'OnResult'}
+        if self.dolog:
+            log.msg("OnResult -> %s" % (repr(self.hxp.parse(OnResult_xml))))
+        self.assertEquals(self.hxp.parse(OnResult_xml), OnResult_dict)
 
-        OnUSSEvent_dict = {'Script': 'testscript', 'MSISDN': '27821234567', 'Text': 'REQ', 'NetworkSID': '310941653', 'ConnStr': '*120*99*123#', 'SessionID': '16502', 'Type': 'OnUSSEvent'}
 
-        print hxp.parse(OnUSSEvent_xml)
-        self.assertEquals(hxp.parse(OnUSSEvent_xml), OnUSSEvent_dict)
-
-
+    def testOnReceiveSMS(self):
         OnReceiveSMS_xml = '''
         <Message>
             <Version Version="1.0"/>
@@ -60,34 +63,108 @@ class HigateTestCase(TestCase):
             </Response>
         </Message>
         '''
-
         OnReceiveSMS_dict = {'NetworkID': '1', 'FromAddr': '27829023456', 'SeqNo': '576674646', 'AdultRating': '0', 'hex': '06052677F6A565 ...etc', 'Value': '0', 'ToTag': '777', 'ToAddr': '27829020203777', 'EsmClass': '128', 'DataCoding': '8', 'Type': 'OnReceiveSMS', 'Sent': '20100614135709'}
+        if self.dolog:
+            log.msg("OnReceiveSMS -> %s" % (repr(self.hxp.parse(OnReceiveSMS_xml))))
+        self.assertEquals(self.hxp.parse(OnReceiveSMS_xml), OnReceiveSMS_dict)
 
-        print hxp.parse(OnReceiveSMS_xml)
-        self.assertEquals(hxp.parse(OnReceiveSMS_xml), OnReceiveSMS_dict)
 
-        OnResult_xml = '''
+    def testOnOBSResponse(self):
+        OnOBSResponse_xml = '''
         <Message>
-            <Version Version="1.0" />
-            <Response Type="OnResult"
-                      TOC="SMS"
-                      RefNo="2313344"
-                      SeqNo="8199250">
+            <Version Version="1.0"/>
+            <Response Type="OnOBSResponse" RefNo="123" SeqNo="1234568">
                 <SystemID>Higate</SystemID>
-                <UserID>Http001</UserID>
-                <Service>HC001</Service>
-                <NetworkID>1</NetworkID>
-                <OnResult Flags="0"
-                          Code="3"
-                          SubCode="0"
-                          Text="Acknowledged" />
+                <UserID>LoginName</UserID>
+                <Service>SERVICECODE</Service>
+                <NetworkID>2</NetworkID>
+                <Flags>32</Flags>
+                <ResultCode>6</ResultCode>
+                <ResultText>An exception occured in : setErrorVaribles : ControlException on control eventChargeValidation[ORA-0</ResultText>
+                <OnOBSResponse Type="TEXT"></OnOBSResponse>
             </Response>
         </Message>
         '''
+        OnOBSResponse_dict = {}
+        if self.dolog:
+            log.msg("OnOBSResponse -> %s" % (repr(self.hxp.parse(OnOBSResponse_xml))))
+        self.assertEquals(self.hxp.parse(OnOBSResponse_xml), OnOBSResponse_dict)
 
-        OnResult_dict = {'Code': '3', 'Text': 'Acknowledged', 'SubCode': '0', 'RefNo': '2313344', 'Flags': '0', 'SeqNo': '8199250', 'TOC': 'SMS', 'Type': 'OnResult'}
 
-        print hxp.parse(OnResult_xml)
-        self.assertEquals(hxp.parse(OnResult_xml), OnResult_dict)
+    def testOnLBSResponse(self):
+        OnLBSResponse_xml = '''
+        <Message>
+            <Version Version="1.0"/>
+            <Response Type="OnLBSResponse" RefNo="123" SeqNo="548245219">
+                <SystemID>Higate</SystemID>
+                <UserID>LoginName</UserID>
+                <Service>SERVICECODE</Service>
+                <NetworkID>1</NetworkID>
+                <Flags>4096</Flags>
+                <ResultCode>4</ResultCode>
+                <ResultText>Receipted</ResultText>
+                <OnLBSResponse Type="XML">
+                    <LBS>
+                        <AuthRef/>
+                        <SubService/>
+                        <Result>
+                            <Param>
+                                <Lat>-25955564</Lat>
+                                <Lon>28133442</Lon>
+                                <Accuracy>High</Accuracy>
+                                <DateTime>2009-01-27T13:17:28.000Z</DateTime>
+                                <ERange>0</ERange>
+                                <Zone>Vod:0:0</Zone>
+                                <RC>0</RC>
+                            </Param>
+                        </Result>
+                    </LBS>
+                </OnLBSResponse>
+            </Response>
+        </Message>
+        '''
+        OnLBSResponse_dict = {}
+        if self.dolog:
+            log.msg("OnLBSResponse -> %s" % (repr(self.hxp.parse(OnLBSResponse_xml))))
+        self.assertEquals(self.hxp.parse(OnLBSResponse_xml), OnLBSResponse_dict)
+
+
+    def testOnUSSEvent(self):
+        OnUSSEvent_xml = '''
+        <Message>
+            <Version Version="1.0"/>
+            <Response Type="OnUSSEvent">
+                <SystemID>Higate</SystemID>
+                <UserID>LoginName</UserID>
+                <Service>SERVICECODE</Service>
+                <Network ID="1" MCC="655" MNC="001"/>
+                <OnUSSEvent Type="Request">
+                    <USSContext SessionID="16502" NetworkSID="310941653" MSISDN="27821234567" Script="testscript" ConnStr="*120*99*123#"/>
+                    <USSText Type="TEXT">REQ</USSText>
+                </OnUSSEvent>
+            </Response>
+        </Message>
+        '''
+        OnUSSEvent_dict = {'Script': 'testscript', 'MSISDN': '27821234567', 'Text': 'REQ', 'NetworkSID': '310941653', 'ConnStr': '*120*99*123#', 'SessionID': '16502', 'Type': 'OnUSSEvent'}
+        if self.dolog:
+            log.msg("OnUSSEvent -> %s" % (repr(self.hxp.parse(OnUSSEvent_xml))))
+        self.assertEquals(self.hxp.parse(OnUSSEvent_xml), OnUSSEvent_dict)
+
+
+    def testUSSReply(self):
+        USSReply_xml = '''
+        <Message>
+        <Version Version="1.0"/>
+         <Request Type="USSReply" SessionID="223665" Flags="0">
+               <UserID Orientation="TR">LoginName</UserID>
+               <Password>xxxxxxxx</Password>
+               <USSText Type="TEXT">Welcome the this USSD session</USSText>
+         </Request>
+        </Message>
+        '''
+        USSReply_dict = {}
+        if self.dolog:
+            log.msg("USSReply -> %s" % (repr(self.hxp.parse(USSReply_xml))))
+        self.assertEquals(self.hxp.parse(USSReply_xml), USSReply_dict)
 
 
