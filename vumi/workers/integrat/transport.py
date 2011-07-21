5380ead7ebdd1dd61327addeec01f50efcff54da
Simon de Haan <simon@praekeltfoundation.org>
2011-07-21 07:52:31 +0200

    cleaned up implementation of Integrat transport

--- vumi/workers/integrat/transport.py
+++ vumi/workers/integrat/transport.py
@@ -4,67 +4,92 @@ from twisted.internet.defer import inlineCallbacks
 from twisted.web.client import Agent
 from twisted.web.http_headers import Headers
 from twisted.python import log
+from twisted.web import http
+from twisted.web.resource import Resource
 
-from vumi.utils import StringProducer
+from vumi.utils import http_request, safe_routing_key
+from vumi.message import Message
+from vumi.service import Worker
 from vumi.workers.httprpc.transport import HttpRpcTransport
 from vumi.webapp.api.utils import post_data_to_url
+from vumi.workers.integrat.utils import HigateXMLParser
 
-def httpRequest(url, data, headers={}, method='POST'):
-    # Construct an Agent.
-    agent = Agent(reactor)
+hxg = HigateXMLParser()
 
-    d = agent.request(method,
-                      url,
-                      Headers(headers),
-                      StringProducer(data) if data else None)
+class IntegratHttpResource(Resource):
+    def __init__(self, config, publisher):
+        self.config = config
+        self.publisher = publisher
+    
+    def render(self, request):
+        request.setResponseCode(http.OK)
+        request.setHeader('Content-Type', 'text/plain')
+        with self.publisher.transaction():
+            hxg_msg = hxg.parse(request.content.read())
+            text = hxg_msg.get('USSText','').strip()
+            if hxg_msg['EventType'] == 'Request':
+                if text == 'REQ':
+                    event_type = 'new_session'
+                else:
+                    event_type = 'resume_session'
+            else:
+                event_type = '%s_session' % hxg_msg['EventType'].lower()
+            
+            routing_key = 'ussd.inbound.%s.%s' % (
+                self.config['transport_name'],
+                safe_routing_key(hxg_msg['ConnStr'])
+            )
+            msg = Message(**{
+                'transport_session_id': hxg_msg['SessionID'],
+                'transport_message_type': event_type,
+                'message': text,
+                'recipient': hxg_msg['ConnStr'],
+                'sender': hxg_msg['MSISDN'],
+            })
+            log.msg('publishing', msg, routing_key)
+            self.publisher.publish_message(msg, routing_key=routing_key)
+        return ''
+    
 
-    def handle_response(response):
-        if response.code == 204:
-            d = defer.succeed('')
-        else:
-            class SimpleReceiver(protocol.Protocol):
-                def __init__(s, d):
-                    s.buf = ''; s.d = d
-                def dataReceived(s, data):
-                    s.buf += data
-                def connectionLost(s, reason):
-                    # TODO: test if reason is twisted.web.client.ResponseDone, if not, do an errback
-                    s.d.callback(s.buf)
+class HealthResource(Resource):
+    isLeaf = True
 
-            d = defer.Deferred()
-            response.deliverBody(SimpleReceiver(d))
-        return d
+    def render(self, request):
+        request.setResponseCode(http.OK)
+        return 'OK'
 
-    d.addCallback(handle_response)
-    return d
-
-
-class IntegratTransport(HttpRpcTransport):
+class IntegratOutboundTransport(Worker):
+    @inlineCallbacks
+    def startWorker(self):
+        self.consumer = yield self.consume('ussd.outbound.%(transport_name)s' % self.config,
+                                            self.handle_outbound_message)
     
     @inlineCallbacks
-    def finishRequest(self, uuid, message=''):
-        log.msg('finishRequest for', uuid, "'%s'" % message)
-        data = str(message).strip()
-        log.msg("HttpRpcTransport.finishRequest with data:", repr(data))
-        log.msg(repr(self.requests))
-        request = self.requests.get(uuid)
-        if request:
-            request.setHeader('Content-Type', 'text/xml; charset=utf-8')
-            if data:
-                request.setHeader('Content-Length', len(data))
-                request.write(data)
-            else:
-                request.setResponseCode(204)
-            request.finish()
-            
-            if data:
-                yield httpRequest(
-                    self.config['url'],data,
-                    headers={'Content-Type': ['text/xml; charset:utf-8']}
-                )
-            
-            del self.requests[uuid]
+    def handle_outbound_message(self, message):
+        data = message.payload
+        yield http_request(self.config['url'], hxg.build({
+            'Flags': data.get('close','0'),
+            'SessionID': data['transport_session_id'],
+            'Type': 'USSReply',
+            'USSText': data['message'],
+            'Password': self.config.get('password'),
+            'UserID': self.config.get('username')
+        }), headers = {
+            'Content-Type': ['text/xml; charset=utf-8']
+        })
+        
 
-    def stopWorker(self):
-        log.msg("Stopping the HttpRpcTransport")
+class IntegratInboundTransport(Worker):
     
+    @inlineCallbacks
+    def startWorker(self):
+        """called by the Worker class when the AMQP connections been established"""
+        self.publisher = yield self.publish_to('ussd.inbound.%(transport_name)s.fallback' % self.config)
+        self.web_resource = yield self.start_web_resources(
+            [
+                (IntegratHttpResource(self.config, self.publisher), self.config['web_path']),
+                (HealthResource(), 'health'),
+            ],
+            self.config['web_port']
+        )
+
--- vumi/workers/integrat/worker.py
+++ vumi/workers/integrat/worker.py
@@ -3,72 +3,56 @@ from twisted.python import log
 
 from vumi.message import Message
 from vumi.service import Worker
+from vumi.utils import safe_routing_key
 from vumi.errors import VumiError
-from vumi.workers.integrat.utils import HigateXMLParser
-
-hxp = HigateXMLParser()
 
 class IntegratWorker(Worker):
     
     @inlineCallbacks
     def startWorker(self):
         log.msg('starting worker with ', self.config)
-        self.publisher = yield self.publish_to(self.config['publish_key'])
-        self.consumer = yield self.consume(self.config['consume_key'], 
-                                            self.consume_message)
+        self.publisher = yield self.publish_to('ussd.outbound.%(transport_name)s' % self.config)
+        self.consumer = yield self.consume('ussd.inbound.%s.%s' % (
+            self.config['transport_name'],
+            safe_routing_key(self.config['ussd_code'])
+        ), self.consume_message)
         
     
     def consume_message(self, message):
-        uuid = message.payload.get('uuid')
-        xml_message = message.payload.get('message')
-        hgmsg = hxp.parse(xml_message.get('content'))
-        log.msg(hgmsg)
-        
-        event_name = hgmsg.get('EventType','').lower()
-        handler = getattr(self, 'handle_%s' % event_name, self.noop)
-        msg = Message(uuid=uuid,message=handler(hgmsg))
-        self.publisher.publish_message(msg)
+        data = message.payload
+        handler = getattr(self, '%(transport_message_type)s' % data,
+                            self.noop)
+        handler(data)
     
-    def noop(self, hgmsg):
-        return ''
+    def noop(self, data):
+        log.msg('Got', data, 'but not doing anything with it')
     
     def end(self, session_id, closing_text):
         return self.reply(session_id, closing_text, 1)
     
     def reply(self, session_id, reply_text, flag=0):
-        return hxp.build({
-            'Flags': str(flag),
-            'SessionID': session_id,
-            'Type': 'USSReply',
-            'USSText': reply_text,
-            'Password': self.config.get('password'),
-            'UserID': self.config.get('username')
-        })
+        self.publisher.publish_message(Message(**{
+            'transport_session_id': session_id,
+            'message': reply_text,
+            'close': str(flag)
+        }))
     
-    def handle_request(self, hgmsg):
-        session_id = hgmsg['SessionID']
-        text = hgmsg['USSText'].strip()
-        if text == 'REQ':
-            return self.new_session(session_id, text)
-        return self.resume_session(session_id, text)
+    def new_session(self, data):
+        session_id = data['transport_session_id']
+        msisdn = data['sender']
+        self.reply(session_id, 'Hi %s, this is an echo service. ' % msisdn +
+                                'Enter 0 to stop.')
     
-    def handle_open(self, hgmsg):
-        session_id = hgmsg['SessionID']
-        self.session_started(session_id)
+    def resume_session(self, data):
+        session_id = data['transport_session_id']
+        message = data['message'].strip()
+        if message == '0':
+            self.end(session_id, 'Thanks!')
+        else:
+            self.reply(session_id, 'You said: %s' % message)
     
-    def handle_close(self, hgmsg):
-        session_id = hgmsg['SessionID']
-        self.session_ended(session_id)
+    def open_session(self, data):
+        pass
     
-    def session_started(self, session_id):
-        log.msg('session started for %s' % session_id)
-    
-    def session_ended(self, session_id):
-        log.msg('session ended for %s' % session_id)
-    
-    def new_session(self, session_id, text):
-        return self.reply(session_id, 'hi there new %s' % session_id)
-    
-    def resume_session(self, session_id, text):
-        return self.end(session_id, 'you said %s. Bye!' % text)
-    
+    def close_session(self, data):
+        pass
