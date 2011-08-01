08191a2d6ffe456dd8eb54bd1eb6ff3bb3d0fac3
Simon de Haan <simon@praekeltfoundation.org>
2011-08-01 22:50:12 +0200

    using the coverage reporter & coverage to generate cobertura coverage xml output

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -6,3 +6,4 @@ find vumi/ -name '*.pyc' -delete
 
 ./manage.py test #-- --with-xunit --xunit-file=django_test_results.xml
 trial --coverage --reporter=subunit vumi | subunit2junitxml | grep --line-buffered -Fv 'Setting coverage directory to coverage.' | tee test_results.xml
+trial --reporter=bwverbose-coverage vumi && coverage xml
