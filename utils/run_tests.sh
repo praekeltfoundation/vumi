2bec219eee31be02022a087a814f2efd1c2216d2
Jeremy Thurgood <jeremy@praekelt.com>
2011-08-01 19:30:29 +0200

    Ignore the coverage message from trial.

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -4,5 +4,5 @@ cd $(dirname $0)/..
 
 find vumi/ -name '*.pyc' -delete
 
-./manage.py test
-trial --coverage --reporter=subunit vumi | subunit2junitxml > test_results.xml
+./manage.py test #-- --with-xunit --xunit-file=django_test_results.xml
+trial --coverage --reporter=subunit vumi | subunit2junitxml | grep --line-buffered -Fv 'Setting coverage directory to coverage.' | tee test_results.xml
