6e8b240747a75a341b37c63410ad2ec2b1acdf45
Jeremy Thurgood <jeremy@praekelt.com>
2011-08-01 23:42:18 +0200

    Turn on xunit output for Django tests.

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -4,6 +4,6 @@ cd $(dirname $0)/..
 
 find vumi/ -name '*.pyc' -delete
 
-./manage.py test #-- --with-xunit --xunit-file=django_test_results.xml
+./manage.py test -- --with-xunit --xunit-file=django_test_results.xml
 trial --reporter=subunit vumi | subunit2junitxml > test_results.xml
 trial --reporter=bwverbose-coverage vumi && coverage xml
