b391796c1c90d01016d93837039cc828e3ac9e51
Jeremy Thurgood <jeremy@praekelt.com>
2011-08-11 11:23:32 +0200

    PEP-8 cleanup for vumi.workers.integrat

--- vumi/workers/integrat/worker.py
+++ vumi/workers/integrat/worker.py
@@ -8,42 +8,42 @@ from vumi.utils import safe_routing_key
 
 
 class IntegratWorker(Worker):
-    
+
     @inlineCallbacks
     def startWorker(self):
         log.msg('starting worker with ', self.config)
-        self.publisher = yield self.publish_to('ussd.outbound.%(transport_name)s' % self.config)
+        self.publisher = yield self.publish_to(
+            'ussd.outbound.%(transport_name)s' % self.config)
         self.consumer = yield self.consume('ussd.inbound.%s.%s' % (
             self.config['transport_name'],
             safe_routing_key(self.config['ussd_code'])
         ), self.consume_message)
-        
-    
+
     def consume_message(self, message):
         data = message.payload
         handler = getattr(self, '%(transport_message_type)s' % data,
                             self.noop)
         handler(data)
-    
+
     def noop(self, data):
         log.msg('Got', data, 'but not doing anything with it')
-    
+
     def end(self, session_id, closing_text):
         return self.reply(session_id, closing_text, 1)
-    
+
     def reply(self, session_id, reply_text, flag=0):
         self.publisher.publish_message(Message(**{
             'transport_session_id': session_id,
             'message': reply_text,
             'close': str(flag)
         }))
-    
+
     def new_session(self, data):
         session_id = data['transport_session_id']
         msisdn = data['sender']
         self.reply(session_id, 'Hi %s, this is an echo service. ' % msisdn +
                                 'Enter 0 to stop.')
-    
+
     def resume_session(self, data):
         session_id = data['transport_session_id']
         message = data['message'].strip()
@@ -51,21 +51,23 @@ class IntegratWorker(Worker):
             self.end(session_id, 'Thanks!')
         else:
             self.reply(session_id, 'You said: %s' % message)
-    
+
     def open_session(self, data):
         pass
-    
+
     def close_session(self, data):
         pass
-    
+
 
 class TwitterUSSDTransport(IntegratWorker):
 
     @inlineCallbacks
     def startWorker(self):
         """docstring for startWorker"""
-        self.publisher = yield self.publish_to('ussd.outbound.%(transport_name)s' % self.config)
-        self.twitter = twitter.Twitter(self.config['username'], self.config['password'])
+        self.publisher = yield self.publish_to(
+            'ussd.outbound.%(transport_name)s' % self.config)
+        self.twitter = twitter.Twitter(self.config['username'],
+                                       self.config['password'])
         self.consumer = yield self.consume('ussd.inbound.%s.%s' % (
             self.config['transport_name'],
             safe_routing_key(self.config['ussd_code'])
@@ -73,17 +75,18 @@ class TwitterUSSDTransport(IntegratWorker):
 
     def new_session(self, data):
         session_id = data['transport_session_id']
-        self.reply(session_id, 'Whose latest twitter mention do you want to see?')
-    
+        self.reply(session_id,
+                   'Whose latest twitter mention do you want to see?')
+
     @inlineCallbacks
     def resume_session(self, data):
         session_id = data['transport_session_id']
-        search_term = data['message'].replace('@','')
-        
+        search_term = data['message'].replace('@', '')
+
         def got_entry(msg):
             log.msg(msg)
             self.end(session_id, msg.title)
-        
-        resp = yield self.twitter.search('@%s' % search_term, got_entry, {
+
+        yield self.twitter.search('@%s' % search_term, got_entry, {
             'rpp': '1'
         })
