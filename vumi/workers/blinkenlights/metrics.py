5eca19770de01cc645dc97e3dabf813e26dd41b9
Simon Cross <simon.cross@praekeltfoundation.org>
2011-08-24 14:35:29 +0200

    Convert metric workers to new blinkenlights metric framework (one test fails -- the test itself is waiting on the completion of the test broker).

--- vumi/workers/blinkenlights/metrics.py
+++ vumi/workers/blinkenlights/metrics.py
@@ -1,150 +1,40 @@
 # -*- test-case-name: vumi.workers.blinkenlights.tests.test_metrics -*-
 
 import time
-from datetime import datetime
 import random
 
 from twisted.python import log
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import inlineCallbacks, Deferred
 from twisted.internet import reactor
+from twisted.internet.task import LoopingCall
 
-from vumi.service import Consumer, Publisher, Worker
-from vumi.blinkenlights.message20110707 import MetricsMessage
-
-
-class MetricsConsumer(Consumer):
-    exchange_name = "vumi.metrics"
-    exchange_type = "direct"
-    routing_key = "vumi.metrics"
-    durable = True
-
-    def __init__(self, callback):
-        self.callback = callback
-        self.queue_name = self.routing_key
-
-    def consume_message(self, message):
-        return self.callback(message)
-
-
-def timedelta_millis(td):
-    millis = 1000 * 3600 * 24 * td.days
-    millis += 1000 * td.seconds
-    millis += td.microseconds / 1000.0
-    return millis
-
-
-class MetricsPublisher(Publisher):
-    exchange_name = "vumi.metrics"
-    exchange_type = "direct"
-    routing_key = "vumi.metrics"
-    durable = True
-    auto_delete = False
-    delivery_mode = 2
-
-    def __init__(self):
-        self._reset_current_metrics()
-
-    def add_counter(self, counter_name, value=1):
-        """
-        Increment a counter.
-        """
-        current_value = self.counters.get(counter_name, 0)
-        self.counters[counter_name] = current_value + value
-
-    def start_timer(self, timer_name):
-        """
-        Start a timer.
-        """
-        self._get_timer(timer_name)['start'].append(datetime.utcnow())
-
-    def stop_timer(self, timer_name):
-        """
-        Start a timer that was previously started with start_timer().
-        """
-        if timer_name not in self.timers:
-            # TODO: Find a way to dedup this
-            raise ValueError("Trying to stop timer that hasn't been"
-                             " started: %s" % (timer_name))
-        timer = self._get_timer(timer_name)
-        if not len(timer['stop']) < len(timer['start']):
-            raise ValueError("Trying to stop timer that hasn't been"
-                             " started: %s" % (timer_name))
-        timer['stop'].append(datetime.utcnow())
-
-    def send_metrics(self):
-        """
-        Send metrics to Blinkenlights.
-
-        All metrics collected since the last call to send_metrics()
-        are sent and then cleared, ready for collecting the next set.
-        """
-        msg = self._make_message(self._build_metrics())
-        self.publish_message(msg)
-        self._reset_current_metrics()
-
-    def _reset_current_metrics(self):
-        self.counters = {}
-        self.timers = {}
-
-    def _build_metrics(self):
-        metrics = []
-        for name, value in self.counters.items():
-            metrics.append({'name': name, 'count': value})
-        for name, value in self.timers.items():
-            start, stop = value['start'], value['stop']
-            if len(start) != len(stop):
-                # TODO: Something appropriate
-                pass
-            times = zip(start, stop)
-            total_millis = sum(timedelta_millis(e - s) for s, e in times)
-            metrics.append({'name': name, 'count': len(times),
-                            'time': total_millis})
-        return metrics
-
-    def _get_timer(self, timer_name):
-        return self.timers.setdefault(timer_name, {'start': [], 'stop': []})
-
-    def _make_message(self, metrics):
-        msg = MetricsMessage('metrics', 'metrics generator', '0', metrics)
-        return msg.to_vumi_message()
+from vumi.service import Publisher, Worker
+from vumi.blinkenlights.metrics import (MetricsConsumer, MetricManager, Count,
+                                        Sum, SimpleValue, Timer)
 
 
 class GraphitePublisher(Publisher):
+    """Publisher for sending messages to Graphite."""
+
     exchange_name = "graphite"
     exchange_type = "topic"
     durable = True
     auto_delete = False
     delivery_mode = 2
 
+    def _local_timestamp(self, timestamp):
+        """Graphite requires local timestamps."""
+        # TODO: investigate whether graphite can be encourage to use UTC
+        #       timestamps
+        return timestamp - time.timezone
+
     def publish_metric(self, metric, value, timestamp):
+        timestamp = self._local_timestamp(timestamp)
         self.publish_raw("%f %d" % (value, timestamp), routing_key=metric)
 
 
-class MetricsCollector(Worker):
-
-    @inlineCallbacks
-    def startWorker(self):
-        log.msg("Starting the MetricsCollector with config: %s" % self.config)
-        self.consumer = yield self.start_consumer(MetricsConsumer,
-                                                  self.consume_metrics)
-
-    def consume_metrics(self, message):
-        msg = MetricsMessage.from_dict(message.payload)
-        for name, metrics in msg.metrics.items():
-            self.metrics.setdefault(name, []).extend(metrics)
-        log.msg("Collected: " + ", ".join("%s=%s" % m for m in
-                                          self.metrics_stats()))
-
-    def metrics_stats(self, *keys):
-        if not keys:
-            keys = self.metrics.keys()
-        return [(k, len(self.metrics[k])) for k in keys]
-
-    def stopWorker(self):
-        log.msg("Stopping the MetricsCollector")
-
-
 class GraphiteMetricsCollector(Worker):
+    """Worker that collects Vumi metrics and publishes them to Graphite."""
 
     @inlineCallbacks
     def startWorker(self):
@@ -154,45 +44,67 @@ class GraphiteMetricsCollector(Worker):
         self.consumer = yield self.start_consumer(MetricsConsumer,
                                                   self.consume_metrics)
 
-    def process_timestamp(self, timestamp):
-        unix_timestamp = time.mktime(timestamp.timetuple())
-        # Convert to local time for Graphite *vomit*
-        unix_timestamp = unix_timestamp - time.timezone
-        return unix_timestamp
-
-    def consume_metrics(self, message):
-        msg = MetricsMessage.from_dict(message.payload)
-        unix_timestamp = self.process_timestamp(msg.timestamp)
-        for name, metrics in msg.metrics.items():
-            value = sum(m[0] for m in metrics)
-            self.graphite_publisher.publish_metric(name, value, unix_timestamp)
+    def consume_metrics(self, metric_name, timestamp, value):
+        self.graphite_publisher.publish_metric(metric_name, value, timestamp)
 
     def stopWorker(self):
         log.msg("Stopping the GraphiteMetricsCollector")
 
 
 class RandomMetricsGenerator(Worker):
+    """Worker that publishes a set of random metrics.
+
+    Useful for tests and demonstrations.
+
+    Configuration Values
+    --------------------
+    manager_period : float in seconds, optional
+        How often to have the internal metric manager send metrics
+        messages. Default is 5s.
+    generator_period: float in seconds, optional
+        How often the random metric loop should send values to the
+        metric manager. Default is 1s.
+    """
 
     @inlineCallbacks
     def startWorker(self):
         log.msg("Starting the MetricsGenerator with config: %s" % self.config)
-        self.publisher = yield self.start_publisher(MetricsPublisher)
-        self.run()
+        manager_period = float(self.config.get("manager_period", 5.0))
+        log.msg("MetricManager will sent metrics every %s seconds",
+                manager_period)
+        generator_period = float(self.config.get("generator_period", 1.0))
+        log.msg("Random metrics values will be generated every %s seconds",
+                generator_period)
 
+        self.mm = yield self.start_publisher(MetricManager, "vumi.random.",
+                                             manager_period)
+        self.counter = self.mm.register(Count("count"))
+        self.sum = self.mm.register(Sum("sum"))
+        self.simple = self.mm.register(SimpleValue("value"))
+        self.timer = self.mm.register(Timer("timer"))
+        self.next = Deferred()
+        self.task = LoopingCall(self.run)
+        self.task.start(generator_period)
+
+    @inlineCallbacks
     def run(self):
-        self.send_some_metrics()
-        reactor.callLater(random.choice([0.1, 0.2, 0.5, 0.5, 1, 1]), self.run)
+        if random.choice([True, False]):
+            self.counter.inc()
+        self.sum.add(random.uniform(0.0, 1.0))
+        self.simple.set(random.normalvariate(2.0, 0.1))
+        with self.timer:
+            d = Deferred()
+            wait = random.uniform(0.0, 0.1)
+            reactor.callLater(wait, lambda: d.callback(None))
+            yield d
+        done, self.next = self.next, Deferred()
+        done.callback(self)
 
-    def generate_metric(self):
-        metric_name = 'vumi.metrics.' + random.choice(['foo', 'bar', 'baz'])
-        self.publisher.add_counter(metric_name, random.randint(1, 10))
-
-    def send_some_metrics(self):
-        num = random.randint(3, 10)
-        for i in range(num):
-            self.generate_metric()
-        log.msg("Sending %s metrics" % (num,))
-        self.publisher.send_metrics()
+    def wake_after_run(self):
+        """Return a deferred that fires after the next run completes."""
+        return self.next
 
     def stopWorker(self):
+        self.mm.stop()
+        self.task.stop()
         log.msg("Stopping the MetricsGenerator")
--- vumi/workers/blinkenlights/tests/test_metrics.py
+++ vumi/workers/blinkenlights/tests/test_metrics.py
@@ -1,113 +1,71 @@
-from datetime import datetime, timedelta
-
 from twisted.trial.unittest import TestCase
-
+from twisted.internet.defer import inlineCallbacks
+from vumi.tests.utils import (TestChannel, get_stubbed_worker,
+                              FakeAMQBroker)
 from vumi.workers.blinkenlights import metrics
+from vumi.message import Message
+
+import time
 
 
-class UTCNearNow(object):
-    def __init__(self, threshold=10):
-        self.threshold = threshold
+class TestGraphitePublisher(TestCase):
 
-    def __eq__(self, other):
-        if not isinstance(other, datetime):
-            return False
-        now = datetime.utcnow()
-        if other > now:
-            return False
-        if other < now - timedelta(seconds=self.threshold):
-            return False
-        return True
+    def _check_msg(self, channel, metric, value, timestamp):
+        msg = channel.publish_log[-1]
+        self.assertEqual(msg["routing_key"], metric)
+        self.assertEqual(msg["exchange"], "graphite")
+        content = msg["content"]
+        self.assertEqual(content.properties, {"delivery mode": 2})
+        self.assertEqual(content.body, "%f %d" % (value,
+                                                  timestamp - time.timezone))
+
+    def test_publish_metric(self):
+        datapoint = ("vumi.test.v1", 1.0, 1234)
+        channel = TestChannel()
+        pub = metrics.GraphitePublisher()
+        pub.start(channel)
+        pub.publish_metric(*datapoint)
+        self._check_msg(channel, *datapoint)
 
 
-class MetricsPublisherTestCase(TestCase):
+class TestGraphiteMetricsCollector(TestCase):
+    @inlineCallbacks
+    def test_single_message(self):
+        broker = FakeAMQBroker()
+        worker = get_stubbed_worker(metrics.GraphiteMetricsCollector,
+                                    broker=broker)
+        yield worker.startWorker()
 
-    def _get_testable_publisher(self):
-        pub = metrics.MetricsPublisher()
-        pub._published = []
+        broker.publish("vumi.metrics", "vumi.metrics", "foo")
+        yield None
 
-        def _pm(msg):
-            pub._published.append(msg)
-        pub.publish_message = _pm
-        return pub
+        output_channel = worker._amqp_client.channels[0]
+        input_channel = worker._amqp_client.channels[1]
 
-    def assert_pub_metrics(self, pub, counters, timers):
-        self.assertEquals(counters, pub.counters)
-        self.assertEquals(timers, pub.timers)
+        self.fail("TODO: send message into input channel and test")
 
-    def test_counters(self):
-        """
-        Adding a counter to a metrics publisher should do the right thing.
-        """
-        pub = self._get_testable_publisher()
 
-        self.assert_pub_metrics(pub, {}, {})
-        pub.add_counter('foo')
-        self.assert_pub_metrics(pub, {'foo': 1}, {})
-        pub.add_counter('foo', 2)
-        self.assert_pub_metrics(pub, {'foo': 3}, {})
-        pub.add_counter('bar', 2)
-        self.assert_pub_metrics(pub, {'foo': 3, 'bar': 2}, {})
-        pub.add_counter('bar')
-        self.assert_pub_metrics(pub, {'foo': 3, 'bar': 3}, {})
+class TestRandomMetricsGenerator(TestCase):
+    @inlineCallbacks
+    def test_one_run(self):
+        broker = FakeAMQBroker()
+        worker = get_stubbed_worker(metrics.RandomMetricsGenerator,
+                                    config={
+                                        "manager_period": "0.1",
+                                        "generator_period": "0.1",
+                                    },
+                                    broker=broker)
+        yield worker.startWorker()
 
-    def test_timers(self):
-        """
-        Starting and stopping timers should do the right things.
-        """
-        pub = self._get_testable_publisher()
+        yield worker.wake_after_run()
+        yield worker.wake_after_run()
 
-        self.assert_pub_metrics(pub, {}, {})
-        self.assertRaises(ValueError, pub.stop_timer, 'foo')
-        self.assert_pub_metrics(pub, {}, {})
-        t = UTCNearNow()
-        pub.start_timer('foo')
-        self.assert_pub_metrics(pub, {}, {'foo': {'start': [t], 'stop': []}})
-        pub.start_timer('foo')
-        self.assert_pub_metrics(pub, {}, {'foo': {'start': [t, t],
-                                                  'stop': []}})
-        pub.stop_timer('foo')
-        self.assert_pub_metrics(pub, {}, {'foo': {'start': [t, t],
-                                                  'stop': [t]}})
-        pub.start_timer('bar')
-        self.assert_pub_metrics(pub, {}, {'foo': {'start': [t, t],
-                                                  'stop': [t]},
-                                          'bar': {'start': [t],
-                                                  'stop': []}})
-        pub.stop_timer('foo')
-        self.assert_pub_metrics(pub, {}, {'foo': {'start': [t, t],
-                                                  'stop': [t, t]},
-                                          'bar': {'start': [t], 'stop': []}})
-        pub.stop_timer('bar')
-        self.assert_pub_metrics(pub, {}, {'foo': {'start': [t, t],
-                                                  'stop': [t, t]},
-                                          'bar': {'start': [t], 'stop': [t]}})
-        self.assertRaises(ValueError, pub.stop_timer, 'foo')
+        msg1, msg2 = broker.get_dispatched('vumi.metrics', 'vumi.metrics')
+        msg1 = Message.from_json(msg1.body).payload
+        self.assertEqual(msg1["datapoints"], [])
+        msg2 = Message.from_json(msg2.body).payload
+        self.assertEqual(sorted(d[0] for d in msg2["datapoints"]),
+                         ["vumi.random.count", "vumi.random.sum",
+                          "vumi.random.timer", "vumi.random.value"])
 
-    def test_build_metrics(self):
-        """
-        A metrics message should be built in the correct format.
-        """
-        pub = self._get_testable_publisher()
-        t = lambda secs: datetime(2011, 07, 07, 00, 00, secs)
-
-        pub.counters = {'foo': 10, 'bar': 11}
-        pub.timers = {
-            'foot': {
-                'start': [t(0), t(1)],
-                'stop': [t(3), t(2)],
-                },
-            'bart': {
-                'start': [t(4)],
-                'stop': [t(5)]},
-            }
-
-        _key = lambda i: (i['name'], i['count'])
-        expected = sorted([
-                {'name': 'foo', 'count': 10},
-                {'name': 'bar', 'count': 11},
-                {'name': 'foot', 'count': 2, 'time': 4000.0},
-                {'name': 'bart', 'count': 1, 'time': 1000.0},
-                ], key=_key)
-
-        self.assertEquals(expected, sorted(pub._build_metrics(), key=_key))
+        yield worker.stopWorker()
