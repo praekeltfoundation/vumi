60fd9f06c087c599e556565addf2231206d53505
dmaclay <dmaclay@gmail.com>
2011-09-08 18:21:07 +0200

    pep8 ... we hate's the nasty pep, my precioussss

--- vumi/workers/smpp/server.py
+++ vumi/workers/smpp/server.py
@@ -5,16 +5,16 @@ from twisted.python import log
 from twisted.internet import reactor
 from twisted.internet.protocol import Protocol, ServerFactory
 
-from smpp.pdu_builder import (BindTransceiverResp, EnquireLinkResp, SubmitSMResp,
-                                DeliverSM)
+from smpp.pdu_builder import (BindTransceiverResp, EnquireLinkResp,
+        SubmitSMResp, DeliverSM)
 from smpp.pdu_inspector import binascii, unpack_pdu
+
 
 class SmscServer(Protocol):
 
     def __init__(self):
         log.msg('__init__', 'SmscServer')
         self.datastream = ''
-
 
     def popData(self):
         data = None
@@ -24,7 +24,6 @@ class SmscServer(Protocol):
                 data = self.datastream[0:command_length]
                 self.datastream = self.datastream[command_length:]
         return data
-
 
     def handleData(self, data):
         pdu = unpack_pdu(data)
@@ -38,16 +37,14 @@ class SmscServer(Protocol):
         if pdu['header']['command_id'] == 'enquire_link':
             self.handle_enquire_link(pdu)
 
-
     def handle_bind_transceiver(self, pdu):
         if pdu['header']['command_status'] == 'ESME_ROK':
             sequence_number = pdu['header']['sequence_number']
-            system_id = system_id = pdu['body']['mandatory_parameters']['system_id']
+            system_id = pdu['body']['mandatory_parameters']['system_id']
             pdu_resp = BindTransceiverResp(sequence_number,
-                    system_id = system_id
+                    system_id=system_id
                     )
             self.sendPDU(pdu_resp)
-
 
     def handle_enquire_link(self, pdu):
         if pdu['header']['command_status'] == 'ESME_ROK':
@@ -55,21 +52,20 @@ class SmscServer(Protocol):
             pdu_resp = EnquireLinkResp(sequence_number)
             self.sendPDU(pdu_resp)
 
-
     def command_status(self, pdu):
         if pdu['body']['mandatory_parameters']['short_message'][:5] == "ESME_":
-            return pdu['body']['mandatory_parameters']['short_message'].split(' ')[0]
+            return pdu['body']['mandatory_parameters']['short_message'].split(
+                    ' ')[0]
         else:
             return 'ESME_ROK'
-
-
 
     def handle_submit_sm(self, pdu):
         if pdu['header']['command_status'] == 'ESME_ROK':
             sequence_number = pdu['header']['sequence_number']
             message_id = str(uuid.uuid4())
             command_status = self.command_status(pdu)
-            pdu_resp = SubmitSMResp(sequence_number, message_id, command_status)
+            pdu_resp = SubmitSMResp(
+                    sequence_number, message_id, command_status)
             self.sendPDU(pdu_resp)
             reactor.callLater(2, self.delivery_report, message_id)
             self.boomerang(pdu)
@@ -81,19 +77,17 @@ class SmscServer(Protocol):
         destination_addr = pdu['body']['mandatory_parameters']['source_addr']
         source_addr = pdu['body']['mandatory_parameters']['destination_addr']
         pdu = DeliverSM(sequence_number,
-                short_message = short_message,
-                destination_addr = destination_addr,
-                source_addr = source_addr)
+                short_message=short_message,
+                destination_addr=destination_addr,
+                source_addr=source_addr)
         self.sendPDU(pdu)
-
 
     def delivery_report(self, message_id):
         sequence_number = 1
         short_message = 'id:%s sub:001 dlvrd:001 submit date:%s done date:%s stat:DELIVRD err:000 text:' % (
                 message_id, datetime.now().strftime("%y%m%d%H%M%S"), datetime.now().strftime("%y%m%d%H%M%S"))
-        pdu = DeliverSM(sequence_number, short_message = short_message)
+        pdu = DeliverSM(sequence_number, short_message=short_message)
         self.sendPDU(pdu)
-
 
     def boomerang(self, pdu):
         if pdu['body']['mandatory_parameters']['short_message'] == "boomerang":
@@ -103,28 +97,27 @@ class SmscServer(Protocol):
             sequence_number = 1
             short_message1 = "\x05\x00\x03\xff\x03\x01back"
             pdu1 = DeliverSM(sequence_number,
-                    short_message = short_message1,
-                    destination_addr = destination_addr,
-                    source_addr = source_addr)
+                    short_message=short_message1,
+                    destination_addr=destination_addr,
+                    source_addr=source_addr)
 
             sequence_number = 2
             short_message2 = "\x05\x00\x03\xff\x03\x02 at"
             pdu2 = DeliverSM(sequence_number,
-                    short_message = short_message2,
-                    destination_addr = destination_addr,
-                    source_addr = source_addr)
+                    short_message=short_message2,
+                    destination_addr=destination_addr,
+                    source_addr=source_addr)
 
             sequence_number = 3
             short_message3 = "\x05\x00\x03\xff\x03\x03 you"
             pdu3 = DeliverSM(sequence_number,
-                    short_message = short_message3,
-                    destination_addr = destination_addr,
-                    source_addr = source_addr)
+                    short_message=short_message3,
+                    destination_addr=destination_addr,
+                    source_addr=source_addr)
 
             self.sendPDU(pdu2)
             self.sendPDU(pdu3)
             self.sendPDU(pdu1)
-
 
     def dataReceived(self, data):
         self.datastream += data
@@ -132,7 +125,6 @@ class SmscServer(Protocol):
         while data != None:
             self.handleData(data)
             data = self.popData()
-
 
     def sendPDU(self, pdu):
         data = pdu.get_bin()
@@ -145,5 +137,3 @@ class SmscServerFactory(ServerFactory):
     def buildProtocol(self, addr):
         smsc = SmscServer()
         return smsc
-
-
