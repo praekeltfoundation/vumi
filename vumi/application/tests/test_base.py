6bddb0336444b76a2b9dc22022fb905921af1587
Simon Cross <simon.cross@praekeltfoundation.org>
2011-09-22 14:57:46 +0200

    Complete ApplicationWorker tests for now.

--- vumi/application/tests/test_base.py
+++ vumi/application/tests/test_base.py
@@ -18,13 +18,19 @@ class DummyApplicationWorker(ApplicationWorker):
         self.record = []
 
     def consume_unknown_event(self, event):
-        self.record.append(('unknown', event))
+        self.record.append(('unknown_event', event))
 
     def consume_ack(self, event):
         self.record.append(('ack', event))
 
     def consume_delivery_report(self, event):
         self.record.append(('delivery_report', event))
+
+    def consume_user_message(self, message):
+        self.record.append(('user_message', message))
+
+    def consume_unknown_message(self, message):
+        self.record.append(('unknown_message', message))
 
 
 class FakeUserMessage(TransportUserMessage):
@@ -53,7 +59,7 @@ class TestApplicationWorker(TestCase):
         yield self.worker.stopWorker()
 
     @inlineCallbacks
-    def send(self, msg, routing_suffix='outbound'):
+    def send(self, msg, routing_suffix='inbound'):
         routing_key = "%s.%s" % (self.transport_name, routing_suffix)
         self.broker.publish_message("vumi", routing_key, msg)
         yield self.broker.kick_delivery()
@@ -62,30 +68,39 @@ class TestApplicationWorker(TestCase):
     def send_event(self, event):
         yield self.send(event, 'event')
 
+    def recv(self, routing_suffix='outbound'):
+        routing_key = "%s.%s" % (self.transport_name, routing_suffix)
+        contents = self.broker.get_dispatched("vumi", routing_key)
+        return [Message.from_json(content.body) for content in contents]
+
     @inlineCallbacks
     def test_event_dispatch(self):
         bad_event1 = TransportEvent(event_type='ack',
+                                    sent_message_id='remote-id',
                                     user_message_id='bad-uuid')
         bad_event1['event_type'] = 'eep'
         bad_event2 = FakeUserMessage()
         events = [
             ('ack', TransportEvent(event_type='ack',
+                                   sent_message_id='remote-id',
                                    user_message_id='ack-uuid')),
             ('delivery_report', TransportEvent(event_type='delivery_report',
                                                delivery_status='pending',
                                                user_message_id='dr-uuid')),
-            ('unknown', bad_event1),
-            ('unknown', bad_event2),
+            ('unknown_event', bad_event1),
+            ('unknown_event', bad_event2),
             ]
         for name, event in events:
             yield self.send_event(event)
             self.assertEqual(self.worker.record, [(name, event)])
             del self.worker.record[:]
 
+    @inlineCallbacks
     def test_user_message_dispatch(self):
         messages = [
             ('user_message', FakeUserMessage()),
             ('unknown_message', TransportEvent(event_type='ack',
+                                               sent_message_id='remote-id',
                                                user_message_id='ack-uuid')),
             ]
         for name, message in messages:
@@ -94,4 +109,30 @@ class TestApplicationWorker(TestCase):
             del self.worker.record[:]
 
     def test_reply_to(self):
-        pass
+        msg = FakeUserMessage()
+        self.worker.reply_to(msg, "More!")
+        self.worker.reply_to(msg, "End!", False)
+        replies = self.recv()
+        expecteds = [msg.reply("More!"), msg.reply("End!", False)]
+        for key in ['timestamp', 'message_id']:
+            for msg in expecteds + replies:
+                del msg.payload[key]
+
+        for reply, expected in zip(replies, expecteds):
+            self.assertEqual(reply, expected)
+        self.assertEqual(len(replies), len(expecteds))
+
+    def test_subclassing_api(self):
+        worker = get_stubbed_worker(ApplicationWorker,
+                                    {'transport_name': 'test'})
+        ack = TransportEvent(event_type='ack',
+                             sent_message_id='remote-id',
+                             user_message_id='ack-uuid')
+        dr = TransportEvent(event_type='delivery_report',
+                            delivery_status='pending',
+                            user_message_id='dr-uuid')
+        worker.consume_ack(ack)
+        worker.consume_delivery_report(dr)
+        worker.consume_unknown_event(FakeUserMessage())
+        worker.consume_user_message(FakeUserMessage())
+        worker.consume_unknown_message(ack)
