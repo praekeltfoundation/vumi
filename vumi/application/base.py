62e603ff7e2925c16a93ed3eea168db0e3af39b3
Simon Cross <simon.cross@praekeltfoundation.org>
2011-09-22 11:27:09 +0200

    Refactor to add .event queue.

--- vumi/application/base.py
+++ vumi/application/base.py
@@ -18,32 +18,20 @@ class ApplicationWorker(Worker):
     def startWorker(self):
         log.msg('Starting a %s worker with config: %s'
                 % (self.__class__.__name__,  self.config))
-        self.transport_publisher = yield self.publish_to(
-            '%(transport_name)s.outbound' % self.config)
-        self.transport_consumer = yield self.consume(
-            '%(transport_name)s.inbound' % self.config,
-            self.consume_message)
-        self._handlers = {
-            TransportUserMessage.MESSAGE_TYPE: self.consume_user_message,
-            TransportEvent.MESSAGE_TYPE: self.consume_event,
-            }
         self._event_handlers = {
             'ack': self.consume_ack,
             'delivery_report': self.consume_delivery_report,
             }
+        self.transport_publisher = yield self.publish_to(
+            '%(transport_name)s.outbound' % self.config)
+        self.transport_consumer = yield self.consume(
+            '%(transport_name)s.inbound' % self.config,
+            self.dispatch_user_message)
+        self.transport_consumer = yield self.consume(
+            '%(transport_name)s.event' % self.config,
+            self.dispatch_event)
 
-    def consume_message(self, message):
-        """Dispatch a message."""
-        message_type = message.get('message_type')
-        handler = self._handlers.get(message_type)
-        if handler is None:
-            return self.consume_bad_message(message)
-        return handler(message)
-
-    def consume_bad_message(self, message):
-        log.err("Unexpected message type in message %r" % (message,))
-
-    def consume_event(self, event):
+    def dispatch_event(self, event):
         """Dispatch to event_type specific handlers."""
         event_type = event.get('event_type')
         handler = self._event_handlers.get(event_type)
@@ -54,15 +42,6 @@ class ApplicationWorker(Worker):
     def consume_unknown_event(self, event):
         log.msg("Unknown event type in message %r" % (event,))
 
-    def reply_to(self, original_message, content, continue_session=True,
-                 **kws):
-        reply = original_message.reply(content, continue_session, **kws)
-        self.transport_publisher.publish_message(reply)
-
-    def consume_user_message(self, message):
-        """Respond to user message."""
-        pass
-
     def consume_ack(self, event):
         """Handle an ack message."""
         pass
@@ -70,3 +49,20 @@ class ApplicationWorker(Worker):
     def consume_delivery_report(self, event):
         """Handle a delivery report."""
         pass
+
+    def dispatch_user_message(self, message):
+        if message.get('message_type') != TransportUserMessage.MESSAGE_TYPE:
+            return self.consume_bad_user_message(message)
+        return self.consume_user_message(message)
+
+    def consume_unknown_message(self, message):
+        log.err("Unexpected message type in message %r" % (message,))
+
+    def consume_user_message(self, message):
+        """Respond to user message."""
+        pass
+
+    def reply_to(self, original_message, content, continue_session=True,
+                 **kws):
+        reply = original_message.reply(content, continue_session, **kws)
+        self.transport_publisher.publish_message(reply)
