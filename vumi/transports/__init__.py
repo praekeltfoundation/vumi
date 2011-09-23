afcc13de163f689159924ff94910464bb0f05638
Simon de Haan <simon@praekeltfoundation.org>
2011-09-23 19:46:28 +0200

    typo, missing s

--- vumi/transports/__init__.py
+++ vumi/transports/__init__.py
@@ -6,6 +6,6 @@ This is where all transports that are part of core vumi live.
    Anything in :mod:`vumi.workers` is deprecated and needs to be migrated.
 """
 
-from vumi.transport.base import Transport
+from vumi.transports.base import Transport
 
 __all__ = ['Transport']
