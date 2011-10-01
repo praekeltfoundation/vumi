eba089de2ba9c2e688cb8cdb6f8067b190e147f2
Simon Cross <simon.cross@praekeltfoundation.org>
2011-10-01 10:21:22 +0200

    Add DJANGO_SETTINGS_MODULE to unit tests.

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -7,9 +7,11 @@ find vumi/ -name '*.pyc' -delete
 echo "=== Erasing previous coverage data..."
 coverage erase
 echo "=== Running trial tests..."
+export DJANGO_SETTINGS_MODULE='vumi.webapp.settings'
 coverage run ./ve/bin/trial --reporter=subunit vumi | tee results.txt | subunit2junitxml > test_results.xml
 subunit2pyunit < results.txt
 rm results.txt
+export -n DJANGO_SETTINGS_MODULE
 echo "=== Running django tests..."
 ./manage.py test --with-xunit --xunit-file=django_test_results.xml --cover-package=vumi.webapp
 echo "=== Processing coverage data..."
