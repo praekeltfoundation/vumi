da564716e6f93b31147382a176b96da33f8f84e0
dmaclay <dmaclay@gmail.com>
2011-10-10 11:26:03 +0200

    get rid of camelCase

--- vumi/workers/ikhwezi/tests/test_ikhwezi.py
+++ vumi/workers/ikhwezi/tests/test_ikhwezi.py
@@ -81,11 +81,11 @@ class RedisInputSequenceTest(TestCase):
     def set_completed_text(self, fun):
         self.completed_text = fun(self.quiz['completed']['headertext'])
 
-    def runInputSequence(self, inputs, force_order=None):
+    def run_input_sequence(self, inputs, force_order=None):
         user_in = inputs.pop(0)
-        print ''
-        print self.session_event
-        print user_in
+        #print ''
+        #print self.session_event
+        #print user_in
         ik = self.get_quiz_entry()
         if force_order:
             ik.force_order(force_order)
@@ -95,58 +95,58 @@ class RedisInputSequenceTest(TestCase):
         self.language = ik.language
         self.set_completed_text(ik._)
         self.set_exit_text(ik._)
-        print resp
+        #print resp
         if len(inputs):
-            return self.runInputSequence(inputs)
+            return self.run_input_sequence(inputs)
         return resp
 
-    def finishInputSequence(self, inputs):
-        resp = self.runInputSequence(inputs)
+    def finish_input_sequence(self, inputs):
+        resp = self.run_input_sequence(inputs)
         final_headertext = resp.headertext
         ref_text = self.exit_text
         self.assertTrue(final_headertext.endswith(ref_text))
 
-    def completedInputSequence(self, inputs):
-        resp = self.runInputSequence(inputs)
+    def completed_input_sequence(self, inputs):
+        resp = self.run_input_sequence(inputs)
         final_headertext = resp.headertext
         ref_text = self.completed_text
         self.assertTrue(final_headertext.endswith(ref_text))
 
-    def testAnswerOutOfRange1(self):
+    def test_answer_out_of_range_1(self):
         """
         The 66 is impossible
         """
         inputs = ['*120*112233#', 1, 66, 1, 1, 1, 1, 2]
-        self.finishInputSequence(inputs)
+        self.finish_input_sequence(inputs)
 
-    def testAnswerOutOfRange2(self):
+    def test_answer_out_of_range_2(self):
         """
         The 22 is impossible
         """
         inputs = ['*120*112233#', 22, 2, 2, 2, 2, 2, 2]
-        self.finishInputSequence(inputs)
+        self.finish_input_sequence(inputs)
 
-    def testAnswerWrongType1(self):
+    def test_answer_wrong_type_1(self):
         inputs = ['*120*112233#', 1, "is there a 3rd option?", 1, 1, 1, 1, 2]
-        self.finishInputSequence(inputs)
+        self.finish_input_sequence(inputs)
 
-    def testAnswerWrongType2(self):
+    def test_answer_wrong_type_2(self):
         inputs = ['*120*112233#', 1, '*120*11223344#', 1, 1, 1, 2, 2]
-        self.finishInputSequence(inputs)
+        self.finish_input_sequence(inputs)
 
-    def testAnswerWrongType3(self):
+    def test_answer_wrong_type_3(self):
         """
         Mismatches on Continue question auto continue
         """
         inputs = ['*120*112233#', 1, 1, "huh", 1, 2, 1, 'exit', 1, 2]
-        self.finishInputSequence(inputs)
+        self.finish_input_sequence(inputs)
 
-    def testSequence1(self):
+    def test_sequence_1(self):
         inputs = ['*120*112233#', 'blah', 55, '1', 1, 55, 55, 'blah',
                 '1', 1, 55, 'blah', 55, '1', 2, 2, 2]
-        self.finishInputSequence(inputs)
+        self.finish_input_sequence(inputs)
 
-    def testSequence2(self):
+    def test_sequence_2(self):
         """
         This sequence answers all questions,
         forcing an exit response to the final continure question
@@ -154,26 +154,26 @@ class RedisInputSequenceTest(TestCase):
         inputs = ['*120*112233#', 1, '1', 55, 2, '1', 55, 1, '1',
                 55, 2, 55, '2', 1, '1', 1, '1', 1, 55, '2',
                 55, '1', 1, '1', 55, '2', 'bye', 'no', 1, 2]
-        self.finishInputSequence(inputs)
+        self.finish_input_sequence(inputs)
 
-    def testSequence3(self):
+    def test_sequence_3(self):
         inputs = ['*120*112233#', 'demo', '2', 55, 'demo', 55, 55, 55, 55,
                 '2', 55, 'demo', '2', 55, 55, 'demo', '2', 1, 55,
                 55, '1', 55, '2', 55, '2', 55, '2', 55, '2', 2]
-        self.finishInputSequence(inputs)
+        self.finish_input_sequence(inputs)
 
-    def testSequence4(self):
+    def test_sequence_4(self):
         """
         Check the message on resuming after completion
         """
         inputs = ['*120*112233#', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-        self.finishInputSequence(inputs)
+        self.finish_input_sequence(inputs)
         self.session_event = 'new'
         inputs = ['*120*112233#']
-        self.completedInputSequence(inputs)
+        self.completed_input_sequence(inputs)
 
-    def testWithForcedOrder(self):
+    def test_with_forced_order(self):
         force_order = [
             'demographic1',
             'demographic2',
@@ -201,36 +201,36 @@ class RedisInputSequenceTest(TestCase):
             'continue'
             ]
         inputs = ['*120*112233#']
-        resp = self.runInputSequence(inputs, force_order)
+        resp = self.run_input_sequence(inputs, force_order)
         self.assertEqual(4, len(resp.option_list))
         inputs = [1]
-        resp = self.runInputSequence(inputs)
+        resp = self.run_input_sequence(inputs)
         self.assertEqual(2, len(resp.option_list))
         inputs = [1]
-        resp = self.runInputSequence(inputs)
+        resp = self.run_input_sequence(inputs)
         self.assertEqual(9, len(resp.option_list))
         inputs = [1]
-        resp = self.runInputSequence(inputs)
+        resp = self.run_input_sequence(inputs)
         self.assertEqual(4, len(resp.option_list))
 
         # simulate resume
         inputs = ['*120*112233#']
         self.session_event = 'new'
-        resp = self.runInputSequence(inputs)
+        resp = self.run_input_sequence(inputs)
         self.assertEqual(4, len(resp.option_list))
 
         # and 3 more attempts
         inputs = [1]
         self.session_event = 'new'
-        resp = self.runInputSequence(inputs)
+        resp = self.run_input_sequence(inputs)
         self.assertEqual(2, len(resp.option_list))
         inputs = [1]
         self.session_event = 'new'
-        resp = self.runInputSequence(inputs)
+        resp = self.run_input_sequence(inputs)
         self.assertEqual(2, len(resp.option_list))
         inputs = [None]
         self.session_event = 'new'
-        resp = self.runInputSequence(inputs)
+        resp = self.run_input_sequence(inputs)
         self.assertEqual(0, len(resp.option_list))
 
 
