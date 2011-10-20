fe23015c72e4260e0d5eaf19ee168f83a24a5f86
dmaclay <dmaclay@gmail.com>
2011-10-20 15:57:08 +0200

    should work

--- vumi/dispatchers/simple/dispatcher.py
+++ vumi/dispatchers/simple/dispatcher.py
@@ -1,8 +1,26 @@
 
 from twisted.python import log
+from twisted.internet.defer import inlineCallbacks
 
 from vumi.message import Message, TransportUserMessage
 from vumi.service import Worker
+
+
+class MessageHandler():
+    def __init__(self, worker, queue_name, publisher, publish_keys=[]):
+        self.worker = worker
+        self.queue_name = queue_name
+        self.publisher = publisher
+        self.publish_keys = publish_keys
+        log.msg("SimpleDispatcher forwarding from %s to %s" % (
+            self.queue_name, self.publish_keys))
+
+    def consume_message(self, message):
+        log.msg("SimpleDispatcher consuming on %s: %s" % (
+            self.queue_name,
+            repr(message)))
+        for k in self.publish_keys:
+            self.publisher.publish_message(message, routing_key=k)
 
 
 class SimpleDispatcher(Worker):
@@ -11,31 +29,10 @@ class SimpleDispatcher(Worker):
     def startWorker(self):
         log.msg("Starting SimpleDispatcher with config: %s" % (self.config))
 
-        #self.publisher = yield self.publish_to(self.publish_key)
-        #self.consume(self.consume_key, self.consume_message)
-
-    def consume_message(self, message):
-        log.msg("SimpleDispatcher consuming on %s: %s" % (
-            self.consume_key,
-            repr(message.payload)))
-        user_m = TransportUserMessage(**message.payload)
-        request = user_m.payload['content']
-        msisdn = user_m.payload['from_addr']
-        session_event = user_m.payload.get('session_event')
-        provider = user_m.payload.get('provider')
-        def response_callback(resp):
-            reply = user_m.reply(str(resp))
-            self.publisher.publish_message(reply)
-        ik = IkhweziQuiz(
-                self.config,
-                self.quiz,
-                self.translations,
-                self.ds,
-                msisdn,
-                session_event,
-                provider,
-                request,
-                response_callback)
+        self.publisher = yield self.publish_to("simpledispatcher.fallback")
+        for queue_name, publish_keys in self.config['route_mappings'].items():
+            h = MessageHandler(self, queue_name, self.publisher, publish_keys)
+            yield self.consume(queue_name, h.consume_message)
 
     def stopWorker(self):
         log.msg("Stopping the IkhweziQuizWorker")
