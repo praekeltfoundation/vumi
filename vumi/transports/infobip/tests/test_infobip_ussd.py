5f454c249ab45fa6b75da21375a2afb405a34212
Simon Cross <simon.cross@praekeltfoundation.org>
2011-11-04 17:51:37 +0200

    Untested implementation of the infobip transport.

---
+++ vumi/transports/infobip/tests/test_infobip_ussd.py
@@ -0,0 +1,8 @@
+"""Test for vumi.transport.infobip.infobip_ussd."""
+
+from twisted.trial.unittest import TestCase
+
+from vumi.transports.infobip.infobip_ussd import InfobipUssdTransport
+
+class TestInfobipUssdTransport(TestCase):
+    pass
