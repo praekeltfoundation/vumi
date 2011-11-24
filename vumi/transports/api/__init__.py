d65a5b448bcebc505f11af720e8cd00c94372962
dmaclay <dmaclay@gmail.com>
2011-11-25 01:28:56 +0200

    old style template transport - brittle on matching arg counts

--- vumi/transports/api/__init__.py
+++ vumi/transports/api/__init__.py
@@ -1,5 +1,9 @@
 """API transports to inject messages into VUMI."""
 
-from vumi.transports.api.api import (HttpApiTransport, OldSimpleHttpTransport)
+from vumi.transports.api.api import (HttpApiTransport,
+                                    OldSimpleHttpTransport,
+                                    OldTemplateHttpTransport)
 
-__all__ = ['HttpApiTransport', 'OldSimpleHttpTransport']
+__all__ = ['HttpApiTransport',
+            'OldSimpleHttpTransport',
+            'OldTemplateHttpTransport']
--- vumi/transports/api/api.py
+++ vumi/transports/api/api.py
@@ -1,6 +1,7 @@
 # -*- test-case-name: vumi.transports.api.tests.test_api -*-
 import uuid
 import json
+import re
 
 from zope.interface import implements
 
@@ -159,6 +160,55 @@ class OldSimpleHttpTransport(Transport):
                 from_addr=from_addr,
                 provider='vumi',
                 transport_type='old_simple_http',
+            )
+            return_list.append({
+                "message": message,
+                "to_msisdn": to_msisdn,
+                "from_msisdn": from_msisdn,
+                "message_id": message_id,
+                })
+        return return_list
+
+
+class OldTemplateHttpTransport(OldSimpleHttpTransport):
+
+    def handle_outbound_message(self, message):
+        log.msg("OldTemplateHttpTransport consuming %s" % (message))
+
+    def extract_template_args(self, args, length):
+        template_args = []
+        for i in range(length):
+            template_args.append({})
+        for k, v in args.items():
+            if k.startswith("template_"):
+                for i, x in enumerate(v):
+                    template_args[i][k] = x
+        return template_args
+
+    def handle_raw_inbound_messages(self, request):
+        opener = re.compile('{{ *')
+        closer = re.compile(' *}}')
+        template = request.args.get('template', [None])[0]
+        template = opener.sub('%(template_', template)
+        template = closer.sub(')s', template)
+        to_msisdns = request.args.get('to_msisdn', [None])
+        from_msisdn = request.args.get('from_msisdn', [None])[0]
+        template_args = self.extract_template_args(request.args, len(to_msisdns))
+        return_list = []
+        for i, to_msisdn in enumerate(to_msisdns):
+            message_id = uuid.uuid4().get_hex()
+            message = content = template % template_args[i]
+            to_addr = to_msisdn
+            from_addr = from_msisdn
+            log.msg("OldTemplateHttpTransport sending from %s to %s message \"%s\"" % (
+                from_addr, to_addr, content))
+            self.publish_message(
+                message_id=message_id,
+                content=content,
+                to_addr=to_addr,
+                from_addr=from_addr,
+                provider='vumi',
+                transport_type='old_template_http',
             )
             return_list.append({
                 "message": message,
