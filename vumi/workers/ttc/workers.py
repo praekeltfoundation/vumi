bdbf98d3758d1eb149f58d4ac25fbfb0e8307549
Olivier Vernin <overnin@gmail.com>
2011-11-25 18:54:45 +0300

    fix bug

--- vumi/workers/ttc/workers.py
+++ vumi/workers/ttc/workers.py
@@ -4,7 +4,7 @@ from twisted.python import log
 
 from vumi.application import ApplicationWorker
 
-class TtcGenericWorker(ApplicaitonWorker):
+class TtcGenericWorker(ApplicationWorker):
     
     def consume_user_message(self, message):
         log.msg("User message: %s" % msg['content'])
