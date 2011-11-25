3481f57b07ecc802822109ebd8c076eb1487c2d4
Olivier Vernin <overnin@gmail.com>
2011-11-25 20:43:29 +0300

    add a control queue

--- vumi/workers/ttc/workers.py
+++ vumi/workers/ttc/workers.py
@@ -6,5 +6,20 @@ from vumi.application import ApplicationWorker
 
 class TtcGenericWorker(ApplicationWorker):
     
+    def start_worker(self):
+        super(TtcGenericWorker, self).start_worker();
+        self.control_consumer = yield self.consume(
+            '%(transport_name)s.control' % self.config,
+            self.control,
+            message_class=Message)
+        
+    
     def consume_user_message(self, message):
-        log.msg("User message: %s" % msg['content'])
+        log.msg("User message: %s" % message['content'])
+        
+    def control(self,message):
+        log.msg("Control message!")
+    
+    def dispatch_event(self, message):
+        log.msg("Event message!")
+    
