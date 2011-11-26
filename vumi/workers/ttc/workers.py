165ec63fa12c90f4165ab702e1f38cb426aa8d5d
Olivier Vernin <overnin@gmail.com>
2011-11-26 12:14:48 +0300

    generic work is able to recive a control message from the frontend

--- vumi/workers/ttc/workers.py
+++ vumi/workers/ttc/workers.py
@@ -13,15 +13,21 @@ class TtcGenericWorker(ApplicationWorker):
         super(TtcGenericWorker, self).startWorker()
         self.control_consumer = yield self.consume(
             '%(transport_name)s.control' % self.config,
-            self.control,
+            self.consume_control,
             message_class=Message)
         
     
     def consume_user_message(self, message):
         log.msg("User message: %s" % message['content'])
         
-    def control(self,message):
+    def consume_control(self, message):
         log.msg("Control message!")
+        #data = message.payload['data']
+        program = message.get('program', {})
+        name = program.get('name')
+        group = program.get('group',{})
+        number = group.get('number')
+        log.msg("Control message %s to be add %s" % (number,name))
     
     def dispatch_event(self, message):
         log.msg("Event message!")
