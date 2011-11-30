e7f09fdd4dbf5ed537e428271f73337810285f2c
Simon Cross <simon.cross@praekeltfoundation.org>
2011-11-30 15:53:59 +0200

    Refactor message injection to not block while reading the file and to allow specifying which files to read and write from.

--- vumi/scripts/inject_messages.py
+++ vumi/scripts/inject_messages.py
@@ -2,8 +2,8 @@
 import sys
 import json
 from twisted.python import usage
-from twisted.internet import reactor, task
-from twisted.internet.defer import maybeDeferred, Deferred, inlineCallbacks
+from twisted.internet import reactor, threads
+from twisted.internet.defer import maybeDeferred, inlineCallbacks
 from vumi.message import TransportUserMessage
 from vumi.service import Options, Worker, WorkerCreator
 
@@ -29,22 +29,20 @@ class MessageInjector(Worker):
         self.publisher = yield self.publish_to('%s.inbound' %
                                                 self.transport_name)
         self.publisher.require_bind = False
-        self.reader = task.LoopingCall(self.readline)
-        self.reader.start(0)
 
-    def emit(self, obj):
-        if self.config['verbose']:
-            sys.stdout.write('%s\n' % (obj,))
+    def process_file(self, in_file, out_file=None):
+        return threads.deferToThread(self._process_file_in_thread,
+                                     in_file, out_file)
 
-    def readline(self):
-        line = sys.stdin.readline()
-        if line:
+    def _process_file_in_thread(self, in_file, out_file):
+        for line in in_file:
             line = line.strip()
-            self.emit(line)
-            self.process_line(line)
-        else:
-            self.reader.stop()
-            reactor.stop()
+            self.emit(out_file, line)
+            threads.blockingCallFromThread(reactor, self.process_line, line)
+
+    def emit(self, out_file, obj):
+        if out_file is not None:
+            out_file.write('%s\n' % (obj,))
 
     def process_line(self, line):
         data = {
@@ -62,8 +60,13 @@ def main(options):
         vumi_options[opt] = options.pop(opt)
 
     worker_creator = WorkerCreator(vumi_options)
-    worker_creator.create_worker(
+    worker = worker_creator.create_worker(
         'vumi.scripts.inject_messages.MessageInjector', options)
+
+    in_file = sys.stdin
+    out_file = sys.stdout if vumi_options['verbose'] else None
+
+    return worker.process_file(in_file, out_file)
 
 
 if __name__ == '__main__':
