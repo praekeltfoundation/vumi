7804dd012ddac444a5aa696ffc12048d38b75ef9
Jeremy Thurgood <jeremy@praekelt.com>
2011-12-07 13:48:07 +0200

    Factored the old API out in preparation for serious #59 work.

--- vumi/transports/api/__init__.py
+++ vumi/transports/api/__init__.py
@@ -1,8 +1,8 @@
 """API transports to inject messages into VUMI."""
 
-from vumi.transports.api.api import (HttpApiTransport,
-                                    OldSimpleHttpTransport,
-                                    OldTemplateHttpTransport)
+from vumi.transports.api.api import HttpApiTransport
+from vumi.transports.api.oldapi import (OldSimpleHttpTransport,
+                                        OldTemplateHttpTransport)
 
 __all__ = ['HttpApiTransport',
             'OldSimpleHttpTransport',
