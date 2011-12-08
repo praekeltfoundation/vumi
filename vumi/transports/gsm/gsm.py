800c4b9f6edf0301af65c0acf0e39eb59e730447
Simon de Haan <simon@praekeltfoundation.org>
2011-12-08 12:12:23 +0200

    more work in progress but heading out for the day

--- vumi/transports/gsm/gsm.py
+++ vumi/transports/gsm/gsm.py
@@ -1,8 +1,12 @@
 from twisted.internet.defer import inlineCallbacks, returnValue
 from twisted.internet.threads import deferToThread
 from twisted.internet.task import LoopingCall
+from twisted.python import log
 from vumi.transports.base import Transport
+from vumi.utils import get_deploy_int
+from uuid import uuid4
 import gammu
+import redis
 
 
 class GSMTransport(Transport):
@@ -15,6 +19,8 @@ class GSMTransport(Transport):
 
         transport_name: gsm_transport
         poll_interval: 120 # seconds, defaults to 60
+        country_code: 27 # the international phone number country prefix
+        phone_number: 27761234567 # the phone number of the SIM
         gammu:
             UseGlobalDebugFile: 0,
             DebugFile: ''
@@ -30,15 +36,26 @@ class GSMTransport(Transport):
 
     """
 
+    redis_outbound_queue = 'outbound_queue'
+
     def validate_config(self):
         """
         Make sure the config values are all present
         and are valid
         """
+        self.redis_config = self.config('redis', {})
         self.gammu_config = self.config('gammu')
         self.poll_interval = int(self.config.get('poll_interval', 60))
+        self.country_code = int(self.config.get('country_code'))
+        self.phone_number = self.config.get('phone_number')
+        self.phone = None
 
     def setup_transport(self):
+        dbindex = get_deploy_int(self._amqp_client.vhost)
+        redis_config = self.config.get('redis', {})
+        self.r_server = yield redis.Redis(db=dbindex, **redis_config)
+        self.r_prefix = "%(transport_name)s" % self.config
+
         self.poller = LoopingCall(self.check_for_new_messages)
         self.poller.start(self.receive_and_send_messages, True)
 
@@ -58,16 +75,66 @@ class GSMTransport(Transport):
 
     @inlineCallbacks
     def receive_and_send_messages(self):
-        phone = yield connect_phone()
-        yield disconnect_phone(phone)
+        self.phone = yield connect_phone()
+        yield disconnect_phone(self.phone)
+        self.phone = None
 
+    def noop(self, *args, **kwargs):
+        log.msg('Doing nothing with %s and %s' % (args, kwargs))
 
+    def receive_messages(self, phone):
+        message = phone.GetNextSMS(0, True)
+        if not message:
+            return
+
+        dispatch_map = {
+            'Deliver': self.receive_message,
+            'Status_Report': self.receive_delivery_report,
+        }
+
+        handler = dispatch_map.get(message['Type'], self.noop)
+        handler(message)
+
+    def receive_message(self, message)
+        self.publish_message(
+            to_addr=normalize_msisdn(message['Number'],
+                        country_code=self.country_code),
+            from_addr=normalize_msisdn(self.phone_number,
+                        country_code=self.country_code),
+            content=message['Text'],
+            transport_type='sms',
+            message_id=uuid4(),
+            transport_metadata={
+                # when it was received on the modem
+                'received_at': message['DateTime'],
+                # when it was retrieved from the modem
+                'read_at': message['SMSCDateTime'],
+            })
+
+    @inlineCallbacks
     def teardown_transport(self):
-        pass
+        self.poller.stop()
+        if self.phone:
+
+            def cb_modem_not_connected(failure):
+                failure.trap(gammu.ERR_NOTCONNECTED)
+                log.err()
+
+            deferred = disconnect_phone(self.phone)
+            deferred.addErrback(cb_modem_not_connected)
+            yield deferred
 
     def handle_outbound_message(self, message):
         """
         Send an outbound message out as an SMS
-        via the GSM Modem
+        via the GSM Modem.
+
+        Since we only poll the modem every `poll_interval` seconds
+        there's the possibility of messages being lost when the transport
+        restarts if messages have been ack'd off the queue but
+        haven't been sent out through the phone yet.
+
+        As a result we stash them in redis and whenever we have a connection
+        to the phone we pull them out again and send them off.
         """
-        pass
+        self.r_server.rpush(self.redis_outbound_queue, message.to_json())
