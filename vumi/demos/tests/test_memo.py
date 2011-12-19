c8e08c742c2b0b7cd6da4cce55677127e1f17028
Simon Cross <simon.cross@praekeltfoundation.org>
2011-12-19 17:08:32 +0200

    Add test for messages being sent to different channels and check that only messages for the correct channel are sent.

--- vumi/demos/tests/test_memo.py
+++ vumi/demos/tests/test_memo.py
@@ -22,18 +22,22 @@ class TestMemoWorker(unittest.TestCase):
         yield self.worker.stopWorker()
 
     @inlineCallbacks
-    def send(self, content, channel=None):
+    def send(self, content, from_addr='testnick', channel=None):
         transport_metadata = {}
         if channel is not None:
             transport_metadata['irc_channel'] = channel
         msg = TransportUserMessage(content=content,
-                                   from_addr='testnick', to_addr='+134567',
+                                   from_addr=from_addr,
+                                   to_addr='some-addr',
                                    transport_name='test',
                                    transport_type='fake',
                                    transport_metadata=transport_metadata)
         self.broker.publish_message('vumi', '%s.inbound' % self.transport_name,
                                     msg)
         yield self.broker.kick_delivery()
+
+    def clear_messages(self):
+        self.broker.clear_messages('vumi', '%s.outbound' % self.transport_name)
 
     @inlineCallbacks
     def recv(self, n=0):
@@ -62,4 +66,29 @@ class TestMemoWorker(unittest.TestCase):
         replies = yield self.recv()
         self.assertEqual(replies, [
             ('reply', 'Sure thing, boss.'),
+            ])
+
+    @inlineCallbacks
+    def test_send_memos(self):
+        yield self.send('bot: tell testmemo this is memo 1', channel='#test')
+        yield self.send('bot: tell testmemo this is memo 2', channel='#test')
+        yield self.send('bot: tell testmemo this is a different channel',
+                        channel='#another')
+
+        # replies to setting memos
+        replies = yield self.recv(3)
+        self.clear_messages()
+
+        yield self.send('ping', channel='#test', from_addr='testmemo')
+        replies = yield self.recv(2)
+        self.assertEqual(replies, [
+            ('reply', 'message from testnick: this is memo 1'),
+            ('reply', 'message from testnick: this is memo 2'),
+            ])
+        self.clear_messages()
+
+        yield self.send('ping', channel='#another', from_addr='testmemo')
+        replies = yield self.recv(1)
+        self.assertEqual(replies, [
+            ('reply', 'message from testnick: this is a different channel'),
             ])
