1f578c0d21f4c7e8eeeb6edcd732241d30bcec48
Simon de Haan <simon@praekeltfoundation.org>
2011-12-23 13:58:19 +0200

    relaying to HTTP, using the reply as a response if appropriate HTTP header set

--- vumi/demos/http_relay.py
+++ vumi/demos/http_relay.py
@@ -1,26 +1,34 @@
+# -*- test-case-name: vumi.demos.tests.test_http_relay -*-
+from twisted.python import log
 from twisted.web import http
 from twisted.internet.defer import inlineCallbacks
 from vumi.application.base import ApplicationWorker
-from vumi.message import TransportUserMessage
 from vumi.utils import http_request_full
-from vumi.errors import VumiError, InvalidMessage
+from vumi.errors import VumiError
 from urllib2 import urlparse
 from base64 import b64encode
 
 
-class HTTPRelayError(VumiError): pass
+class HTTPRelayError(VumiError):
+    pass
 
-class HTTPRelay(ApplicationWorker):
+
+class HTTPRelayApplication(ApplicationWorker):
 
     reply_header = 'X-Vumi-HTTPRelay-Reply'
+
+    @inlineCallbacks
+    def startWorker(self, *args, **kwargs):
+        self.validate_config()
+        yield super(HTTPRelayApplication, self).startWorker(*args, **kwargs)
 
     def validate_config(self):
         self.supported_auth_methods = {
             'basic': self.generate_basic_auth_headers,
         }
         self.url = urlparse.urlparse(self.config['url'])
-        self.username = self.url.username
-        self.password =  self.url.password
+        self.username = self.config.get('username', '')
+        self.password = self.config.get('password', '')
         self.http_method = self.config.get('http_method', 'POST')
         self.auth_method = self.config.get('auth_method', 'basic')
         if self.auth_method not in self.supported_auth_methods:
@@ -32,7 +40,7 @@ class HTTPRelay(ApplicationWorker):
         credentials = ':'.join([username, password])
         auth_string = b64encode(credentials.encode('utf-8'))
         return {
-            'Authorization': 'Basic %s' % (auth_string,)
+            'Authorization': ['Basic %s' % (auth_string,)]
         }
 
     @inlineCallbacks
@@ -45,8 +53,13 @@ class HTTPRelay(ApplicationWorker):
 
         response = yield http_request_full(self.url.geturl(),
                             message.to_json(), headers, self.http_method)
+        headers = response.headers
         if response.code == http.OK:
-            header = response.getHeader(self.reply_header) == 'true':
-            content = response.delivered_body.strip()
-            if header and content:
-                self.reply_to(message, content)
+            if headers.hasHeader(self.reply_header):
+                raw_headers = headers.getRawHeaders(self.reply_header)
+                content = response.delivered_body.strip()
+                if (raw_headers[0].lower() == 'true') and content:
+                    self.reply_to(message, content)
+        else:
+            log.err('%s responded with %s' % (self.url.geturl(),
+                                                response.code))
