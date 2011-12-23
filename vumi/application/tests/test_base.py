3cb98e220b78911ef9e0eee48c51ad82a8010a2e
Simon de Haan <simon@praekeltfoundation.org>
2011-12-23 15:47:50 +0200

    pyflakes fix

--- vumi/application/tests/test_base.py
+++ vumi/application/tests/test_base.py
@@ -1,6 +1,5 @@
 from twisted.trial.unittest import TestCase
 from twisted.internet.defer import inlineCallbacks, returnValue
-from vumi.tests.utils import get_stubbed_worker
 
 from vumi.application.base import ApplicationWorker, SESSION_NEW, SESSION_CLOSE
 from vumi.message import TransportUserMessage, TransportEvent
