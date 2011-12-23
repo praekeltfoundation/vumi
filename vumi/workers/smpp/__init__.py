a8036d186be7b984bd8b7280547414972e2051fa
dmaclay <dmaclay@gmail.com>
2011-12-23 17:44:31 +0200

    clean out __init__ so trial behaves & move tests to tests/

--- vumi/workers/smpp/__init__.py
+++ vumi/workers/smpp/__init__.py
@@ -2,10 +2,3 @@
 Old DEPRECATED SMPP workers.
 """
 
-from vumi.workers.smpp.worker import (SMSAckWorker,
-                                         SMSBatchWorker,
-                                         SMSReceiptWorker)
-
-
-__all__ = ['SMSAckWorker', 'SMSBatchWorker',
-           'SMSReceiptWorker']
