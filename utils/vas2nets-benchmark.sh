99e64a137b090947371de72d02e1a14d74fa0b43
Simon de Haan <simon@praekeltfoundation.org>
2012-01-03 16:21:20 +0200

    removing old vas2nets benchmarking scripts

--- utils/vas2nets-benchmark.sh
+++
@@ -1,1 +0,0 @@
-ab -c 50 -g vas2nets.dat -n 10000 "http://127.0.0.1:8123/api/v1/sms/vas2nets/receive/?messageid=1&time=2011.1.1%2000:00:00&destination=20120&sender=321&text=hello%20world&provider=1&keyword=1"
