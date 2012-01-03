3fbb4b8d7786773992a023524fd426942daad213
Simon Cross <simon.cross@praekeltfoundation.org>
2012-01-03 13:30:40 +0200

    Remove IRC demo logger worker (ircarchive has been updated and the logger can now be replaced with vumi.application.http_relay.

--- vumi/demos/ircbot.py
+++ vumi/demos/ircbot.py
@@ -3,67 +3,11 @@
 """Demo workers for constructing a simple IRC bot."""
 
 import re
-import json
 
 from twisted.python import log
 from twisted.internet.defer import inlineCallbacks
 
 from vumi.application import ApplicationWorker
-from vumi.utils import http_request
-
-
-class LoggerWorker(ApplicationWorker):
-    """Logs messages to a URL.
-
-    Configuration
-    -------------
-    transport_name : str
-        Name of the transport.
-    log_server : str
-        URL to post log messages too.
-        E.g. http://ircarchive.appspot.com/bot/
-    """
-
-    def startWorker(self):
-        """Start the worker"""
-        self.log_server_url = self.config['log_server']
-        super(LoggerWorker, self).startWorker()
-
-    @inlineCallbacks
-    def log(self, **kwargs):
-        """Write a message to web logging API."""
-        log.msg("Logging payload: %r" % (kwargs,))
-        headers = {
-            'Content-Type': ['application/json'],
-            }
-        response = yield http_request(self.log_server_url,
-                                      json.dumps(kwargs),
-                                      headers=headers)
-        log.msg("Response: %r" % (response,))
-
-    @inlineCallbacks
-    def consume_user_message(self, msg):
-        """Log message from a user."""
-        nickname = msg.user()
-        transport_metadata = msg['transport_metadata']
-        irc_server = transport_metadata.get('irc_server')
-        irc_channel = transport_metadata.get('irc_channel')
-        irc_command = transport_metadata.get('irc_command', 'PRIVMSG')
-        text = msg['content']
-
-        # only log messages sent to an IRC channel and server
-        if irc_channel is None or irc_server is None:
-            return
-
-        network = irc_server.partition(':')[0]
-
-        if irc_command == 'PRIVMSG':
-            yield self.log(message_type='message', nickname=nickname,
-                           server=network, channel=irc_channel, msg=text)
-        elif irc_command == 'ACTION':
-            yield self.log(message_type='action', server=network,
-                           channel=irc_channel,
-                           msg="* %s %s" % (nickname, text))
 
 
 class MemoWorker(ApplicationWorker):
