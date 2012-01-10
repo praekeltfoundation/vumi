6632e3d6096adf3db71170b46657cf8ecf52bcbe
Simon de Haan <simon@praekeltfoundation.org>
2012-01-10 11:58:07 +0200

    working on #103, not sure how to test this thing

---
+++ vumi/log.py
@@ -0,0 +1,25 @@
+import logging
+from twisted.python import log
+
+observer = log.PythonLoggingObserver()
+observer.start()
+
+def _mk_logger(level, callback):
+    level = level.upper()
+    level_value = getattr(logging, level, None)
+    if not level_value:
+        raise RuntimeError, '%s is not a valid level' % (level,)
+    def logger(msg, **kwargs):
+        defaults = {
+            'logLevel': level_value
+        }
+        defaults.update(kwargs)
+        callback('%s: %s' % (level, msg), **kwargs)
+    return logger
+
+debug = _mk_logger('DEBUG', log.msg)
+info = _mk_logger('INFO', log.msg)
+warning = _mk_logger('WARNING', log.msg)
+error = _mk_logger('ERROR', log.err)
+critical = _mk_logger('CRITICAL', log.err)
+exception = _mk_logger('ERROR', log.err)
