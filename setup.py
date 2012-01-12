39520ea92dcecc128fba5b7ce8f7c1274a63db8e
Simon Cross <simon.cross@praekeltfoundation.org>
2012-01-12 14:31:15 +0200

    Version 0.3.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def remove_externals(requirements):
 
 setup(
     name="vumi",
-    version="0.3.1a",
+    version="0.3.1",
     url='http://github.com/praekelt/vumi',
     license='BSD',
     description="Super-scalable messaging engine for the delivery of SMS, "
