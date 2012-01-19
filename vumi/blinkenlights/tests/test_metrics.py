4255578c5bac80fa3bf6d282aed27f6411a2805b
Jeremy Thurgood <jeremy@praekelt.com>
2012-01-19 12:16:41 +0200

    Comment.

--- vumi/blinkenlights/tests/test_metrics.py
+++ vumi/blinkenlights/tests/test_metrics.py
@@ -95,7 +95,7 @@ class TestMetricManager(TestCase):
     def test_stop_unstarted(self):
         mm = metrics.MetricManager("vumi.test.", 0.1, self.on_publish)
         mm.stop()
-        mm.stop()
+        mm.stop()  # Check that .stop() is idempotent.
 
     @inlineCallbacks
     def test_in_worker(self):
