b1af35403f22a8bffe850a9c0336e2b63cf837fc
Simon de Haan <simon@praekeltfoundation.org>
2012-01-23 16:40:59 +0200

    work with payload instead of json

--- vumi/transports/tests/test_scheduler.py
+++ vumi/transports/tests/test_scheduler.py
@@ -25,8 +25,8 @@ class SchedulerTestCase(TestCase):
         return (scheduled_at, message)
 
     def assertDelivered(self, message):
-        delivered_messages = [TransportUserMessage.from_json(json_message)
-                                for _, json_message in self._delivery_history]
+        delivered_messages = [TransportUserMessage(**payload)
+                                for _, payload in self._delivery_history]
         self.assertIn(message['message_id'],
             [message['message_id'] for message in delivered_messages])
 
@@ -59,7 +59,7 @@ class SchedulerTestCase(TestCase):
         msg = self.mkmsg_in()
         now = time.mktime(datetime(2012, 1, 1).timetuple())
         delta = 10  # seconds from now
-        key, bucket_key = self.scheduler.schedule(delta, msg.to_json(), now)
+        key, bucket_key = self.scheduler.schedule(delta, msg.payload, now)
         self.assertEqual(bucket_key, '%s#%s.%s' % (
             self.scheduler.r_prefix,
             'scheduled_keys',
@@ -78,7 +78,7 @@ class SchedulerTestCase(TestCase):
         msg = self.mkmsg_in()
         now = time.mktime(datetime(2012, 1, 1).timetuple())
         delta = 16  # seconds from now
-        self.scheduler.schedule(delta, msg.to_json(), now)
+        self.scheduler.schedule(delta, msg.payload, now)
         scheduled_time = now + delta + self.scheduler.granularity
         yield self.scheduler.deliver_scheduled(scheduled_time)
         self.assertDelivered(msg)
@@ -89,7 +89,7 @@ class SchedulerTestCase(TestCase):
         for i in range(0, 3):
             msg = self.mkmsg_in(message_id='message_%s' % (i,))
             delta = i * 10
-            key, _ = self.scheduler.schedule(delta, msg.to_json(), now)
+            key, _ = self.scheduler.schedule(delta, msg.payload, now)
             scheduled_time = now + delta + self.scheduler.granularity
             self.assertEqual(set([key]),
                 self.scheduler.get_all_scheduled_keys())
@@ -103,7 +103,7 @@ class SchedulerTestCase(TestCase):
         # been running since 1912
         msg = self.mkmsg_in()
         way_back = time.mktime(datetime(1912, 1, 1).timetuple())
-        scheduled_key, _ = self.scheduler.schedule(0, msg.to_json(), way_back)
+        scheduled_key, _ = self.scheduler.schedule(0, msg.payload, way_back)
         self.assertTrue(scheduled_key)
         now = time.mktime(datetime.now().timetuple())
         yield self.scheduler.deliver_scheduled(now)
@@ -121,7 +121,7 @@ class SchedulerTestCase(TestCase):
         msg = self.mkmsg_in()
         now = time.mktime(datetime.now().timetuple())
         scheduled_time = now + self.scheduler.granularity
-        key, bucket = self.scheduler.schedule(0, msg.to_json(), scheduled_time)
+        key, bucket = self.scheduler.schedule(0, msg.payload, scheduled_time)
         self.assertEqual(len(self.get_pending_messages()), 1)
         self.assertEqual(set([key]),
             self.scheduler.get_all_scheduled_keys())
