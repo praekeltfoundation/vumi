f2792de873882fdda7ced1af9731b54cf95f9214
Simon Cross <simon.cross@praekeltfoundation.org>
2012-02-29 15:49:22 +0200

    Remove exclusion of deleted django webapp migrations module.

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -13,5 +13,5 @@ rm results.txt
 echo "=== Processing coverage data..."
 coverage xml
 echo "=== Checking for PEP-8 violations..."
-pep8 --repeat --exclude='migrations' vumi | tee pep8.txt
+pep8 --repeat vumi | tee pep8.txt
 echo "=== Done."
