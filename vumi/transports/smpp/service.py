87e3f9e452c908500833bd4d47951dae747a591d
dmaclay <dmaclay@gmail.com>
2012-03-02 00:09:37 +0200

    removed more unused code

--- vumi/transports/smpp/service.py
+++ vumi/transports/smpp/service.py
@@ -6,45 +6,17 @@ from vumi.service import Worker, Consumer, Publisher
 from vumi.transports.smpp.clientserver.server import SmscServerFactory
 
 
-class SmppServiceConsumer(Consumer):
-    exchange_name = "vumi"
-    exchange_type = "direct"
-    durable = True
-    auto_delete = False
-    queue_name = "smpp.outbound.dummy"
-    routing_key = "smpp.outbound.dummy"
-
-    def __init__(self):
-        log.msg("Consuming on %s -> %s" % (self.routing_key, self.queue_name))
-
-
-class SmppServicePublisher(Publisher):
-    exchange_name = "vumi"
-    exchange_type = "direct"
-    routing_key = 'smpp.inbound.dummy'
-    durable = False
-    auto_delete = False
-    delivery_mode = 2
-
-
 class SmppService(Worker):
     """
     The SmppService
     """
     test_hook = None
 
-    #def
-
     def set_test_hook(self, test_hook):
         self.test_hook = test_hook
 
     def startWorker(self):
         log.msg("Starting the SmppService")
-        #self.publisher = yield SmppServicePublisher()
-        #self.consumer = yield SmppServiceConsumer()
         # start the Smpp Service
         self.factory = SmscServerFactory(test_hook=self.test_hook)
         self.listening = reactor.listenTCP(self.config['port'], self.factory)
-
-    #def stopWorker(self):
-        #log.msg("Stopping the SmppService")
--- vumi/transports/smpp/tests/test_smpp.py
+++ vumi/transports/smpp/tests/test_smpp.py
@@ -24,9 +24,6 @@ class EsmeClientInitTestcase(TestCase):
 
     def test_esme_init_with_redis(self):
         r_server = redis.Redis("localhost", db=13)
-        #print dir(r_server)
-        #print r_server.delete("vumi.transports.smpp.tests.test_smpp#counter")
-        #print r_server.keys()
         self.esme = ESME(
                 None,
                 r_server,
