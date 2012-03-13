d0488ce8f566106bfc987a683c959947a5d06083
Olivier Vernin <overnin@gmail.com>
2012-03-13 12:49:36 +0300

    issue 211 create a keyword dispatcher

--- vumi/dispatchers/base.py
+++ vumi/dispatchers/base.py
@@ -295,3 +295,48 @@ class UserGroupingRouter(SimpleDispatchRouter):
         group = self.get_group_for_user(msg.user().encode('utf8'))
         app = self.groups[group]
         self.dispatcher.exposed_publisher[app].publish_message(msg)
+
+
+class ContentKeywordRouter(BaseDispatchRouter):
+    """Router that dispatches based on msg content first word also named as the keyw
+    ord in the sms context.
+    
+    :type keyword_mappings: dict
+    :param keyword_mappings:
+        Mapping from application transport names to keyword.
+        If a message's content first word is matching a given keyword,
+        the message is send to the application listenning on the given transport name.
+    
+    """
+    
+    def setup_routing(self):
+        self.mappings = []
+        for name, keyword in self.config['keyword_mappings'].items():
+            self.mappings.append((name, keyword))
+        
+        self.r_server = redis.Redis("localhost", db='0')
+            
+    def dispatch_inbound_message(self, msg):
+        log.msg('Inbound message')
+        keyword = msg['content'].split()[0]
+        for name, application in self.mappings:
+            if (keyword == application):
+                log.msg('Message is routed to %s' % (name,))
+                self.dispatcher.exposed_publisher[name].publish_message(msg)
+    
+    def dispatch_inbound_event(self, msg):
+        log.msg("Inbound event")
+        name = self.r_server.get(msg['user_message_id'])
+        if (not name):
+            log.msg("Error not route back tuple stored in Redis for %s" % (msg['user_message_id'],))
+        try:
+            log.msg('Event is routed to %s' %(name,))
+            self.dispatcher.exposed_publisher[name].publish_message(msg)
+        except:
+            log.msg("Error no publishing route for %s" % (name,))
+        
+    def dispatch_outbound_message(self, msg):
+        log.msg("Outbound message")
+        name = self.config['transport_names'][0]
+        self.dispatcher.transport_publisher[name].publish_message(msg)
+        self.r_server.set(msg['message_id'], msg['transport_name'])
