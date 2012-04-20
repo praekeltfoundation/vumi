ff5a5975480dcf7385bfc06c7af3c2ba6068672b
Simon Cross <simon.cross@praekeltfoundation.org>
2012-04-20 17:14:43 +0200

    Add test skeletons for riak and txriak manager modules.

---
+++ vumi/persist/tests/test_riak_manager.py
@@ -0,0 +1,9 @@
+"""Tests for vumi.persist.riak_manager."""
+
+from twisted.trial.unittest import TestCase
+
+from vumi.persist.riak_manager import RiakManager
+
+
+class TestRiakManager(TestCase):
+    pass
---
+++ vumi/persist/tests/test_txriak_manager.py
@@ -0,0 +1,9 @@
+"""Tests for vumi.persist.txriak_manager."""
+
+from twisted.trial.unittest import TestCase
+
+from vumi.persist.txriak_manager import TxRiakManager
+
+
+class TestTxRiakManager(TestCase):
+    pass
