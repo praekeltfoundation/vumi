a36804f026a56191c56a6171f577cd08bb956e26
Simon Cross <simon.cross@praekeltfoundation.org>
2012-04-20 11:52:18 +0200

    Start implement ForeignKey fields.

--- vumi/persist/model.py
+++ vumi/persist/model.py
@@ -36,20 +36,23 @@ class FieldDescriptor(object):
     """Property for getting and setting fields."""
 
     def __init__(self, key, field):
-        self._key = key
-        self._field = field
+        self.key = key
+        self.field = field
 
     def setup(self, cls):
         pass
 
+    def validate(self, value):
+        self.field.validate(value)
+
     def __get__(self, instance, owner):
         if instance is None:
-            return self._field
-        return instance._data[self._key]
+            return self.field
+        return instance._data[self.key]
 
     def __set__(self, instance, value):
-        self._field.validate(value)
-        instance._data[self._key] = value
+        self.validate(value)
+        instance._data[self.key] = value
 
 
 class Field(object):
@@ -66,6 +69,10 @@ class Field(object):
         Raise an exception if it isn't.
         """
         pass
+
+
+class ValidationError(Exception):
+    """Raised when a value assigned to a field is invalid."""
 
 
 class Model(object):
@@ -192,8 +199,33 @@ class Unicode(Field):
     pass
 
 
-class OneToMany(Field):
-    pass
+class ForeignKeyDescriptor(object):
+    def setup(self, cls):
+        self.cls = cls
+        self.othercls = self.field.othercls
+
+    def validate(self, value):
+        if not isinstance(value, self.othercls):
+            raise ValidationError("Field %r of %r requires a %r" %
+                                  (self.key, self.cls, self.othercls))
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self._field
+        return None  # TODO: get this value from somewhere
+        # return instance._data[self._key]
+
+    def __set__(self, instance, value):
+        self._field.validate(value)
+        # TODO: write this value to a secondary index or something
+
+
+class ForeignKey(Field):
+    descriptor_class = ForeignKeyDescriptor
+
+    def __init__(self, othercls, index=None):
+        self._othercls = othercls
+        self._index = index
 
 
 # TODO: Write an XML RPC server that takes a manager and a list of model
