d9061137775481e38862be5bde4a2398b1f3e17f
Jeremy Thurgood <jeremy@praekelt.com>
2012-06-08 13:39:52 +0200

    Updated Cellulant transport with new redis stuff.

--- vumi/transports/cellulant/cellulant.py
+++ vumi/transports/cellulant/cellulant.py
@@ -1,7 +1,8 @@
 # -*- test-case-name: vumi.transports.cellulant.tests.test_cellulant -*-
 
-import redis
+from twisted.internet.defer import inlineCallbacks
 
+from vumi.persist import SessionManager
 from vumi.transports.httprpc import HttpRpcTransport
 from vumi.message import TransportUserMessage
 from vumi import log
@@ -38,44 +39,42 @@ class CellulantTransport(HttpRpcTransport):
         super(CellulantTransport, self).validate_config()
         self.transport_type = self.config.get('transport_type', 'ussd')
 
+    @inlineCallbacks
     def setup_transport(self):
         super(CellulantTransport, self).setup_transport()
-        self.redis_config = self.config.get('redis', {})
-        self.r_prefix = "vumi.transports.cellulant:%s" % self.transport_name
-        self.r_session_timeout = int(self.config.get("ussd_session_timeout",
-                                                                        600))
-        self.connect_to_redis()
+        r_config = self.config.get('redis', {})
+        r_prefix = "vumi.transports.cellulant:%s" % self.transport_name
+        session_timeout = int(self.config.get("ussd_session_timeout", 600))
+        self.session_manager = yield SessionManager.from_redis_config(
+            r_config, r_prefix, session_timeout)
 
-    # the connection to redis is a seperate method to allow overriding in tests
-    def connect_to_redis(self):
-        self.r_server = redis.Redis(**self.redis_config)
-
-    def r_key(self, msisdn, session):
-        return "%s:%s:%s" % (self.r_prefix, msisdn, session)
+    @inlineCallbacks
+    def teardown_transport(self):
+        yield self.session_manager.stop()
+        yield super(CellulantTransport, self).teardown_transport()
 
     def set_ussd_for_msisdn_session(self, msisdn, session, ussd):
-        self.r_server.set(self.r_key(msisdn, session), ussd)
-        self.r_server.expire(self.r_key(msisdn, session),
-                self.r_session_timeout)
+        return self.session_manager.create_session(
+            "%s:%s" % (msisdn, session), ussd=ussd)
 
     def get_ussd_for_msisdn_session(self, msisdn, session):
-        return self.r_server.get(self.r_key(msisdn, session))
+        d = self.session_manager.load_session("%s:%s" % (msisdn, session))
+        return d.addCallback(lambda s: s.get('ussd', None))
 
+    @inlineCallbacks
     def handle_raw_inbound_message(self, message_id, request):
         op_code = request.args.get('opCode')[0]
         to_addr = None
         if op_code == "BEG":
             to_addr = request.args.get('INPUT')[0]
-            self.set_ussd_for_msisdn_session(
-                    request.args.get('MSISDN')[0],
-                    request.args.get('sessionID')[0],
-                    to_addr,
-                    )
+            yield self.set_ussd_for_msisdn_session(
+                request.args.get('MSISDN')[0],
+                request.args.get('sessionID')[0],
+                to_addr)
         else:
-            to_addr = self.get_ussd_for_msisdn_session(
-                    request.args.get('MSISDN')[0],
-                    request.args.get('sessionID')[0],
-                    )
+            to_addr = yield self.get_ussd_for_msisdn_session(
+                request.args.get('MSISDN')[0],
+                request.args.get('sessionID')[0])
 
         if ((request.args.get('ABORT')[0] not in ('0', 'null'))
             or (op_code == 'ABO')):
