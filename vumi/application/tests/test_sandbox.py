7563be7196bda0137b52a94b2ef37305e1829b95
Simon Cross <simon.cross@praekeltfoundation.org>
2012-06-12 02:46:37 +0200

    Fix bad command test so that the process is consistently killed (this is what the test is supposed to check).

--- vumi/application/tests/test_sandbox.py
+++ vumi/application/tests/test_sandbox.py
@@ -37,7 +37,13 @@ class SandboxTestCase(ApplicationTestCase):
 
     @inlineCallbacks
     def test_bad_command_from_sandbox(self):
-        app = yield self.setup_app('/bin/echo', ['-n', '{}'])
+        app = yield self.setup_app(sys.executable,
+                                   ['-c',
+                                   "import sys, time\n"
+                                   "sys.stdout.write('{}\\n')\n"
+                                   "sys.stdout.flush()\n"
+                                   "time.sleep(5)\n"
+                                   ])
         event = TransportEvent(event_type='ack', user_message_id=1,
                                sent_message_id=1, sandbox_id='sandbox1')
         status = yield app.process_event_in_sandbox(event)
@@ -45,15 +51,9 @@ class SandboxTestCase(ApplicationTestCase):
         self.assertEqual(str(sandbox_err.value).split(' [')[0],
                          "Resource fallback: unknown command 'unknown'"
                          " received from sandbox 'sandbox1'")
-        # There are two possible conditions here:
-        # 1) The process is killed and terminates with signal 9.
-        # 2) The process exits normally before it can be killed and returns
-        #    exit status 0.
-        if status is None:
-            [kill_err] = self.flushLoggedErrors(ProcessTerminated)
-            self.assertTrue('process ended by signal' in str(kill_err.value))
-        else:
-            self.assertEqual(status, 0)
+        self.assertEqual(status, None)
+        [kill_err] = self.flushLoggedErrors(ProcessTerminated)
+        self.assertTrue('process ended by signal' in str(kill_err.value))
 
     @inlineCallbacks
     def test_stderr_from_sandbox(self):
