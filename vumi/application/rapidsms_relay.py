4d38dcf73d97293aff2e3393e72c36cb7ad01740
Simon Cross <simon.cross@praekeltfoundation.org>
2012-07-03 18:04:17 +0200

    Shift towards supporting RapidSMS http backend rather than httprouter.

--- vumi/application/rapidsms_relay.py
+++ vumi/application/rapidsms_relay.py
@@ -1,87 +1,120 @@
 # -*- test-case-name: vumi.application.tests.test_rapidsms_relay -*-
-from urllib2 import urlparse
 from urllib import urlencode
-import json
+from base64 import b64encode
 
-from twisted.internet.defer import inlineCallbacks
-from twisted.internet.task import LoopingCall
+from twisted.web import http
+from twisted.web.resource import Resource
 
 from vumi.application.base import ApplicationWorker
 from vumi.utils import http_request_full
+from vumi.errors import ConfigError
 from vumi import log
 
 
-class RapidSMSRelayApplication(ApplicationWorker):
+class HealthResource(Resource):
+    isLeaf = True
+
+    def render_GET(self, request):
+        request.setResponseCode(http.OK)
+        request.do_not_log = True
+        return 'OK'
+
+
+class SendResource(Resource):
+    isLeaf = True
+
+    def __init__(self, application):
+        self.application = application
+        Resource.__init__(self)
+
+    def render_(self, request):
+        log.msg("Send request: %s" % (request,))
+        request.setHeader("content-type", "application/json")
+        msg = self.application.handle_raw_outbound_message(request)
+        return msg.to_json()
+
+    def render_PUT(self, request):
+        return self.render_(request, "render_PUT")
+
+    def render_GET(self, request):
+        return self.render_(request, "render_GET")
+
+    def render_POST(self, request):
+        return self.render_(request, "render_POST")
+
+
+class RapidSMSRelay(ApplicationWorker):
     """Application that relays messages to RapidSMS.
 
     RapidSMS relay configuration options:
 
     :param str rapidsms_url:
-        URL of the rapidsms instance. The receive API is expected to be
-        at `<rapidsms_url>/router/receive`.
+        URL of the rapidsms http backend.
     """
-
-   #("^router/can_send/(?P<message_id>\d+)/", can_send),
 
     def validate_config(self):
         self.rapidsms_url = self.config['rapidsms_url']
-        self.rapidsms_password = self.config.get('rapidsms_password')
-        self.rapidsms_backend = self.config.get('rapidsms_backend', 'vumi')
-        self.delivered_on_ack = self.config.get('delivered_on_ack', False)
-        self.outbox_poll_period = int(self.config.get('outbox_poll_period',
-                                                      60))
-        self._outbox_polling = LoopingCall(self._poll_outbox)
+        self.web_path = self.config['web_path']
+        self.web_port = int(self.config['web_port'])
+
+        self.supported_auth_methods = {
+            'basic': self.generate_basic_auth_headers,
+        }
+        self.username = self.config.get('username')
+        self.password = self.config.get('password')
+        self.http_method = self.config.get('http_method', 'POST')
+        self.auth_method = self.config.get('auth_method', 'basic')
+        if self.auth_method not in self.supported_auth_methods:
+            raise ConfigError('HTTP Authentication method %s'
+                              ' not supported' % (repr(self.auth_method,)))
+
+    def generate_basic_auth_headers(self, username, password):
+        credentials = ':'.join([username, password])
+        auth_string = b64encode(credentials.encode('utf-8'))
+        return {
+            'Authorization': ['Basic %s' % (auth_string,)]
+        }
+
+    def get_auth_headers(self):
+        if self.username is not None:
+            handler = self.supported_auth_methods.get(self.auth_method)
+            return handler(self.username, self.password)
+        return {}
 
     def setup_application(self):
-        self._outbox_polling.start(self.outbox_poll_period)
+        # start receipt web resource
+        self.web_resource = yield self.start_web_resources(
+            [
+                (SendResource(self), self.web_path),
+                (HealthResource(self), 'health'),
+            ],
+            self.web_port)
 
-    def _poll_outbox(self):
-        d = self._rapidsms_call("router/outbox")
-        d.addCallback(lambda response: json.loads(response.delivered_body))
-        d.addCallback(self._process_outbox)
-        return d
+    def teardown_application(self):
+        yield self.web_resource.loseConnection()
 
-    def _process_outbox(self, outbox):
-        log.info("Polled outbox: %r" % (outbox.get('status', 'No status')))
-        for message in outbox.get('outbox', []):
-            pass
-            # TODO: call .send_to()
-            #return dict(
-            #        id=self.pk,
-            #        contact=self.connection.identity,
-            #        backend=self.connection.backend.name,
-            #        direction=self.direction,
-            #        status=self.status,
-            #        text=self.text,
-            #        date=self.date.isoformat())
+    def handle_raw_outbound_message(self, request):
+        pass
 
-    def _rapidsms_call(self, route, **kw):
-        if self.rapidsms_password is not None:
-            kw['password'] = self.rapidsms_password
-        url = urlparse.urljoin(self.rapidsms_url, route)
-        url = "%s?%s" % (url, urlencode(kw))
-        return http_request_full(url)
+    def _call_rapidsms(self, message):
+        headers = self.get_auth_headers()
+        params = {
+            'sms': message['content'] or '',
+            'sender': message['from_addr'],
+        }
+        url = "%s?%s" % (self.rapidsms_url, urlencode(params))
+        return http_request_full(url, headers=headers)
 
     def consume_user_message(self, message):
-        # rapidsms limits the sender to 20 characters
-        sender = message['from_addr'][:20]
-        content = message['content'] or ''
-        return self._rapidsms_call("router/receive",
-                                   backend=self.rapidsms_backend,
-                                   sender=sender,
-                                   message=content,
-                                   echo=False)
+        return self._call_rapidsms(message)
 
-    def _delivered(self, event):
-        msg_id = event['user_message_id']
-        return self._rapidsms_call("router/delivered",
-                                   message_id=msg_id)
+    def close_session(self, message):
+        return self._call_rapidsms(message)
 
     def consume_ack(self, event):
-        if self.delivered_on_ack:
-            return self._delivered(event)
+        log.info("Acknowledgement received for message %r"
+                 % (event['user_message_id']))
 
     def consume_delivery_report(self, event):
-        if not self.delivered_on_ack:
-            if event['delivery_status'] == 'delivered':
-                return self._delivered(event)
+        log.info("Delivery report received for message %r, status %r"
+                 % (event['user_message_id'], event['delivery_status']))
