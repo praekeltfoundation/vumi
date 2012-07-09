d1cda1b343658d8b13f8a2bdf28f70ecdfa30113
Simon Cross <simon.cross@praekeltfoundation.org>
2012-07-09 16:32:32 +0200

    Add class docstring for AddressTranslationMiddleware.

--- vumi/middleware/address_translator.py
+++ vumi/middleware/address_translator.py
@@ -2,6 +2,27 @@ from vumi.middleware import BaseMiddleware
 
 
 class AddressTranslationMiddleware(BaseMiddleware):
+    """Address translation middleware.
+
+    Used for mapping a set of `to_addr` values in outbound messages to
+    new values. Inbound messages have the inverse mapping applied to
+    their `from_addr` values..  This is useful during debugging,
+    testing and development.
+
+    For example, you might want to make your Gmail address look like
+    an MSISDN to an application to test SMS address handling, for
+    instance. Or you might want to have an outgoing SMS end up at your
+    Gmail account.
+
+    Configuration options:
+
+    :param dict outbound_map:
+        Mapping of old `to_addr` values to new `to_addr` values for
+        outbound messages. Inbound messages have the inverse mapping
+        applied to `from_addr` values. Addresses not in this dictionary
+        are not affected.
+    """
+
     def setup_middleware(self):
         self.outbound_map = self.config.get('outbound_map')
         self.inbound_map = dict((v, k) for k, v in self.outbound_map.items())
