c9e4a05ed2677fd569642e0ef77dd9f63bf3e15f
Simon Cross <simon.cross@praekeltfoundation.org>
2012-07-10 12:10:39 +0200

    Make sub_manager test neater and also check key_separator.

--- vumi/persist/tests/test_redis_base.py
+++ vumi/persist/tests/test_redis_base.py
@@ -6,12 +6,14 @@ from vumi.persist.redis_base import Manager
 
 
 class ManagerTestCase(TestCase):
-    def mk_manager(self, client, key_prefix='test'):
+    def mk_manager(self, client=None, key_prefix='test'):
+        if client is None:
+            client = object()
         return Manager(client, key_prefix)
 
     def test_sub_manager(self):
-        dummy_client = object()
-        manager = self.mk_manager(dummy_client)
+        manager = self.mk_manager()
         sub_manager = manager.sub_manager("foo")
         self.assertEqual(sub_manager._key_prefix, "test#foo")
-        self.assertEqual(sub_manager._client, dummy_client)
+        self.assertEqual(sub_manager._client, manager._client)
+        self.assertEqual(sub_manager._key_separator, manager._key_separator)
