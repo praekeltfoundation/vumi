3e4de1a5c952ae43b94fbd43b7b0ea96ce0754c0
Simon Cross <simon.cross@praekeltfoundation.org>
2012-07-10 09:42:17 +0200

    Add support for async TxRedis.

--- vumi/components/tagpool.py
+++ vumi/components/tagpool.py
@@ -5,7 +5,10 @@
 
 import json
 
+from twisted.internet.defer import returnValue
+
 from vumi.errors import VumiError
+from vumi.persist.redis_base import Manager
 
 
 class TagpoolError(VumiError):
@@ -21,82 +24,96 @@ class TagpoolManager(object):
 
     def __init__(self, redis):
         self.redis = redis
+        self.manager = redis  # TODO: This is a bit of a hack to make the
+                              #       the calls_manager decorator work
 
+    @Manager.calls_manager
     def acquire_tag(self, pool):
-        local_tag = self._acquire_tag(pool)
-        return (pool, local_tag) if local_tag is not None else None
+        local_tag = yield self._acquire_tag(pool)
+        returnValue((pool, local_tag) if local_tag is not None else None)
 
+    @Manager.calls_manager
     def acquire_specific_tag(self, tag):
         pool, local_tag = tag
-        if self._acquire_specific_tag(pool, local_tag):
-            return tag
-        return None
+        acquired = yield self._acquire_specific_tag(pool, local_tag)
+        if acquired:
+            returnValue(tag)
+        returnValue(None)
 
+    @Manager.calls_manager
     def release_tag(self, tag):
         pool, local_tag = tag
-        self._release_tag(pool, local_tag)
+        yield self._release_tag(pool, local_tag)
 
+    @Manager.calls_manager
     def declare_tags(self, tags):
         pools = {}
         for pool, local_tag in tags:
             pools.setdefault(pool, []).append(local_tag)
         for pool, local_tags in pools.items():
-            self._register_pool(pool)
-            self._declare_tags(pool, local_tags)
+            yield self._register_pool(pool)
+            yield self._declare_tags(pool, local_tags)
 
+    @Manager.calls_manager
     def get_metadata(self, pool):
         metadata_key = self._tag_pool_metadata_key(pool)
-        metadata = self.redis.hgetall(metadata_key)
+        metadata = yield self.redis.hgetall(metadata_key)
         metadata = dict((k, json.loads(v)) for k, v in metadata.iteritems())
-        return metadata
+        returnValue(metadata)
 
+    @Manager.calls_manager
     def set_metadata(self, pool, metadata):
         metadata_key = self._tag_pool_metadata_key(pool)
         metadata = dict((k, json.dumps(v)) for k, v in metadata.iteritems())
-        self._register_pool(pool)
-        self.redis.delete(metadata_key)
-        self.redis.hmset(metadata_key, metadata)
+        yield self._register_pool(pool)
+        yield self.redis.delete(metadata_key)
+        yield self.redis.hmset(metadata_key, metadata)
 
+    @Manager.calls_manager
     def purge_pool(self, pool):
         free_list_key, free_set_key, inuse_set_key = self._tag_pool_keys(pool)
         metadata_key = self._tag_pool_metadata_key(pool)
-        in_use_count = self.redis.scard(inuse_set_key)
+        in_use_count = yield self.redis.scard(inuse_set_key)
         if in_use_count:
             raise TagpoolError('%s tags of pool %s still in use.' % (
                                in_use_count, pool))
         else:
-            self.redis.delete(free_set_key)
-            self.redis.delete(free_list_key)
-            self.redis.delete(inuse_set_key)
-            self.redis.delete(metadata_key)
-            self._unregister_pool(pool)
+            yield self.redis.delete(free_set_key)
+            yield self.redis.delete(free_list_key)
+            yield self.redis.delete(inuse_set_key)
+            yield self.redis.delete(metadata_key)
+            yield self._unregister_pool(pool)
 
     def list_pools(self):
         pool_list_key = self._pool_list_key()
         return self.redis.smembers(pool_list_key)
 
+    @Manager.calls_manager
     def free_tags(self, pool):
         _free_list, free_set_key, _inuse_set = self._tag_pool_keys(pool)
-        return [(pool, local_tag) for local_tag in
-                self.redis.smembers(free_set_key)]
+        free_tags = yield self.redis.smembers(free_set_key)
+        returnValue([(pool, local_tag) for local_tag in free_tags])
 
     def _pool_list_key(self):
         return ":".join(["tagpools", "list"])
 
+    @Manager.calls_manager
     def _register_pool(self, pool):
         """Add a pool to list of pools."""
         pool_list_key = self._pool_list_key()
-        self.redis.sadd(pool_list_key, pool)
+        yield self.redis.sadd(pool_list_key, pool)
 
+    @Manager.calls_manager
     def _unregister_pool(self, pool):
         """Remove a pool to list of pools."""
         pool_list_key = self._pool_list_key()
-        self.redis.srem(pool_list_key, pool)
+        yield self.redis.srem(pool_list_key, pool)
 
+    @Manager.calls_manager
     def inuse_tags(self, pool):
         _free_list, _free_set, inuse_set_key = self._tag_pool_keys(pool)
-        return [(pool, local_tag) for local_tag in
-                self.redis.smembers(inuse_set_key)]
+        inuse_tags = yield self.redis.smembers(inuse_set_key)
+        returnValue([(pool, local_tag) for local_tag in inuse_tags])
 
     def _tag_pool_keys(self, pool):
         return tuple(":".join(["tagpools", pool, state])
@@ -105,30 +122,35 @@ class TagpoolManager(object):
     def _tag_pool_metadata_key(self, pool):
         return ":".join(["tagpools", pool, "metadata"])
 
+    @Manager.calls_manager
     def _acquire_tag(self, pool):
         free_list_key, free_set_key, inuse_set_key = self._tag_pool_keys(pool)
-        tag = self.redis.lpop(free_list_key)
+        tag = yield self.redis.lpop(free_list_key)
         if tag is not None:
-            self.redis.smove(free_set_key, inuse_set_key, tag)
-        return tag
+            yield self.redis.smove(free_set_key, inuse_set_key, tag)
+        returnValue(tag)
 
+    @Manager.calls_manager
     def _acquire_specific_tag(self, pool, local_tag):
         free_list_key, free_set_key, inuse_set_key = self._tag_pool_keys(pool)
-        moved = self.redis.smove(free_set_key, inuse_set_key, local_tag)
+        moved = yield self.redis.smove(free_set_key, inuse_set_key, local_tag)
         if moved:
-            self.redis.lrem(free_list_key, local_tag, num=1)
-        return moved
+            yield self.redis.lrem(free_list_key, local_tag, num=1)
+        returnValue(moved)
 
+    @Manager.calls_manager
     def _release_tag(self, pool, local_tag):
         free_list_key, free_set_key, inuse_set_key = self._tag_pool_keys(pool)
-        count = self.redis.smove(inuse_set_key, free_set_key, local_tag)
+        count = yield self.redis.smove(inuse_set_key, free_set_key, local_tag)
         if count == 1:
-            self.redis.rpush(free_list_key, local_tag)
+            yield self.redis.rpush(free_list_key, local_tag)
 
+    @Manager.calls_manager
     def _declare_tags(self, pool, local_tags):
         free_list_key, free_set_key, inuse_set_key = self._tag_pool_keys(pool)
         new_tags = set(local_tags)
-        old_tags = set(self.redis.sunion(free_set_key, inuse_set_key))
+        old_tags = yield self.redis.sunion(free_set_key, inuse_set_key)
+        old_tags = set(old_tags)
         for tag in sorted(new_tags - old_tags):
-            self.redis.sadd(free_set_key, tag)
-            self.redis.rpush(free_list_key, tag)
+            yield self.redis.sadd(free_set_key, tag)
+            yield self.redis.rpush(free_list_key, tag)
--- vumi/components/tests/test_tagpool.py
+++ vumi/components/tests/test_tagpool.py
@@ -5,100 +5,120 @@
 import json
 
 from twisted.trial.unittest import TestCase
+from twisted.internet.defer import inlineCallbacks
 
-from vumi.persist.fake_redis import FakeRedis
+from vumi.persist.txredis_manager import TxRedisManager
 
 from vumi.components.tagpool import TagpoolManager, TagpoolError
 
 
 class TestTagpoolManager(TestCase):
 
+    @inlineCallbacks
     def setUp(self):
-        self.tagpool = TagpoolManager(FakeRedis())
+        redis = yield TxRedisManager.from_config('FAKE_REDIS', 'teststore')
+        self.tagpool = TagpoolManager(redis)
 
     def pool_key_generator(self, pool):
         def tkey(x):
             return ":".join(["tagpools", pool, x])
         return tkey
 
+    @inlineCallbacks
     def test_declare_tags(self):
         tag1, tag2 = ("poolA", "tag1"), ("poolA", "tag2")
-        self.tagpool.declare_tags([tag1, tag2])
-        self.assertEqual(self.tagpool.acquire_tag("poolA"), tag1)
-        self.assertEqual(self.tagpool.acquire_tag("poolA"), tag2)
-        self.assertEqual(self.tagpool.acquire_tag("poolA"), None)
+        yield self.tagpool.declare_tags([tag1, tag2])
+        self.assertEqual((yield self.tagpool.acquire_tag("poolA")), tag1)
+        self.assertEqual((yield self.tagpool.acquire_tag("poolA")), tag2)
+        self.assertEqual((yield self.tagpool.acquire_tag("poolA")), None)
         tag3 = ("poolA", "tag3")
-        self.tagpool.declare_tags([tag2, tag3])
-        self.assertEqual(self.tagpool.acquire_tag("poolA"), tag3)
+        yield self.tagpool.declare_tags([tag2, tag3])
+        self.assertEqual((yield self.tagpool.acquire_tag("poolA")), tag3)
 
+    @inlineCallbacks
     def test_purge_pool(self):
         tag1, tag2 = ("poolA", "tag1"), ("poolA", "tag2")
-        self.tagpool.declare_tags([tag1, tag2])
-        self.tagpool.purge_pool('poolA')
-        self.assertEqual(self.tagpool.acquire_tag('poolA'), None)
+        yield self.tagpool.declare_tags([tag1, tag2])
+        yield self.tagpool.purge_pool('poolA')
+        self.assertEqual((yield self.tagpool.acquire_tag('poolA')), None)
 
+    @inlineCallbacks
     def test_purge_inuse_pool(self):
         tag1, tag2 = ("poolA", "tag1"), ("poolA", "tag2")
-        self.tagpool.declare_tags([tag1, tag2])
-        self.assertEqual(self.tagpool.acquire_tag('poolA'), tag1)
-        self.assertRaises(TagpoolError, self.tagpool.purge_pool, 'poolA')
+        yield self.tagpool.declare_tags([tag1, tag2])
+        self.assertEqual((yield self.tagpool.acquire_tag('poolA')), tag1)
+        try:
+            yield self.tagpool.purge_pool('poolA')
+        except TagpoolError:
+            pass
+        else:
+            self.fail("Expected TagpoolError to be raised.")
 
+    @inlineCallbacks
     def test_list_pools(self):
         tag1, tag2 = ("poolA", "tag1"), ("poolB", "tag2")
-        self.tagpool.declare_tags([tag1, tag2])
-        self.assertEqual(self.tagpool.list_pools(),
+        yield self.tagpool.declare_tags([tag1, tag2])
+        self.assertEqual((yield self.tagpool.list_pools()),
                          set(['poolA', 'poolB']))
 
+    @inlineCallbacks
     def test_acquire_tag(self):
         tkey = self.pool_key_generator("poolA")
         tag1, tag2 = ("poolA", "tag1"), ("poolA", "tag2")
-        self.tagpool.declare_tags([tag1, tag2])
-        self.assertEqual(self.tagpool.acquire_tag("poolA"), tag1)
-        self.assertEqual(self.tagpool.acquire_tag("poolB"), None)
-        self.assertEqual(self.tagpool.redis.lrange(tkey("free:list"),
-                                                   0, -1),
+        yield self.tagpool.declare_tags([tag1, tag2])
+        self.assertEqual((yield self.tagpool.acquire_tag("poolA")), tag1)
+        self.assertEqual((yield self.tagpool.acquire_tag("poolB")), None)
+        redis = self.tagpool.redis
+        self.assertEqual((yield redis.lrange(tkey("free:list"), 0, -1)),
                          ["tag2"])
-        self.assertEqual(self.tagpool.redis.smembers(tkey("free:set")),
+        self.assertEqual((yield redis.smembers(tkey("free:set"))),
                          set(["tag2"]))
-        self.assertEqual(self.tagpool.redis.smembers(tkey("inuse:set")),
+        self.assertEqual((yield redis.smembers(tkey("inuse:set"))),
                          set(["tag1"]))
 
+    @inlineCallbacks
     def test_acquire_specific_tag(self):
         tkey = self.pool_key_generator("poolA")
         tags = [("poolA", "tag%d" % i) for i in range(10)]
         tag5 = tags[5]
-        self.tagpool.declare_tags(tags)
-        self.assertEqual(self.tagpool.acquire_specific_tag(tag5), tag5)
-        self.assertEqual(self.tagpool.acquire_specific_tag(tag5), None)
+        yield self.tagpool.declare_tags(tags)
+        self.assertEqual((yield self.tagpool.acquire_specific_tag(tag5)), tag5)
+        self.assertEqual((yield self.tagpool.acquire_specific_tag(tag5)), None)
         free_local_tags = [t[1] for t in tags]
         free_local_tags.remove("tag5")
-        self.assertEqual(self.tagpool.redis.lrange(tkey("free:list"),
-                                                   0, -1),
+        redis = self.tagpool.redis
+        self.assertEqual((yield redis.lrange(tkey("free:list"), 0, -1)),
                          free_local_tags)
-        self.assertEqual(self.tagpool.redis.smembers(tkey("free:set")),
+        self.assertEqual((yield redis.smembers(tkey("free:set"))),
                          set(free_local_tags))
-        self.assertEqual(self.tagpool.redis.smembers(tkey("inuse:set")),
+        self.assertEqual((yield redis.smembers(tkey("inuse:set"))),
                          set(["tag5"]))
 
+    @inlineCallbacks
     def test_release_tag(self):
         tkey = self.pool_key_generator("poolA")
         tag1, tag2, tag3 = [("poolA", "tag%d" % i) for i in (1, 2, 3)]
-        self.tagpool.declare_tags([tag1, tag2, tag3])
-        self.tagpool.acquire_tag("poolA")
-        self.tagpool.acquire_tag("poolA")
-        self.tagpool.release_tag(tag1)
-        self.assertEqual(self.tagpool.redis.lrange(tkey("free:list"),
-                                                   0, -1),
+        yield self.tagpool.declare_tags([tag1, tag2, tag3])
+        yield self.tagpool.acquire_tag("poolA")
+        yield self.tagpool.acquire_tag("poolA")
+        yield self.tagpool.release_tag(tag1)
+        redis = self.tagpool.redis
+        self.assertEqual((yield redis.lrange(tkey("free:list"), 0, -1)),
                          ["tag3", "tag1"])
-        self.assertEqual(self.tagpool.redis.smembers(tkey("free:set")),
+        self.assertEqual((yield redis.smembers(tkey("free:set"))),
                          set(["tag1", "tag3"]))
-        self.assertEqual(self.tagpool.redis.smembers(tkey("inuse:set")),
+        self.assertEqual((yield redis.smembers(tkey("inuse:set"))),
                          set(["tag2"]))
 
+    @inlineCallbacks
     def test_metadata(self):
         mkey = self.pool_key_generator("poolA")("metadata")
-        mget = lambda field: json.loads(self.tagpool.redis.hget(mkey,
-                                                                field))
+
+        def mget(field):
+            d = self.tagpool.redis.hget(mkey, field)
+            d.addCallback(json.loads)
+            return d
+
         metadata = {
             "transport_type": "sms",
             "default_msg_fields": {
@@ -108,10 +128,12 @@ class TestTagpoolManager(TestCase):
                 },
             },
         }
-        self.tagpool.set_metadata("poolA", metadata)
-        self.assertEqual(self.tagpool.get_metadata("poolA"), metadata)
-        self.assertEqual(mget("transport_type"), "sms")
+        yield self.tagpool.set_metadata("poolA", metadata)
+        self.assertEqual((yield self.tagpool.get_metadata("poolA")),
+                         metadata)
+        self.assertEqual((yield mget("transport_type")), "sms")
 
         short_metadata = {"foo": "bar"}
-        self.tagpool.set_metadata("poolA", short_metadata)
-        self.assertEqual(self.tagpool.get_metadata("poolA"), short_metadata)
+        yield self.tagpool.set_metadata("poolA", short_metadata)
+        self.assertEqual((yield self.tagpool.get_metadata("poolA")),
+                         short_metadata)
