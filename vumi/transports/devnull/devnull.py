80262045beefebfa32f586b1ac664ba5a90a606b
Simon de Haan <simon@praekeltfoundation.org>
2012-09-15 00:52:16 +0200

    docstring

--- vumi/transports/devnull/devnull.py
+++ vumi/transports/devnull/devnull.py
@@ -11,6 +11,24 @@ from twisted.internet.defer import inlineCallbacks
 
 
 class DevNullTransport(Transport):
+    """
+    DevNullTransport for messages that need fake delivery to networks.
+    Useful for testing.
+
+    Configuration parameters:
+
+    :param transport_type str:
+        The transport type to emulate, defaults to sms.
+    :param failure_rate float:
+        How many messages should be treated as failures.
+        Float value between 0.0 and 1.0.
+    :param reply_rate float:
+        For how many messages should we generate a reply?
+        Float value between 0.0 and 1.0.
+    :param reply_copy str:
+        What copy should be sent as the reply, defaults to echo-ing the content
+        of the outbound message.
+    """
 
     def validate_config(self):
         self.transport_type = self.config.get('transport_type', 'sms')
