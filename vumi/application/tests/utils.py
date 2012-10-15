962d80b2d3fa9b1b77666ad544efb9d564d873d4
Jeremy Thurgood <jeremy@praekelt.com>
2012-10-15 16:40:50 +0200

    Reimplement TransportTestCase on top of VumiWorkerTestCase.

--- vumi/application/tests/utils.py
+++ vumi/application/tests/utils.py
@@ -1,42 +1,26 @@
-from datetime import datetime
+from twisted.internet.defer import inlineCallbacks
 
-from twisted.trial.unittest import TestCase
-from twisted.internet.defer import inlineCallbacks, returnValue
-
-from vumi.message import TransportUserMessage, TransportEvent
-from vumi.tests.fake_amqp import FakeAMQPBroker
-from vumi.tests.utils import get_stubbed_worker, PersistenceMixin
+from vumi.tests.utils import VumiWorkerTestCase, PersistenceMixin
 
 
-class ApplicationTestCase(TestCase, PersistenceMixin):
+class ApplicationTestCase(VumiWorkerTestCase, PersistenceMixin):
 
     """
     This is a base class for testing application workers.
 
     """
 
-    # base timeout of 5s for all application tests
-    timeout = 5
-
-    transport_name = "sphex"
-    transport_type = None
     application_class = None
 
     def setUp(self):
-        self._workers = []
-        self._amqp = FakeAMQPBroker()
         self._persist_setUp()
+        super(ApplicationTestCase, self).setUp()
 
     @inlineCallbacks
     def tearDown(self):
-        for worker in self._workers:
-            yield worker.stopWorker()
+        yield super(ApplicationTestCase, self).tearDown()
         yield self._persist_tearDown()
 
-    def rkey(self, name):
-        return "%s.%s" % (self.transport_name, name)
-
-    @inlineCallbacks
     def get_application(self, config, cls=None, start=True):
         """
         Get an instance of a worker class.
@@ -58,98 +42,21 @@ class ApplicationTestCase(TestCase, PersistenceMixin):
         if cls is None:
             cls = self.application_class
         config = self.mk_config(config)
-        config.setdefault('transport_name', self.transport_name)
         if 'send_to' not in config and cls.SEND_TO_TAGS:
             config['send_to'] = {}
             for tag in cls.SEND_TO_TAGS:
                 config['send_to'][tag] = {
                     'transport_name': '%s_outbound' % tag}
-        worker = get_stubbed_worker(cls, config, self._amqp)
-        self._workers.append(worker)
-        if start:
-            yield worker.startWorker()
-        returnValue(worker)
-
-    def mkmsg_in(self, content='hello world', message_id='abc',
-                 to_addr='9292', from_addr='+41791234567', group=None,
-                 session_event=None, transport_type=None,
-                 helper_metadata=None, transport_metadata=None):
-        if transport_type is None:
-            transport_type = self.transport_type
-        if helper_metadata is None:
-            helper_metadata = {}
-        if transport_metadata is None:
-            transport_metadata = {}
-        return TransportUserMessage(
-            from_addr=from_addr,
-            to_addr=to_addr,
-            group=group,
-            message_id=message_id,
-            transport_name=self.transport_name,
-            transport_type=transport_type,
-            transport_metadata=transport_metadata,
-            helper_metadata=helper_metadata,
-            content=content,
-            session_event=session_event,
-            timestamp=datetime.now(),
-            )
-
-    def mkmsg_out(self, content='hello world', message_id='1',
-                  to_addr='+41791234567', from_addr='9292', group=None,
-                  session_event=None, in_reply_to=None,
-                  transport_type=None, transport_metadata=None,
-                  ):
-        if transport_type is None:
-            transport_type = self.transport_type
-        if transport_metadata is None:
-            transport_metadata = {}
-        params = dict(
-            to_addr=to_addr,
-            from_addr=from_addr,
-            group=group,
-            message_id=message_id,
-            transport_name=self.transport_name,
-            transport_type=transport_type,
-            transport_metadata=transport_metadata,
-            content=content,
-            session_event=session_event,
-            in_reply_to=in_reply_to,
-            )
-        return TransportUserMessage(**params)
-
-    def mkmsg_ack(self, user_message_id='1', sent_message_id='abc',
-                  transport_metadata=None):
-        if transport_metadata is None:
-            transport_metadata = {}
-        return TransportEvent(
-            event_type='ack',
-            user_message_id=user_message_id,
-            sent_message_id=sent_message_id,
-            transport_name=self.transport_name,
-            transport_metadata=transport_metadata,
-            )
-
-    def mkmsg_delivery(self, status='delivered', user_message_id='abc',
-                       transport_metadata=None):
-        if transport_metadata is None:
-            transport_metadata = {}
-        return TransportEvent(
-            event_type='delivery_report',
-            transport_name=self.transport_name,
-            user_message_id=user_message_id,
-            delivery_status=status,
-            to_addr='+41791234567',
-            transport_metadata=transport_metadata,
-            )
+        return self.get_worker(config, cls, start)
 
     def get_dispatched_messages(self):
-        return self._amqp.get_messages('vumi', self.rkey('outbound'))
+        return self.get_dispatched_outbound()
 
     def wait_for_dispatched_messages(self, amount):
-        return self._amqp.wait_messages('vumi', self.rkey('outbound'), amount)
+        return self.wait_for_dispatched_outbound(amount)
+
+    def clear_dispatched_messages(self):
+        return self.clear_dispatched_outbound()
 
     def dispatch(self, message, rkey=None, exchange='vumi'):
-        if rkey is None:
-            rkey = self.rkey('inbound')
-        self._amqp.publish_message(exchange, rkey, message)
-        return self._amqp.kick_delivery()
+        return self.dispatch_inbound(message, rkey, exchange)
