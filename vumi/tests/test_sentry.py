0e74fb5f09c86482384bafd968aeb4d497e3d6b8
Simon Cross <hodgestar@gmail.com>
2012-10-23 23:15:25 +0200

    Add tests for SentryLoggerService and vumi_raven_client.

--- vumi/tests/test_sentry.py
+++ vumi/tests/test_sentry.py
@@ -1,14 +1,17 @@
 """Tests for vumi.sentry."""
 
 import logging
+import base64
+import json
 
 from twisted.trial.unittest import TestCase
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import inlineCallbacks, Deferred
 from twisted.web import http
 from twisted.python.failure import Failure
 
-from vumi.tests.utils import MockHttpServer, LogCatcher
-from vumi.sentry import quiet_get_page, SentryLogObserver
+from vumi.tests.utils import MockHttpServer, LogCatcher, import_skip, mocking
+from vumi.sentry import (quiet_get_page, SentryLogObserver, vumi_raven_client,
+                         SentryLoggerService)
 
 
 class TestQuietGetPage(TestCase):
@@ -39,12 +42,16 @@ class DummySentryClient(object):
     def __init__(self):
         self.exceptions = []
         self.messages = []
+        self.teardowns = 0
 
     def captureMessage(self, *args, **kwargs):
         self.messages.append((args, kwargs))
 
     def captureException(self, *args, **kwargs):
         self.exceptions.append((args, kwargs))
+
+    def teardown(self):
+        self.teardowns += 1
 
 
 class TestSentryLogObserver(TestCase):
@@ -82,5 +89,58 @@ class TestSentryLogObserver(TestCase):
         ])
 
 
+class TestSentryLoggerSerivce(TestCase):
+
+    def setUp(self):
+        import vumi.sentry
+        self.client = DummySentryClient()
+        self.patch(vumi.sentry, 'vumi_raven_client', lambda dsn: self.client)
+        self.service = SentryLoggerService("http://example.com/")
+
+    @inlineCallbacks
+    def test_stop_not_running(self):
+        yield self.service.stopService()
+        self.assertFalse(self.service.running)
+
+    @inlineCallbacks
+    def test_start_stop(self):
+        self.assertFalse(self.service.registered())
+        self.assertEqual(self.client.teardowns, 0)
+        yield self.service.startService()
+        self.assertTrue(self.service.registered())
+        yield self.service.stopService()
+        self.assertFalse(self.service.registered())
+        self.assertEqual(self.client.teardowns, 1)
+
+
 class TestRavenUtilityFunctions(TestCase):
-    pass
+
+    def setUp(self):
+        try:
+            import raven
+        except ImportError, e:
+            import_skip(e, 'raven')
+
+    def mk_sentry_dsn(self):
+        proj_user = "4c96ae4ca518483192dd9917c03847c4"
+        proj_key = "05d9515b5c504cc7bf180597fd6f67"
+        proj_no = 2
+        host, port = "example.com", "30000"
+        dsn = "http://%s:%s@%s:%s/%s" % (proj_user, proj_key, host, port,
+                                         proj_no)
+        return dsn
+
+    def parse_call(self, sentry_call):
+        postdata = sentry_call.kwargs['postdata']
+        return json.loads(base64.b64decode(postdata).decode('zlib'))
+
+    def test_vumi_raven_client_capture_message(self):
+        dsn = self.mk_sentry_dsn()
+        mock_page = mocking(quiet_get_page)
+        mock_page.return_value = Deferred()
+        with mock_page:
+            client = vumi_raven_client(dsn)
+            client.captureMessage("my message")
+        [sentry_call] = mock_page.history
+        sentry_data = self.parse_call(sentry_call)
+        self.assertEqual(sentry_data['message'], "my message")
