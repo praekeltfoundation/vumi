51cc1df39a53ef26d36ff8d65aa690f08b57dd99
Simon Cross <hodgestar@gmail.com>
2012-10-23 18:00:18 +0200

    Add tests for SentryLogObserver.

--- vumi/tests/test_sentry.py
+++ vumi/tests/test_sentry.py
@@ -1,11 +1,14 @@
 """Tests for vumi.sentry."""
 
+import logging
+
 from twisted.trial.unittest import TestCase
 from twisted.internet.defer import inlineCallbacks
 from twisted.web import http
+from twisted.python.failure import Failure
 
 from vumi.tests.utils import MockHttpServer, LogCatcher
-from vumi.sentry import quiet_get_page
+from vumi.sentry import quiet_get_page, SentryLogObserver
 
 
 class TestQuietGetPage(TestCase):
@@ -32,8 +35,51 @@ class TestQuietGetPage(TestCase):
         self.assertEqual(result, "Hello")
 
 
+class DummySentryClient(object):
+    def __init__(self):
+        self.exceptions = []
+        self.messages = []
+
+    def captureMessage(self, *args, **kwargs):
+        self.messages.append((args, kwargs))
+
+    def captureException(self, *args, **kwargs):
+        self.exceptions.append((args, kwargs))
+
+
 class TestSentryLogObserver(TestCase):
-    pass
+    def setUp(self):
+        self.client = DummySentryClient()
+        self.obs = SentryLogObserver(self.client)
+
+    def test_level_for_event(self):
+        for expected_level, event in [
+            (logging.WARN, {'logLevel': logging.WARN}),
+            (logging.ERROR, {'isError': 1}),
+            (logging.INFO, {}),
+        ]:
+            self.assertEqual(self.obs.level_for_event(event), expected_level)
+
+    def test_logger_for_event(self):
+        self.assertEqual(self.obs.logger_for_event({'system': 'foo,bar'}),
+                         'foo,bar')
+        self.assertEqual(self.obs.logger_for_event({}), 'unknown')
+
+    def test_log_failure(self):
+        e = ValueError("foo error")
+        f = Failure(e)
+        self.obs({'failure': f, 'system': 'test.log'})
+        self.assertEqual(self.client.exceptions, [
+            (((type(e), e, None),),
+             {'data': {'level': 20, 'logger': 'test.log'}}),
+        ])
+
+    def test_log_message(self):
+        self.obs({'message': ["a"], 'system': 'test.log'})
+        self.assertEqual(self.client.messages, [
+            (('a',),
+             {'data': {'level': 20, 'logger': 'test.log'}})
+        ])
 
 
 class TestRavenUtilityFunctions(TestCase):
