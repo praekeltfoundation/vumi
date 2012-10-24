14ca327d3a63caf37d704c1c96a261f3e0759ea7
Simon de Haan <simon@praekeltfoundation.org>
2012-10-25 00:11:43 +0200

    adding nack to TransportEvent

--- vumi/message.py
+++ vumi/message.py
@@ -291,6 +291,7 @@ class TransportEvent(TransportMessage):
     # map of event_types -> extra fields
     EVENT_TYPES = {
         'ack': {'sent_message_id': lambda v: v is not None},
+        'nack': {'sent_message_id': lambda v: v is not None},
         'delivery_report': {
             'delivery_status': lambda v: v in TransportEvent.DELIVERY_STATUSES,
             },
