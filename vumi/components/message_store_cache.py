9d90339a4a19d6c4274042965147e3c5dc748fdc
Simon de Haan <simon@praekeltfoundation.org>
2012-10-24 06:48:04 +0200

    tests for add_outbound_message and get_outbound_message_keys

--- vumi/components/message_store_cache.py
+++ vumi/components/message_store_cache.py
@@ -1,5 +1,10 @@
 # -*- test-case-name: vumi.components.tests.test_message_store_cache -*-
 # -*- coding: utf-8 -*-
+import time
+
+from twisted.internet.defer import returnValue
+
+from vumi.persist.redis_base import Manager
 
 
 class MessageStoreCache(object):
@@ -7,9 +12,13 @@ class MessageStoreCache(object):
     A helper class to provide a view on information in the message store
     that is difficult to query straight from riak.
     """
+    BATCH_KEY = 'batch_id'
+    OUTBOUND_KEY = 'outbound'
 
     def __init__(self, redis):
-        self.redis = redis
+        # Also store as manager this the @Manager.calls_manager decorator
+        # expects it to be called as such.
+        self.redis = self.manager = redis
 
     def reconcile(self, message_store, batch_id):
         """
@@ -17,14 +26,42 @@ class MessageStoreCache(object):
         """
         pass
 
+    def key(self, *args):
+        return ':'.join([unicode(a) for a in args])
+
+    def batch_key(self, batch_id, *args):
+        return self.key(self.BATCH_KEY, batch_id, *args)
+
+    def outbound_key(self, batch_id):
+        return self.batch_key(batch_id, self.OUTBOUND_KEY)
+
+    def list_batch_ids(self):
+        """
+        Return a list of known batch_ids
+        """
+
+    def get_timestamp(self, datetime):
+        """
+        Return a timestamp value for a datetime value.
+        """
+        return time.mktime(datetime.timetuple())
+
     def add_outbound_message(self, batch_id, msg):
         """
         Add an outbound message to the cache for the given batch_id
         """
-        pass
+        return self.add_outbound_message_key(batch_id, msg['message_id'],
+            self.get_timestamp(msg['timestamp']))
 
+    @Manager.calls_manager
     def add_outbound_message_key(self, batch_id, message_key, timestamp):
-        pass
+        """
+        Add a message key, weighted with the timestamp to the batch_id.
+        """
+        yield self.redis.sadd(self.batch_key(batch_id))
+        yield self.redis.zadd(self.outbound_key(batch_id), **{
+            message_key: timestamp,
+        })
 
     def add_event(self, batch_id, event):
         """
@@ -41,27 +78,29 @@ class MessageStoreCache(object):
     def add_inbound_message_key(self, batch_id, message_key, timestamp):
         pass
 
-    def get_to_addrs(self):
+    def get_to_addrs(self, batch_id):
         """
         Return a set of unique to_addrs addressed in this batch ordered
         by the most recent timestamp.
         """
         pass
 
-    def get_to_addrs_count(self):
+    def get_to_addrs_count(self, batch_id):
         """
         Return count of the unique to_addrs in this batch.
         """
         pass
 
-    def get_inbound_message_keys(self):
+    def get_inbound_message_keys(self, batch_id):
         """
         Return a list of keys ordered according to their timestamps
         """
         pass
 
-    def get_outbound_message_keys(self):
+    @Manager.calls_manager
+    def get_outbound_message_keys(self, batch_id):
         """
         Return a list of keys ordered according to their timestamps.
         """
-        pass
+        keys = yield self.redis.zrange(self.outbound_key(batch_id), 0, -1)
+        returnValue(keys)
--- vumi/components/tests/test_message_store_cache.py
+++ vumi/components/tests/test_message_store_cache.py
@@ -2,8 +2,11 @@
 
 """Tests for vumi.components.message_store_cache."""
 
+from datetime import datetime, timedelta
+
 from twisted.internet.defer import inlineCallbacks
 
+from vumi.message import TransportMessage
 from vumi.application.tests.test_base import ApplicationTestCase
 from vumi.components import MessageStore
 from vumi.components.message_store_cache import MessageStoreCache
@@ -17,7 +20,31 @@ class TestMessageStoreCache(ApplicationTestCase):
         self.redis = yield self.get_redis_manager()
         self.manager = yield self.get_riak_manager()
         self.store = yield MessageStore(self.manager, self.redis)
-        self.store_cache = MessageStoreCache(self.redis)
+        self.cache = MessageStoreCache(self.redis)
+        self.batch_id = 'a-batch-id'
 
-    def test_something(self):
-        self.assertTrue(True)
+    @inlineCallbacks
+    def test_add_outbound_message(self):
+        msg = self.mkmsg_out(to_addr='to@addr.com',
+            message_id=TransportMessage.generate_id())
+        self.cache.add_outbound_message(self.batch_id, msg)
+        [msg_key] = yield self.cache.get_outbound_message_keys(self.batch_id)
+        self.assertEqual(msg_key, msg['message_id'])
+
+    @inlineCallbacks
+    def test_get_outbound_message_keys(self):
+        messages = []
+        for i in range(10):
+            msg = self.mkmsg_out(to_addr='to@addr.com',
+                message_id=TransportMessage.generate_id())
+            # insert them with starting with newest first
+            msg['timestamp'] = datetime.now() - timedelta(seconds=i)
+            self.cache.add_outbound_message(self.batch_id, msg)
+            messages.append(msg)
+
+        # make sure we get keys back ordered according to timestamp, which
+        # means the reverse of how we put them in.
+        keys = yield self.cache.get_outbound_message_keys(self.batch_id)
+        self.assertEqual(len(keys), 10)
+        self.assertEqual(keys, list(reversed(
+            [m['message_id'] for m in messages])))
