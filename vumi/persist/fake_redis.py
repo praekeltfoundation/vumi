ebb6ce2f214d167ac9380d7ad7f8c8e9fd9cc9c8
Simon de Haan <simon@praekeltfoundation.org>
2012-11-05 12:40:52 +0200

    do not encode twice

--- vumi/persist/fake_redis.py
+++ vumi/persist/fake_redis.py
@@ -200,7 +200,6 @@ class FakeRedis(object):
 
     @maybe_async
     def sadd(self, key, *values):
-        values = map(self._encode, values)
         sval = self._data.setdefault(key, set())
         old_len = len(sval)
         sval.update(map(self._encode, values))
