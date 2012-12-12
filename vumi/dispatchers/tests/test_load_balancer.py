45755b0649d479447a520c6ffcba2d0fbb1c7a35
Simon Cross <hodgestar@gmail.com>
2012-12-12 17:29:48 +0200

    Remove some unnecessary msg making methods.

--- vumi/dispatchers/tests/test_load_balancer.py
+++ vumi/dispatchers/tests/test_load_balancer.py
@@ -17,7 +17,7 @@ class TestLoadBalancingRouter(VumiWorkerTestCase):
                 "transport_1",
                 "transport_2",
             ],
-            "exposed_names": ["muxed"],
+            "exposed_names": ["round_robin"],
             "router_class": ("vumi.dispatchers.load_balancer."
                              "LoadBalancingRouter"),
         }
@@ -30,39 +30,26 @@ class TestLoadBalancingRouter(VumiWorkerTestCase):
         yield super(TestLoadBalancingRouter, self).tearDown()
         yield self.router.teardown_routing()
 
-    def mkmsg_in_mux(self, content, from_addr, transport_name):
-        return self.mkmsg_in(transport_name=transport_name, content=content,
-                             from_addr=from_addr)
-
-    def mkmsg_ack_mux(self, from_addr, transport_name):
-        ack = self.mkmsg_ack(transport_name=transport_name)
-        ack['from_addr'] = from_addr
-        return ack
-
-    def mkmsg_out_mux(self, content, from_addr):
-        return self.mkmsg_out(transport_name='muxed', content=content,
-                              from_addr=from_addr)
-
     def test_inbound_message_routing(self):
-        msg1 = self.mkmsg_in_mux('mux 1', 'thing1@muxme', 'transport_1')
+        msg1 = self.mkmsg_in(content='msg 1', transport_name='transport_1')
         self.router.dispatch_inbound_message(msg1)
-        msg2 = self.mkmsg_in_mux('mux 2', 'thing2@muxme', 'transport_2')
+        msg2 = self.mkmsg_in(content='msg 2', transport_name='transport_2')
         self.router.dispatch_inbound_message(msg2)
         publishers = self.dispatcher.exposed_publisher
-        self.assertEqual(publishers['muxed'].msgs, [msg1, msg2])
+        self.assertEqual(publishers['round_robin'].msgs, [msg1, msg2])
 
     def test_inbound_event_routing(self):
-        msg1 = self.mkmsg_ack_mux('thing1@muxme', 'transport_1')
+        msg1 = self.mkmsg_ack(transport_name='transport_1')
         self.router.dispatch_inbound_event(msg1)
-        msg2 = self.mkmsg_ack_mux('thing2@muxme', 'transport_2')
+        msg2 = self.mkmsg_ack(transport_name='transport_2')
         self.router.dispatch_inbound_event(msg2)
         publishers = self.dispatcher.exposed_event_publisher
-        self.assertEqual(publishers['muxed'].msgs, [msg1, msg2])
+        self.assertEqual(publishers['round_robin'].msgs, [msg1, msg2])
 
     def test_outbound_message_routing(self):
-        msg1 = self.mkmsg_out_mux('mux 1', 'thing1@muxme')
+        msg1 = self.mkmsg_out(content='msg 1')
         self.router.dispatch_outbound_message(msg1)
-        msg2 = self.mkmsg_out_mux('mux 2', 'thing2@muxme')
+        msg2 = self.mkmsg_out(content='msg 2')
         self.router.dispatch_outbound_message(msg2)
         publishers = self.dispatcher.transport_publisher
         self.assertEqual(publishers['transport_1'].msgs, [msg1])
