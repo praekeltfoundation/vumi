ef180ac4edfd1523d26fdbb28e6dc32b9d52692c
Simon de Haan <simon@praekeltfoundation.org>
2013-02-14 11:55:43 +0200

    test_inbound_begin

---
+++ vumi/transports/airtel/airtel.py
@@ -0,0 +1,205 @@
+# -*- test-case-name: vumi.transports.airtel.tests.test_airtel -*-
+
+import json
+
+from twisted.internet.defer import inlineCallbacks
+
+from vumi.transports.httprpc import HttpRpcTransport
+from vumi.components import SessionManager
+from vumi.message import TransportUserMessage
+from vumi import log
+
+
+class AirtelUSSDTransport(HttpRpcTransport):
+    """
+    Client implementation for the Comviva Flares HTTP Pull API.
+    Based on Flares 1.5.0, document version 1.2.0
+
+    :param str web_path:
+        The HTTP path to listen on.
+    :param int web_port:
+        The HTTP port
+    :param str transport_name:
+        The name this transport instance will use to create its queues
+    :param dict redis_manager:
+        The configuration parameters for connecting to Redis.
+    :param str airtel_username:
+        The username for this transport
+    :param str airtel_password:
+        The password for this transport
+    :param bool airtel_charge:
+        Whether or not to charge for the responses sent. Defaults to False.
+    :param int airtel_charge_amount:
+        How much to charge. Defaults to Zero.
+    """
+
+    content_type = 'text/plain; charset=utf-8'
+    EXPECTED_CLEANUP_FIELDS = set(['userid', 'password', 'MSISDN',
+                                    'clean', 'status'])
+    EXPECTED_USSD_FIELDS = set(['userid', 'password', 'MSISDN',
+                                    'MSC', 'input'])
+
+    def validate_config(self):
+        super(AirtelUSSDTransport, self).validate_config()
+        self.transport_type = self.config.get('transport_type', 'ussd')
+        self.r_prefix = "vumi.transports.safaricom:%s" % self.transport_name
+        self.redis_config = self.config.get('redis_manager', {})
+        self.r_session_timeout = int(self.config.get("ussd_session_timeout",
+                                                                        600))
+        self.airtel_username = self.config['airtel_username']
+        self.airtel_password = self.config['airtel_password']
+        self.airtel_charge = ('Y' if self.config.get('airtel_charge', False)
+                                else 'N')
+        self.airtel_charge_amount = int(
+                                    self.config.get('airtel_charge_amount', 0))
+
+    @inlineCallbacks
+    def setup_transport(self):
+        super(AirtelUSSDTransport, self).setup_transport()
+        self.session_manager = yield SessionManager.from_redis_config(
+            self.redis_config, self.r_prefix, self.r_session_timeout)
+
+    def is_cleanup(self, request):
+        return 'cleanup' in request.args
+
+    def get_field_values(self, request, expected_set):
+        values = {}
+        errors = {}
+        for field in request.args:
+            if field not in expected_set:
+                errors.setdefault('unexpected_parameter', []).append(field)
+            else:
+                values[field] = str(request.args.get(field)[0])
+        for field in expected_set:
+            if field not in values:
+                errors.setdefault('missing_parameter', []).append(field)
+        return values, errors
+
+    def is_authenticated(self, request):
+        return (request.args['userid'] == [self.airtel_username] and
+                request.args['password'] == [self.airtel_password])
+
+    def handle_raw_inbound_message(self, message_id, request):
+        if self.is_cleanup(request):
+            return self.handle_cleanup_request(message_id, request)
+        return self.handle_ussd_request(message_id, request)
+
+    @inlineCallbacks
+    def handle_cleanup_request(self, message_id, request):
+        values, errors = self.get_field_values(request,
+                                                self.EXPECTED_CLEANUP_FIELDS)
+        if errors:
+            log.msg('Unhappy incoming message: %s' % (errors,))
+            self.finish_request(message_id, json.dumps(errors), code=400)
+            return
+
+        if not self.is_authenticated(request):
+            log.msg('Invalid authentication credentials: %s:%s' % (
+                values['userid'], values['password']))
+            self.finish_request(message_id, 'Forbidden', code=403)
+            return
+
+        session_id = values['MSISDN']
+        session = yield self.session_manager.load_session(session_id)
+        if not session:
+            log.warning('Received cleanup for unknown session: %s' % (
+                            session_id,))
+            self.finish_request(message_id, 'Unknown Session', code=200)
+            return
+
+        from_addr = values['MSISDN']
+        to_addr = session['to_addr']
+        session_event = TransportUserMessage.SESSION_CLOSE
+
+        yield self.session_manager.clean_session(session_id)
+        yield self.publish_message(
+                message_id=message_id,
+                content='',
+                to_addr=to_addr,
+                from_addr=from_addr,
+                provider='airtel',
+                session_event=session_event,
+                transport_type=self.transport_type,
+                transport_metadata={
+                    'airtel': {
+                        'MSC': values['MSC'],
+                        'clean': values['clean'],
+                        'status': values['status'],
+                    },
+                })
+
+    @inlineCallbacks
+    def handle_ussd_request(self, message_id, request):
+        values, errors = self.get_field_values(request,
+                                                self.EXPECTED_USSD_FIELDS)
+        if errors:
+            log.msg('Unhappy incoming message: %s' % (errors,))
+            self.finish_request(message_id, json.dumps(errors), code=400)
+            return
+
+        if not self.is_authenticated(request):
+            log.msg('Invalid authentication credentials: %s:%s' % (
+                values['userid'], values['password']))
+            self.finish_request(message_id, 'Forbidden', code=403)
+            return
+
+        session_id = values['MSISDN']
+        from_addr = values['MSISDN']
+        ussd_params = values['input']
+
+        session = yield self.session_manager.load_session(session_id)
+        if session:
+            to_addr = session['to_addr']
+            last_ussd_params = session['last_ussd_params']
+            new_params = ussd_params[len(last_ussd_params):]
+            if new_params:
+                if last_ussd_params:
+                    content = new_params[1:]
+                else:
+                    content = new_params
+            else:
+                content = ''
+
+            session['last_ussd_params'] = ussd_params
+            yield self.session_manager.save_session(session_id, session)
+            session_event = TransportUserMessage.SESSION_RESUME
+        else:
+            to_addr = ussd_params
+            yield self.session_manager.create_session(session_id,
+                from_addr=from_addr, to_addr=to_addr,
+                last_ussd_params=ussd_params)
+            session_event = TransportUserMessage.SESSION_NEW
+            content = ''
+
+        yield self.publish_message(
+                message_id=message_id,
+                content=content,
+                to_addr=to_addr,
+                from_addr=from_addr,
+                provider='airtel',
+                session_event=session_event,
+                transport_type=self.transport_type,
+                transport_metadata={
+                    'airtel': {
+                        'MSC': values['MSC'],
+                    },
+                })
+
+    def handle_outbound_message(self, message):
+        if message.payload.get('in_reply_to') and 'content' in message.payload:
+            if message['session_event'] == TransportUserMessage.SESSION_CLOSE:
+                free_flow = 'FB'
+            else:
+                free_flow = 'FC'
+            self.finish_request(message['in_reply_to'],
+                message['content'].encode('utf-8'), code=200, headers={
+                'Freeflow': [free_flow],
+                'charge': [self.airtel_charge],
+                'amount': [self.airtel_charge_amount],
+                })
+            return self.publish_ack(user_message_id=message['message_id'],
+                sent_message_id=message['message_id'])
+        else:
+            return self.publish_nack(user_message_id=message['message_id'],
+                sent_message_id=message['message_id'],
+                reason='Missing in_reply_to or content')
---
+++ vumi/transports/airtel/tests/test_airtel.py
@@ -0,0 +1,251 @@
+import json
+from urllib import urlencode
+
+from twisted.internet.defer import inlineCallbacks
+
+from vumi.transports.tests.utils import TransportTestCase
+from vumi.transports.airtel import AirtelUSSDTransport
+from vumi.message import TransportUserMessage
+from vumi.utils import http_request_full
+
+
+class TestAirtelUSSDTransportTestCase(TransportTestCase):
+
+    transport_class = AirtelUSSDTransport
+    timeout = 1
+
+    @inlineCallbacks
+    def setUp(self):
+        yield super(TestAirtelUSSDTransportTestCase, self).setUp()
+        self.config = {
+            'web_port': 0,
+            'web_path': '/api/v1/airtel/ussd/',
+            'airtel_username': 'userid',
+            'airtel_password': 'password',
+        }
+        self.transport = yield self.get_transport(self.config)
+        self.session_manager = self.transport.session_manager
+        self.transport_url = self.transport.get_transport_url(
+            self.config['web_path'])
+        yield self.session_manager.redis._purge_all()  # just in case
+
+    def mk_full_request(self, **params):
+        return http_request_full('%s?%s' % (self.transport_url,
+            urlencode(params)), data='', method='GET')
+
+    def mk_request(self, **params):
+        defaults = {
+            'userid': 'userid',
+            'password': 'password',
+            'MSISDN': '27761234567',
+        }
+        defaults.update(params)
+        return self.mk_full_request(**defaults)
+
+    def mk_ussd_request(self, content, **kwargs):
+        defaults = {
+            'MSC': 'msc',
+            'input': content,
+        }
+        defaults.update(kwargs)
+        return self.mk_request(**defaults)
+
+    def mk_cleanup_request(self, **kwargs):
+        defaults = {
+            'clean': 'clean-session',
+            'status': 522
+        }
+        defaults.update(kwargs)
+        return self.mk_request(**defaults)
+
+    @inlineCallbacks
+    def test_inbound_begin(self):
+        # Second connect is the actual start of the session
+        deferred = self.mk_ussd_request('*121#')
+        [msg] = yield self.wait_for_dispatched_messages(1)
+        self.assertEqual(msg['content'], '')
+        self.assertEqual(msg['to_addr'], '*121#')
+        self.assertEqual(msg['from_addr'], '27761234567'),
+        self.assertEqual(msg['session_event'],
+                         TransportUserMessage.SESSION_NEW)
+        self.assertEqual(msg['transport_metadata'], {
+            'airtel': {
+                'MSC': 'msc',
+            },
+        })
+
+        reply = TransportUserMessage(**msg.payload).reply("ussd message")
+        self.dispatch(reply)
+        response = yield deferred
+        self.assertEqual(response.delivered_body, 'ussd message')
+        self.assertEqual(response.headers.getRawHeaders('Freeflow'), ['FC'])
+        self.assertEqual(response.headers.getRawHeaders('charge'), ['N'])
+        self.assertEqual(response.headers.getRawHeaders('amount'), ['0'])
+
+    @inlineCallbacks
+    def test_inbound_resume_and_reply_with_end(self):
+        # first pre-populate the redis datastore to simulate prior BEG message
+        yield self.session_manager.create_session('session-id',
+                to_addr='*167*7#', from_addr='27761234567',
+                last_ussd_params='7*a*b',
+                session_event=TransportUserMessage.SESSION_RESUME)
+
+        # Safaricom gives us the history of the full session in the USSD_PARAMS
+        # The last submitted bit of content is the last value delimited by '*'
+        deferred = self.mk_request(USSD_PARAMS='7*a*b*c')
+
+        [msg] = yield self.wait_for_dispatched_messages(1)
+        self.assertEqual(msg['content'], 'c')
+        self.assertEqual(msg['to_addr'], '*167*7#')
+        self.assertEqual(msg['from_addr'], '27761234567')
+        self.assertEqual(msg['session_event'],
+                         TransportUserMessage.SESSION_RESUME)
+        self.assertEqual(msg['transport_metadata'], {
+            'safaricom': {
+                'session_id': 'session-id',
+            },
+        })
+
+        reply = TransportUserMessage(**msg.payload).reply("hello world",
+            continue_session=False)
+        self.dispatch(reply)
+        response = yield deferred
+        self.assertEqual(response, 'END hello world')
+
+    @inlineCallbacks
+    def test_inbound_resume_with_failed_to_addr_lookup(self):
+        deferred = self.mk_full_request(ORIG='123456',
+            USSD_PARAMS='7*a', SESSION_ID='session-id')
+        response = yield deferred
+        self.assertEqual(json.loads(response), {
+            'missing_parameter': ['DEST'],
+        })
+
+    @inlineCallbacks
+    def test_to_addr_handling(self):
+        defaults = {
+            'DEST': '167',
+            'ORIG': '12345',
+            'SESSION_ID': 'session-id',
+        }
+
+        d1 = self.mk_full_request(USSD_PARAMS='7*1', **defaults)
+        [msg1] = yield self.wait_for_dispatched_messages(1)
+        self.assertEqual(msg1['to_addr'], '*167*7*1#')
+        self.assertEqual(msg1['content'], '')
+        self.assertEqual(msg1['session_event'],
+            TransportUserMessage.SESSION_NEW)
+        reply = TransportUserMessage(**msg1.payload).reply("hello world",
+            continue_session=True)
+        self.dispatch(reply)
+        yield d1
+
+        # follow up with the user submitting 'a'
+        d2 = self.mk_full_request(USSD_PARAMS='7*1*a', **defaults)
+        [msg1, msg2] = yield self.wait_for_dispatched_messages(2)
+        self.assertEqual(msg2['to_addr'], '*167*7*1#')
+        self.assertEqual(msg2['content'], 'a')
+        self.assertEqual(msg2['session_event'],
+            TransportUserMessage.SESSION_RESUME)
+        reply = TransportUserMessage(**msg2.payload).reply("hello world",
+            continue_session=False)
+        self.dispatch(reply)
+        yield d2
+
+    @inlineCallbacks
+    def test_hitting_url_twice_without_content(self):
+        d1 = self.mk_request(USSD_PARAMS='7*3')
+        [msg1] = yield self.wait_for_dispatched_messages(1)
+        self.assertEqual(msg1['to_addr'], '*167*7*3#')
+        self.assertEqual(msg1['content'], '')
+        self.assertEqual(msg1['session_event'],
+            TransportUserMessage.SESSION_NEW)
+        reply = TransportUserMessage(**msg1.payload).reply('Hello',
+            continue_session=True)
+        self.dispatch(reply)
+        yield d1
+
+        # make the exact same request again
+        d2 = self.mk_request(USSD_PARAMS='7*3')
+        [msg1, msg2] = yield self.wait_for_dispatched_messages(2)
+        self.assertEqual(msg2['to_addr'], '*167*7*3#')
+        self.assertEqual(msg2['content'], '')
+        self.assertEqual(msg2['session_event'],
+            TransportUserMessage.SESSION_RESUME)
+        reply = TransportUserMessage(**msg2.payload).reply('Hello',
+            continue_session=True)
+        self.dispatch(reply)
+        yield d2
+
+    @inlineCallbacks
+    def test_submitting_asterisks_as_values(self):
+        yield self.session_manager.create_session('session-id',
+                to_addr='*167*7#', from_addr='27761234567',
+                last_ussd_params='7*a*b')
+        # we're submitting a bunch of *s
+        deferred = self.mk_request(USSD_PARAMS='7*a*b*****')
+
+        [msg] = yield self.wait_for_dispatched_messages(1)
+        self.assertEqual(msg['content'], '****')
+
+        reply = TransportUserMessage(**msg.payload).reply('Hello',
+            continue_session=True)
+        self.dispatch(reply)
+        yield deferred
+        session = yield self.session_manager.load_session('session-id')
+        self.assertEqual(session['last_ussd_params'], '7*a*b*****')
+
+    @inlineCallbacks
+    def test_submitting_asterisks_as_values_after_asterisks(self):
+        yield self.session_manager.create_session('session-id',
+                to_addr='*167*7#', from_addr='27761234567',
+                last_ussd_params='7*a*b**')
+        # we're submitting a bunch of *s
+        deferred = self.mk_request(USSD_PARAMS='7*a*b*****')
+
+        [msg] = yield self.wait_for_dispatched_messages(1)
+        self.assertEqual(msg['content'], '**')
+
+        reply = TransportUserMessage(**msg.payload).reply('Hello',
+            continue_session=True)
+        self.dispatch(reply)
+        yield deferred
+        session = yield self.session_manager.load_session('session-id')
+        self.assertEqual(session['last_ussd_params'], '7*a*b*****')
+
+    @inlineCallbacks
+    def test_submitting_with_base_code_empty_ussd_params(self):
+        d1 = self.mk_request()
+        [msg1] = yield self.wait_for_dispatched_messages(1)
+        self.assertEqual(msg1['to_addr'], '*167#')
+        self.assertEqual(msg1['content'], '')
+        self.assertEqual(msg1['session_event'],
+            TransportUserMessage.SESSION_NEW)
+        reply = TransportUserMessage(**msg1.payload).reply('Hello',
+            continue_session=True)
+        self.dispatch(reply)
+        yield d1
+
+        # ask for first menu
+        d2 = self.mk_request(USSD_PARAMS='1')
+        [msg1, msg2] = yield self.wait_for_dispatched_messages(2)
+        self.assertEqual(msg2['to_addr'], '*167#')
+        self.assertEqual(msg2['content'], '1')
+        self.assertEqual(msg2['session_event'],
+            TransportUserMessage.SESSION_RESUME)
+        reply = TransportUserMessage(**msg2.payload).reply('Hello',
+            continue_session=True)
+        self.dispatch(reply)
+        yield d2
+
+        # ask for second menu
+        d3 = self.mk_request(USSD_PARAMS='1*1')
+        [msg1, msg2, msg3] = yield self.wait_for_dispatched_messages(3)
+        self.assertEqual(msg3['to_addr'], '*167#')
+        self.assertEqual(msg3['content'], '1')
+        self.assertEqual(msg3['session_event'],
+            TransportUserMessage.SESSION_RESUME)
+        reply = TransportUserMessage(**msg3.payload).reply('Hello',
+            continue_session=True)
+        self.dispatch(reply)
+        yield d3
