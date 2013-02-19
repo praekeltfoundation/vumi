5caa4c28f61b97e0180b7b47e25e05a6d4a4296b
justinvdm <justinvdm@lavabit.com>
2013-02-19 13:09:13 +0200

    Handled the authentication procedure and sending responses for XmlOverTcpClient.

--- vumi/transports/mtn_nigeria/tests/test_xml_over_tcp.py
+++ vumi/transports/mtn_nigeria/tests/test_xml_over_tcp.py
@@ -1,25 +1,105 @@
 import struct
 
 from twisted.trial import unittest
-from twisted.test import proto_helpers
+from twisted.internet import reactor
+from twisted.internet.defer import Deferred, inlineCallbacks
+from twisted.internet.protocol import Protocol, Factory, ClientFactory
 
 from vumi.transports.mtn_nigeria.xml_over_tcp import XmlOverTcpClient
 
 
-class ToyXmlOverTcpClient(XmlOverTcpClient):
+class WaitForDataMixin(object):
+    waiting_for_data = False
+    deferred_data = Deferred()
+
+    def wait_for_data(self):
+        self.waiting_for_data = True
+        self.deferred_data = Deferred()
+        return self.deferred_data
+
+    def callback_deferred_data(self, data):
+        if self.waiting_for_data and not self.deferred_data.called:
+            self.deferred_data.callback(data)
+            self.waiting_for_data = False
+
+
+class MockXmlOverTcpServer(Protocol, WaitForDataMixin):
     def __init__(self):
-        XmlOverTcpClient.__init__(self)
-        self.received_data_requests = []
+        self.responses = {}
+
+    def send_data(self, data):
+        self.transport.write(data)
+
+    def connectionMade(self):
+        self.factory.deferred_server.callback(self)
+
+    def dataReceived(self, data):
+        response = self.responses.get(data)
+        if response is not None:
+            self.transport.write(response)
+        self.callback_deferred_data(data)
+
+
+class ToyXmlOverTcpClient(XmlOverTcpClient, WaitForDataMixin):
+    _PACKET_RECEIVED_HANDLERS = {
+        'DummyPacket': 'dummy_packet_received'
+    }
+
+    def __init__(self):
+        XmlOverTcpClient.__init__(self, 'root', 'toor', '1029384756')
+        self.PACKET_RECEIVED_HANDLERS.update(self._PACKET_RECEIVED_HANDLERS)
+        self.received_dummy_packets = []
+        self.received_data_request_packets = []
+        self.next_id = 0
+
+    def connectionMade(self):
+        self.factory.deferred_client.callback(self)
+
+    def dataReceived(self, data):
+        XmlOverTcpClient.dataReceived(self, data)
+        self.callback_deferred_data(data)
+
+    def dummy_packet_received(self, session_id, params):
+        self.received_dummy_packets.append((session_id, params))
 
     def data_request_received(self, session_id, params):
-        self.received_data_requests.append([session_id, params])
+        self.received_data_request_packets.append((session_id, params))
+
+    def gen_id(self):
+        id = str(self.next_id).zfill(16)
+        self.next_id += 1
+        return id
 
 
-class XmlOverTcpClientTestCase(unittest.TestCase):
+class ClientServerMixin(object):
+    @inlineCallbacks
+    def start_protocols(self):
+        server_factory = Factory()
+        server_factory.protocol = MockXmlOverTcpServer
+        server_factory.deferred_server = Deferred()
+        self.server_port = reactor.listenTCP(0, server_factory)
+
+        client_factory = ClientFactory()
+        client_factory.protocol = ToyXmlOverTcpClient
+        client_factory.deferred_client = Deferred()
+        self.client_connector = reactor.connectTCP(
+            '127.0.0.1', self.server_port.getHost().port, client_factory)
+
+        self.client = yield client_factory.deferred_client
+        self.server = yield server_factory.deferred_server
+
+    def stop_protocols(self):
+        self.client_connector.disconnect()
+        self.server_port.loseConnection()
+
+
+class XmlOverTcpClientTestCase(unittest.TestCase, ClientServerMixin):
+    @inlineCallbacks
     def setUp(self):
-        t = proto_helpers.StringTransport()
-        self.client = ToyXmlOverTcpClient()
-        self.client.makeConnection(t)
+        yield self.start_protocols()
+
+    def tearDown(self):
+        self.stop_protocols()
 
     def mk_serialized_packet(self, session_id, body):
         length = len(body) + self.client.HEADER_SIZE
@@ -29,16 +109,174 @@ class XmlOverTcpClientTestCase(unittest.TestCase):
             str(length).zfill(self.client.LENGTH_HEADER_SIZE))
         return header + body
 
+    @inlineCallbacks
+    def test_contiguous_packets_received(self):
+        session_id_a = "1" * 16
+        body_a = "<DummyPacket><someParam>123</someParam></DummyPacket>"
+        session_id_b = "2" * 16
+        body_b = "<DummyPacket><someParam>456</someParam></DummyPacket>"
+        data = self.mk_serialized_packet(session_id_a, body_a)
+        data += self.mk_serialized_packet(session_id_b, body_b)
+        self.server.send_data(data)
+
+        yield self.client.wait_for_data()
+        self.assertEqual(
+            self.client.received_dummy_packets, [
+                (session_id_a, {'someParam': '123'}),
+                (session_id_b, {'someParam': '456'}),
+            ])
+
+    @inlineCallbacks
+    def test_partial_data_received(self):
+        session_id_a = "1" * 16
+        body_a = "<DummyPacket><someParam>123</someParam></DummyPacket>"
+        session_id_b = "2" * 16
+        body_b = "<DummyPacket><someParam>456</someParam></DummyPacket>"
+
+        # add a full first packet, then concatenate a sliced version of a
+        # second packet
+        data = self.mk_serialized_packet(session_id_a, body_a)
+        data += self.mk_serialized_packet(session_id_b, body_b)[:12]
+        self.server.send_data(data)
+
+        yield self.client.wait_for_data()
+        self.assertEqual(
+            self.client.received_dummy_packets,
+            [(session_id_a, {'someParam': '123'})])
+
+    @inlineCallbacks
+    def test_authentication(self):
+        request_id = str("0").zfill(16)
+        session_id = str("1").zfill(16)
+
+        request_body = (
+            "<AUTHRequest>"
+                "<requestId>%s</requestId>"
+                "<userName>root</userName>"
+                "<passWord>toor</passWord>"
+                "<applicationId>1029384756</applicationId>"
+            "</AUTHRequest>"
+        % request_id)
+        expected_request_packet = self.mk_serialized_packet(
+            session_id, request_body)
+
+        response_body = (
+            "<AUTHResponse>"
+                "<requestId>%s</requestId>"
+                "<authMsg>SUCCESS</authMsg>"
+            "</AUTHResponse>"
+        % request_id)
+        response_packet = self.mk_serialized_packet(session_id, response_body)
+        self.server.responses[expected_request_packet] = response_packet
+
+        self.client.login()
+        yield self.client.wait_for_data()
+        self.assertTrue(self.client.authenticated)
+
+    @inlineCallbacks
     def test_data_request_received(self):
-        session_id = "1234" * 4
+        session_id = "1" * 16
         body = (
             "<USSDRequest>"
-                "<requestId>13434</requestId>"
+                "<requestId>1291850641</requestId>"
+                "<msisdn>27845335367</msisdn>"
+                "<starCode>123</starCode>"
+                "<clientId>123</clientId>"
+                "<phase>2</phase>"
+                "<dcs>15</dcs>"
+                "<userdata>*123#</userdata>"
+                "<msgtype>1</msgtype>"
+                "<EndofSession>0</EndofSession>"
             "</USSDRequest>"
         )
         packet = self.mk_serialized_packet(session_id, body)
-        for b in packet:
-            self.client.dataReceived(b)
+        self.client.authenticated = True
+        self.server.send_data(packet)
+
+        yield self.client.wait_for_data()
+        expected_params = {
+            'requestId': '1291850641',
+            'msisdn': '27845335367',
+            'starCode': '123',
+            'clientId': '123',
+            'phase': '2',
+            'dcs': '15',
+            'userdata': '*123#',
+            'msgtype': '1',
+            'EndofSession': '0',
+        }
         self.assertEqual(
-            self.client.received_data_requests,
-            [['1234123412341234', {'requestId': '13434'}]])
+            self.client.received_data_request_packets,
+            [(session_id, expected_params)])
+
+    def test_validate_packet_fields(self):
+        self.assertTrue(self.client.validate_packet_fields(
+            {'a': 1, 'b': 2}, set(['a', 'b']), set(['b', 'c'])))
+
+        self.assertFalse(self.client.validate_packet_fields(
+            {'a': 1, 'b': 2}, set(['a', 'b', 'c'])))
+
+        self.assertFalse(self.client.validate_packet_fields(
+            {'a': 1, 'b': 2, 'c': 2, 'd': 3}, set(['a', 'b']), set(['c'])))
+
+    @inlineCallbacks
+    def test_send_data_response_for_continue_session(self):
+        session_id = "1" * 16
+        body = (
+            "<USSDResponse>"
+                "<requestId>1291850641</requestId>"
+                "<msisdn>27845335367</msisdn>"
+                "<starCode>123</starCode>"
+                "<clientId>123</clientId>"
+                "<phase>2</phase>"
+                "<msgtype>2</msgtype>"
+                "<dcs>15</dcs>"
+                "<userdata>*123#</userdata>"
+                "<EndofSession>0</EndofSession>"
+            "</USSDResponse>"
+        )
+        expected_packet = self.mk_serialized_packet(session_id, body)
+
+        self.client.send_data_response(
+            session_id,
+            end_session=False,
+            requestId='1291850641',
+            msisdn='27845335367',
+            starCode='123',
+            clientId='123',
+            userdata='*123#',
+        )
+
+        received_packet = yield self.server.wait_for_data()
+        self.assertEqual(expected_packet, received_packet)
+
+    @inlineCallbacks
+    def test_send_data_response_for_end_session(self):
+        session_id = "1" * 16
+        body = (
+            "<USSDResponse>"
+                "<requestId>1291850641</requestId>"
+                "<msisdn>27845335367</msisdn>"
+                "<starCode>123</starCode>"
+                "<clientId>123</clientId>"
+                "<phase>2</phase>"
+                "<msgtype>6</msgtype>"
+                "<dcs>15</dcs>"
+                "<userdata>*123#</userdata>"
+                "<EndofSession>1</EndofSession>"
+            "</USSDResponse>"
+        )
+        expected_packet = self.mk_serialized_packet(session_id, body)
+
+        self.client.send_data_response(
+            session_id,
+            end_session=True,
+            requestId='1291850641',
+            msisdn='27845335367',
+            starCode='123',
+            clientId='123',
+            userdata='*123#',
+        )
+
+        received_packet = yield self.server.wait_for_data()
+        self.assertEqual(expected_packet, received_packet)
--- vumi/transports/mtn_nigeria/xml_over_tcp.py
+++ vumi/transports/mtn_nigeria/xml_over_tcp.py
@@ -1,6 +1,8 @@
+import uuid
 import struct
 from xml.etree import ElementTree as ET
 
+from twisted.python import log
 from twisted.internet.protocol import Protocol
 
 
@@ -12,18 +14,39 @@ class XmlOverTcpClient(Protocol):
 
     PACKET_RECEIVED_HANDLERS = {
         'USSDRequest': 'data_request_packet_received',
+        'AUTHResponse': 'login_response_packet_received',
     }
 
-    def __init__(self):
+    MANDATORY_DATA_REQUEST_FIELDS = set([
+        'requestId', 'msisdn', 'clientId', 'starCode', 'msgtype', 'phase',
+        'dcs', 'userdata'])
+    OTHER_DATA_REQUEST_FIELDS = set(['EndofSession'])
+
+    LOGIN_RESPONSE_FIELDS = set(['requestId', 'authMsg'])
+
+    # Data requests and responses need to include a 'dcs' (data coding scheme)
+    # field. '15' is used for ASCII, and is the default. The documentation
+    # does not offer any other codes.
+    DATA_CODING_SCHEME = '15'
+
+    # Data requests and responses need to include a 'phase' field. The
+    # documentation does not provide any information about 'phase', other
+    # than mentioning that it is mandatory, and must be set to '2'
+    PHASE = '2'
+
+    def __init__(self, username, password, application_id):
+        self.username = username
+        self.password = password
+        self.application_id = application_id
+        self.authenticated = False
+
         self._buffer = ''
         self._current_header = None
-        self._state = 'INITIALISED'
 
     def connectionMade(self):
-        # TODO login (on successful login, start heartbeat)
-        self._state = 'CONNECTED'
+        self.login()
 
-    def connectionLost(self):
+    def connectionLost(self, reason):
         pass
         # TODO stop heartbeat
 
@@ -83,12 +106,53 @@ class XmlOverTcpClient(Protocol):
         # dispatch the packet to the appropriate handler
         handler_name = self.PACKET_RECEIVED_HANDLERS.get(packet_type, None)
         if handler_name is None:
-            # TODO
+            log.msg("Packet of an unknown type received: %s" % packet_type)
+            # TODO send error response
             return
         getattr(self, handler_name)(session_id, params)
 
+    @staticmethod
+    def validate_packet_fields(packet_params, mandatory_fields,
+                               other_fields=set()):
+        packet_fields = set(packet_params.keys())
+
+        all_fields = mandatory_fields | other_fields
+        unexpected_fields = packet_fields - all_fields
+        if unexpected_fields:
+            log.msg("Unexpected fields in received packet: %s" %
+                    list(unexpected_fields))
+            # TODO send error response
+            return False
+
+        missing_mandatory_fields = mandatory_fields - packet_fields
+        if missing_mandatory_fields:
+            log.msg("Missing mandatory fields in received packet: %s" %
+                    list(missing_mandatory_fields))
+            # TODO send error response
+            return False
+
+        return True
+
+    def login_response_packet_received(self, session_id, params):
+        if self.validate_packet_fields(params, self.LOGIN_RESPONSE_FIELDS):
+            self.authenticated = True
+            # TODO start heartbeat
+
     def data_request_packet_received(self, session_id, params):
-        # TODO error handling
+        if not self.authenticated:
+            log.msg("Data request packet received before client "
+                    "authentication was completed.")
+            # TODO send error response
+            return
+
+        if not self.validate_packet_fields(params,
+                self.MANDATORY_DATA_REQUEST_FIELDS,
+                self.OTHER_DATA_REQUEST_FIELDS):
+            return
+
+        # if EndofSession is not in params, assume this is the end of a session
+        params.setdefault('EndofSession', '1')
+
         self.data_request_received(session_id, params)
 
     def data_request_received(self, session_id, params):
@@ -98,15 +162,58 @@ class XmlOverTcpClient(Protocol):
     def serialize_packet(cls, session_id, packet_type, params):
         # construct body
         root = ET.Element(packet_type)
-        for param_name, param_value in params.iteritems():
+        for param_name, param_value in params:
             ET.SubElement(root, param_name).text = param_value
-        body = ET.dump(root)
+        body = ET.tostring(root)
 
         # construct header
         length = len(body) + cls.HEADER_SIZE
         header = struct.pack(
-            cls.protocol.HEADER_FORMAT,
+            cls.HEADER_FORMAT,
             session_id,
-            str(length).zfill(cls.protocol.LENGTH_HEADER_SIZE))
+            str(length).zfill(cls.LENGTH_HEADER_SIZE))
 
         return header + body
+
+    def send_packet(self, session_id, packet_type, params):
+        packet = self.serialize_packet(session_id, packet_type, params)
+        self.transport.write(packet)
+
+    @staticmethod
+    def gen_id():
+        """
+        Returns a new uuid's 16 byte string. Used to generate request id's and
+        dummy session id's needed for sending certain types of packets,
+        including login requests and enquire link requests.
+        """
+        return uuid.uuid4().bytes
+
+    def login(self):
+        params = [
+            ('requestId', self.gen_id()),
+            ('userName', self.username),
+            ('passWord', self.password),  # plaintext passwords, yay :/
+            ('applicationId', self.application_id),
+        ]
+        self.send_packet(self.gen_id(), 'AUTHRequest', params)
+
+    def send_data_response(self, session_id, end_session=True, **params):
+        if end_session:
+            msgtype = '6'
+            end_of_session = '1'
+        else:
+            msgtype = '2'
+            end_of_session = '0'
+
+        packet_params = [
+            ('requestId', params['requestId']),
+            ('msisdn', params['msisdn']),
+            ('starCode', params['starCode']),
+            ('clientId', params['clientId']),
+            ('phase', params.get('phase') or self.PHASE),
+            ('msgtype', params.get('msgtype') or msgtype),
+            ('dcs', params.get('dcs') or self.DATA_CODING_SCHEME),
+            ('userdata', params['userdata']),
+            ('EndofSession', params.get('EndofSession') or end_of_session)
+        ]
+        self.send_packet(session_id, 'USSDResponse', packet_params)
