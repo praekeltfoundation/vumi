95e4f1fd87e6d9010ec9a3aa095e8b070f1d507a
Simon Cross <hodgestar@gmail.com>
2013-02-21 00:04:06 +0200

    Remove almost all of the deprecate stuff from BaseWorker (will soon appear in Application and Transport classes).

--- vumi/worker_base.py
+++ vumi/worker_base.py
@@ -1,7 +1,5 @@
 """Basic tools for workers that handle TransportMessages."""
 
-import warnings
-
 from twisted.internet.defer import succeed, maybeDeferred
 from twisted.python import log
 
@@ -9,29 +7,11 @@ from vumi.service import Worker
 from vumi.middleware import setup_middlewares_from_config
 from vumi.connectors import ReceiveInboundConnector, ReceiveOutboundConnector
 from vumi.config import Config, ConfigInt
+from vumi.errors import DuplicateConnectorError
 
 
 def then_call(d, func, *args, **kw):
     return d.addCallback(lambda r: func(*args, **kw))
-
-
-def DeprecatedAttribute(object):
-    def __init__(self, name):
-        self.name = '_%s_value' % (name,)
-
-    def warn(self, obj):
-        warnings.warn(
-            "Direct use of transport publishers and consumers is deprecated."
-            " Use connectors and endpoints instead.",
-            category=DeprecationWarning)
-
-    def __get__(self, obj, cls):
-        self.warn(obj)
-        return getattr(obj, self.name)
-
-    def __set__(self, obj, value):
-        self.warn(obj)
-        setattr(obj, self.name, value)
 
 
 class BaseConfig(Config):
@@ -50,59 +30,62 @@ class BaseWorker(Worker):
     """Base class for a message processing worker.
 
     This contains common functionality used by application, transport and
-    dispatcher workers.
+    dispatcher workers. It should be sub-classed by workers that need to
+    manage their own connectors.
     """
 
     CONFIG_CLASS = BaseConfig
-
-    start_message_consumer = True
-
-    # Some descriptors for common deprecated attributes.
-    transport_publisher = DeprecatedAttribute('transport_publisher')
-    transport_consumer = DeprecatedAttribute('transport_consumer')
-    transport_event_consumer = DeprecatedAttribute('transport_event_consumer')
-    message_publisher = DeprecatedAttribute('message_publisher')
-    event_publisher = DeprecatedAttribute('event_publisher')
 
     def startWorker(self):
         log.msg('Starting a %s worker with config: %s'
                 % (self.__class__.__name__, self.config))
         self.connectors = {}
+        self.middlewares = []
+        self._static_config = self.CONFIG_CLASS(self.config, static=True)
 
         d = maybeDeferred(self._validate_config)
-        then_call(d, self.setup_connectors)
         then_call(d, self.setup_middleware)
-        then_call(d, self._worker_specific_setup)
-        then_call(d, self._finish_worker_setup)
-        then_call(d, self._setup_unpause)
+        then_call(d, self.setup_connectors)
+        then_call(d, self.setup_worker)
+        return d
+
+    def stopWorker(self):
+        d = succeed(None)
+        then_call(d, self.teardown_worker)
+        then_call(d, self.teardown_connectors)
+        then_call(d, self.teardown_middleware)
         return d
 
     def setup_connectors(self):
         raise NotImplementedError()
 
-    def _setup_unpause(self):
-        raise NotImplementedError()
-
-    def _worker_specific_setup(self):
-        pass
-
-    def _worker_specific_teardown(self):
-        pass
-
-    def _finish_worker_setup(self):
-        # Apply pre-fetch limits if we need to.
-        if self.amqp_prefetch_count is not None:
-            self.setup_amqp_qos()
-
-    def stopWorker(self):
+    def teardown_connectors(self):
         d = succeed(None)
         for connector_name in self.connectors.keys():
-            connector = self.connectors.pop(connector_name)
-            then_call(d, connector.teardown)
-        then_call(d, self._worker_specific_teardown)
+            then_call(d, self.teardown_connector, connector_name)
+        return d
+
+    def setup_worker(self):
+        raise NotImplementedError()
+
+    def teardown_worker(self):
+        raise NotImplementedError()
+
+    def setup_middleware(self):
+        """Create middlewares from config."""
+        d = setup_middlewares_from_config(self, self.config)
+        d.addCallback(self.middlewares.extend)
+        return d
+
+    def teardown_middleware(self):
+        """Teardown middlewares."""
+        d = succeed(None)
+        for mw in reversed(self.middlewares):
+            then_call(d, mw.teardown_middleware)
         return d
 
     def get_static_config(self):
+        """Return static (message independent) configuration."""
         return self._static_config
 
     def get_config(self, msg):
@@ -115,68 +98,52 @@ class BaseWorker(Worker):
         return succeed(self.CONFIG_CLASS(self.config))
 
     def _validate_config(self):
-        # We assume that all required fields will either come from the base
-        # config or will have placeholder values that don't fail validation.
-        # This object is only created to trigger validation.
-        # TODO: Eventually we'll be able to remove the legacy validate_config()
-        # and just use config objects.
-        self._static_config = self.CONFIG_CLASS(self.config, static=True)
-        self.amqp_prefetch_count = self._static_config.amqp_prefetch_count
-        return self.validate_config()
+        """Once sub-classes call `super().validate_config` properly,
+           this method can be removed.
+           """
+        # TODO: remove this once all uses of validate_config have been fixed.
+        self.validate_config()
 
     def validate_config(self):
         """
         Application-specific config validation happens in here.
 
-        Subclasses may override this method to perform extra config validation.
+        Sub-classes may override this method to perform extra config
+        validation.
         """
+        # TODO: deprecate this in favour of a similar method on
+        #       config classes.
         pass
 
-    def setup_middleware(self):
-        """
-        Middleware setup happens here.
-
-        Subclasses should not override this unless they need to do nonstandard
-        middleware setup.
-        """
-        # TODO: Make this more flexible
-        d = setup_middlewares_from_config(self, self.config)
-
-        def cb(middlewares):
-            for connector in self.connectors.values():
-                connector.set_middlewares(middlewares)
-        return d.addCallback(cb)
-
-    def _check_for_deprecated_method(self, method_name):
-        # XXX: Is there a better way to do this?
-        my_stp = getattr(type(self), method_name)
-        base_stp = getattr(BaseWorker, method_name)
-        if my_stp == base_stp:
-            return False
-        warnings.warn(
-            "%s() is deprecated. Use connectors and endpoints instead." % (
-                method_name,), category=DeprecationWarning)
-        return True
-
-    def setup_connector(self, connector_cls, connector_name):
+    def setup_connector(self, connector_cls, connector_name, middleware=False):
         if connector_name in self.connectors:
-            log.warning("Connector %r already set up." % (connector_name,))
-            conn = self.connectors[connector_name]
-            if not isinstance(conn, connector_cls):
-                log.warning("Connector %r is type %r, not %r" % (
-                    connector_name, type(conn), connector_cls))
-            # So we always get a deferred from here.
-            return succeed(self.connectors[connector_name])
-        connector = connector_cls(self, connector_name)
+            raise DuplicateConnectorError("Attempt to add duplicate connector"
+                                          " with name %r" % (connector_name,))
+        prefetch_count = self.get_static_config().amqp_prefetch_count
+        middlewares = self.middlewares if middleware else None
+
+        connector = connector_cls(self, connector_name,
+                                  prefetch_count=prefetch_count,
+                                  middlewares=middlewares)
         self.connectors[connector_name] = connector
+
         d = connector.setup()
-        return d.addCallback(lambda r: connector)
+        then_call(d, lambda: connector)
+        return d
 
-    def setup_ri_connector(self, connector_name):
-        return self.setup_connector(ReceiveInboundConnector, connector_name)
+    def teardown_connector(self, connector_name):
+        connector = self.connectors.pop(connector_name)
+        d = connector.teardown()
+        d.addCallback(lambda r: None)
+        return d
 
-    def setup_ro_connector(self, connector_name):
-        return self.setup_connector(ReceiveOutboundConnector, connector_name)
+    def setup_ri_connector(self, connector_name, middleware=True):
+        return self.setup_connector(ReceiveInboundConnector, connector_name,
+                                    middleware=middleware)
+
+    def setup_ro_connector(self, connector_name, middleware=True):
+        return self.setup_connector(ReceiveOutboundConnector, connector_name,
+                                    middleware=middleware)
 
     def pause_connectors(self):
         for connector in self.connectors.itervalues():
@@ -185,7 +152,3 @@ class BaseWorker(Worker):
     def unpause_connectors(self):
         for connector in self.connectors.itervalues():
             connector.unpause()
-
-    def setup_amqp_qos(self):
-        for connector in self.connectors.itervalues():
-            connector.set_consumer_prefetch(int(self.amqp_prefetch_count))
