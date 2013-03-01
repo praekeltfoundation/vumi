616bb8b4557df38dcc351a2b611ae492c4a96729
Simon Cross <hodgestar@gmail.com>
2013-03-01 13:41:27 +0200

    Add test for bad response command status.

--- vumi/transports/smpp/tests/test_smpp.py
+++ vumi/transports/smpp/tests/test_smpp.py
@@ -1,3 +1,5 @@
+import binascii
+
 from twisted.internet.defer import Deferred, inlineCallbacks, succeed
 from twisted.internet.task import Clock
 from smpp.pdu_builder import SubmitSMResp, DeliverSM
@@ -140,6 +142,30 @@ class SmppTransportTestCase(TransportTestCase):
         self.assertEqual(nack['nack_reason'], 'ESME_RSUBMITFAIL')
 
         comparison = self.mkmsg_fail(message.payload, 'ESME_RSUBMITFAIL')
+        [actual] = yield self.get_dispatched_failures()
+        self.assertEqual(actual, comparison)
+
+    @inlineCallbacks
+    def test_failed_submit_with_no_reason(self):
+        message = self.mkmsg_out("message", message_id='446')
+        # Equivalent of SubmitSMResp(1, "3rd_party_id_3", command_status='XXX')
+        # but with a bad command_status (pdu_builder can't produce binary with
+        # command_statuses' it doesn't understand). Use
+        # smpp.pdu.unpack(response_bin) to get a PDU object:
+        response_hex = ("0000001f80000004"
+                        "0000ffff"  # unknown command status
+                        "000000013372645f70617274795f69645f3300")
+        response_bin = binascii.a2b_hex(response_hex)
+        yield self.dispatch(message)
+        yield self.esme.handle_data(response_bin)
+
+        self.assert_sent_contents(["message"])
+        # There should be a nack
+        [nack] = yield self.wait_for_dispatched_events(1)
+        self.assertEqual(nack['user_message_id'], message['message_id'])
+        self.assertEqual(nack['nack_reason'], 'Unspecified')
+
+        comparison = self.mkmsg_fail(message.payload, 'Unspecified')
         [actual] = yield self.get_dispatched_failures()
         self.assertEqual(actual, comparison)
 
