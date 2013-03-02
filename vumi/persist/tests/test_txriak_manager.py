0de01c2764e49fd44d70b1aeb2167a76ce004753
Simon Cross <hodgestar@gmail.com>
2013-03-02 10:41:14 +0200

    Add tests for setting mapreduce_timeout from config.

--- vumi/persist/tests/test_txriak_manager.py
+++ vumi/persist/tests/test_txriak_manager.py
@@ -58,6 +58,8 @@ class CommonRiakManagerTests(object):
         self.assertEqual(manager.__class__, manager_cls)
         self.assertEqual(manager.load_bunch_size,
                          manager.DEFAULT_LOAD_BUNCH_SIZE)
+        self.assertEqual(manager.mapreduce_timeout,
+                         manager.DEFAULT_MAPREDUCE_TIMEOUT)
 
     def test_from_config_with_bunch_size(self):
         manager_cls = self.manager.__class__
@@ -65,6 +67,13 @@ class CommonRiakManagerTests(object):
                                            'load_bunch_size': 10,
                                            })
         self.assertEqual(manager.load_bunch_size, 10)
+
+    def test_from_config_with_mapreduce_timeout(self):
+        manager_cls = self.manager.__class__
+        manager = manager_cls.from_config({'bucket_prefix': 'test.',
+                                           'mapreduce_timeout': 1000,
+                                           })
+        self.assertEqual(manager.mapreduce_timeout, 1000)
 
     def test_sub_manager(self):
         sub_manager = self.manager.sub_manager("foo.")
