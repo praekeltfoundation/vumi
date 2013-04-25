57ee1df33942fe6f6aa271ce91469a0c86402a8a
Jeremy Thurgood <jeremy@praekelt.com>
2013-04-25 17:08:23 +0200

    Tweak test_batch_done() a bit to see if it stops failing weirdly on Travis.

--- vumi/components/tests/test_message_store.py
+++ vumi/components/tests/test_message_store.py
@@ -120,10 +120,13 @@ class TestMessageStore(TestMessageStoreBase):
     def test_batch_done(self):
         tag1 = ("poolA", "tag1")
         batch_id = yield self.store.batch_start([tag1])
+        tag_info = yield self.store.get_tag_info(tag1)
+        self.assertEqual(tag_info.current_batch.key, batch_id)
+
         yield self.store.batch_done(batch_id)
         batch = yield self.store.get_batch(batch_id)
-        tag_info = yield self.store.get_tag_info(tag1)
         self.assertEqual(list(batch.tags), [tag1])
+        tag_info = yield self.store.get_tag_info(tag1)
         self.assertEqual(tag_info.current_batch.key, None)
 
     @inlineCallbacks
