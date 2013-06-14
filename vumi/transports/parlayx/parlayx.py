2437379dbf8f06ce447d860078991f60d4e8ecfb
Jonathan Jacobs <jonathan@jsphere.com>
2013-06-14 15:22:22 +0200

    ParlayX transport and tests.

---
+++ vumi/transports/parlayx/parlayx.py
@@ -0,0 +1,134 @@
+# -*- test-case-name: vumi.transports.parlayx.tests.test_parlayx -*-
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+from vumi import log
+from vumi.config import ConfigText, ConfigInt
+from vumi.transports.base import Transport
+from vumi.transports.failures import TemporaryFailure, PermanentFailure
+from vumi.transports.parlayx.client import (
+    ParlayXClient, ServiceException, PolicyException)
+from vumi.transports.parlayx.server import SmsNotificationService
+from vumi.transports.parlayx.soaputil import SoapFault
+
+
+
+class ParlayXTransportConfig(Transport.CONFIG_CLASS):
+    web_notification_path = ConfigText(
+        'Path to listen for delivery and receipt notifications on',
+        static=True)
+    web_notification_port = ConfigInt(
+        'Port to listen for delivery and receipt notifications on',
+        default=0, static=True)
+    notification_endpoint_uri = ConfigText(
+        'URI of the ParlayX SmsNotificationService in Vumi', static=True)
+    short_code = ConfigText(
+        'Service activation number or short code to receive deliveries for',
+        static=True)
+    remote_send_uri = ConfigText(
+        'URI of the remote ParlayX SendSmsService', static=True)
+    remote_notification_uri = ConfigText(
+        'URI of the remote ParlayX SmsNotificationService', static=True)
+
+
+
+class ParlayXTransport(Transport):
+    CONFIG_CLASS = ParlayXTransportConfig
+    transport_type = 'sms'
+
+
+    def validate_config(self):
+        self.web_notification_path = self.config['web_notification_path']
+        self.web_notification_port = int(self.config['web_notification_port'])
+        self.notification_endpoint_uri = self.config['notification_endpoint_uri']
+        self.short_code = self.config['short_code']
+        self.remote_send_uri = self.config['remote_send_uri']
+        self.remote_notification_uri = self.config['remote_notification_uri']
+        # XXX: check these things?
+
+
+    def _create_client(self):
+        return ParlayXClient(
+            short_code=self.short_code,
+            endpoint=self.notification_endpoint_uri,
+            send_uri=self.remote_send_uri,
+            notification_uri=self.remote_notification_uri)
+
+
+    @inlineCallbacks
+    def setup_transport(self):
+        # XXX: ???
+        #config = self.get_static_config()
+        log.info('Starting ParlayX transport: %s' % (self.transport_name,))
+        self.web_resource = yield self.start_web_resources(
+            [(SmsNotificationService(self.handle_raw_inbound_message,
+                                     self.publish_delivery_report),
+              self.web_notification_path)],
+            self.web_notification_port)
+        self._parlayx_client = self._create_client()
+        yield self._parlayx_client.start_sms_notification()
+
+
+    def teardown_transport(self):
+        log.info('Stopping ParlayX transport: %s' % (self.transport_name,))
+        d = self.web_resource.loseConnection()
+        d.addBoth(lambda ignored: self._parlayx_client.stop_sms_notification())
+        return d
+
+
+    def handle_outbound_message(self, message):
+        """
+        Send a text message via the ParlayX client.
+        """
+        log.info('Sending SMS via ParlayX: %r' % (message.to_json(),))
+        d = self._parlayx_client.send_sms(
+            message['to_addr'],
+            message['content'],
+            message['message_id'])
+        d.addErrback(self.handle_outbound_message_failure, message)
+        d.addCallback(
+            lambda requestIdentifier: self.publish_ack(
+                message['message_id'], requestIdentifier))
+        return d
+
+
+    @inlineCallbacks
+    def handle_outbound_message_failure(self, f, message):
+        """
+        Handle outbound message failures.
+
+        `ServiceException`, `PolicyException` and client-class SOAP faults
+        result in `PermanentFailure` being raised; server-class SOAP faults
+        instances result in `TemporaryFailure` being raised; and other failures
+        are passed through.
+        """
+        log.error(f, 'Sending SMS failure on ParlayX: %r' % (
+            self.transport_name,))
+
+        if not f.check(ServiceException, PolicyException):
+            if f.check(SoapFault):
+                # We'll give server-class unknown SOAP faults the benefit of
+                # the doubt as far as temporary failures go.
+                if f.value.code.endswith('Server'):
+                    raise TemporaryFailure(f)
+
+        yield self.publish_nack(message['message_id'], f.getErrorMessage())
+        if f.check(SoapFault):
+            # We've ruled out unknown SOAP faults, so this must be a permanent
+            # failure.
+            raise PermanentFailure(f)
+        returnValue(f)
+
+
+    def handle_raw_inbound_message(self, message_id, inbound_message):
+        """
+        Handle incoming text messages from `SmsNotificationService` callbacks.
+        """
+        log.info('Receiving SMS via ParlayX: %r: %r' % (
+            message_id, inbound_message,))
+        return self.publish_message(
+            message_id=message_id,
+            content=inbound_message.message,
+            to_addr=inbound_message.service_activation_number,
+            from_addr=inbound_message.sender_address,
+            provider='parlayx',
+            transport_type=self.transport_type)
---
+++ vumi/transports/parlayx/tests/test_parlayx.py
@@ -0,0 +1,201 @@
+from functools import partial
+
+from twisted.internet.defer import inlineCallbacks, succeed, fail
+
+from vumi.transports.failures import PermanentFailure
+from vumi.transports.parlayx import ParlayXTransport
+from vumi.transports.parlayx.client import PolicyException, ServiceException
+from vumi.transports.parlayx.server import DeliveryStatus
+from vumi.transports.parlayx.soaputil import perform_soap_request
+from vumi.transports.parlayx.tests.utils import (
+    create_sms_reception_element, create_sms_delivery_receipt)
+from vumi.transports.tests.utils import TransportTestCase
+
+
+
+class MockParlayXClient(object):
+    """
+    A mock ``ParlayXClient`` that doesn't involve real HTTP requests but
+    instead uses canned responses.
+    """
+    def __init__(self, start_sms_notification=None, stop_sms_notification=None,
+                 send_sms=None):
+        if start_sms_notification is None:
+            start_sms_notification = partial(succeed, None)
+        if stop_sms_notification is None:
+            stop_sms_notification = partial(succeed, None)
+        if send_sms is None:
+            send_sms = partial(succeed, 'request_message_id')
+
+        self.responses = {
+            'start_sms_notification': start_sms_notification,
+            'stop_sms_notification': stop_sms_notification,
+            'send_sms': send_sms}
+        self.calls = []
+
+
+    def _invoke_response(self, name, args):
+        """
+        Invoke the canned response for the method name ``name`` and log the
+        invocation.
+        """
+        self.calls.append((name, args))
+        return self.responses[name]()
+
+
+    def start_sms_notification(self):
+        return self._invoke_response('start_sms_notification', [])
+
+
+    def stop_sms_notification(self):
+        return self._invoke_response('stop_sms_notification', [])
+
+
+    def send_sms(self, to_addr, content, message_id):
+        return self._invoke_response(
+            'send_sms', [to_addr, content, message_id])
+
+
+
+class ParlayXTransportTestCase(TransportTestCase):
+    """
+    Tests for `vumi.transports.parlayx.ParlayXTransport`.
+    """
+    transport_class = ParlayXTransport
+    timeout = 1
+
+
+    @inlineCallbacks
+    def setUp(self):
+        super(ParlayXTransportTestCase, self).setUp()
+        self.port = 9999
+        config = {
+            'transport_name': self.transport_name,
+            'web_notification_path': '/hello',
+            'web_notification_port': self.port,
+            'notification_endpoint_uri': 'endpoint_uri',
+            'short_code': '54321',
+            'remote_send_uri': 'send_uri',
+            'remote_notification_uri': 'notification_uri'}
+        self.uri = 'http://localhost:%s%s' % (
+            self.port, config['web_notification_path'])
+
+        def _create_client(transport):
+            return MockParlayXClient()
+        self.patch(self.transport_class, '_create_client', _create_client)
+        self.transport = yield self.get_transport(config, start=False)
+
+
+    @inlineCallbacks
+    def test_ack(self):
+        """
+        Basic message delivery.
+        """
+        yield self.transport.startWorker()
+        msg = self.mkmsg_out()
+        yield self.dispatch(msg)
+        [event] = yield self.wait_for_dispatched_events(1)
+        self.assertEqual(event['event_type'], 'ack')
+        self.assertEqual(event['user_message_id'], msg['message_id'])
+
+
+    @inlineCallbacks
+    def test_nack(self):
+        """
+        Exceptions raised in an outbound message handler result in the message
+        delivery failing, and a failure event being logged.
+        """
+        def _create_client(transport):
+            return MockParlayXClient(
+                send_sms=partial(fail, ValueError('failed')))
+        self.patch(self.transport_class, '_create_client', _create_client)
+
+        yield self.transport.startWorker()
+        msg = self.mkmsg_out()
+        yield self.dispatch(msg)
+        [event] = yield self.wait_for_dispatched_events(1)
+        self.assertEqual(event['event_type'], 'nack')
+        self.assertEqual(event['user_message_id'], msg['message_id'])
+        self.assertEqual(event['nack_reason'], 'failed')
+
+        failures = self.flushLoggedErrors(ValueError)
+        # Logged once by the transport and once by Twisted for being unhandled.
+        self.assertEqual(2, len(failures))
+
+
+    @inlineCallbacks
+    def _test_nack_permanent(self, expected_exception):
+        """
+        The expected exception, when raised in an outbound message handler,
+        results in a `PermanentFailure` and is logged along with the original
+        exception.
+        """
+        def _create_client(transport):
+            return MockParlayXClient(
+                send_sms=partial(
+                    fail, expected_exception('soapenv:Client', 'failed')))
+        self.patch(self.transport_class, '_create_client', _create_client)
+
+        yield self.transport.startWorker()
+        msg = self.mkmsg_out()
+        yield self.dispatch(msg)
+        [event] = yield self.wait_for_dispatched_events(1)
+        self.assertEqual(event['event_type'], 'nack')
+        self.assertEqual(event['user_message_id'], msg['message_id'])
+        self.assertEqual(event['nack_reason'], 'failed')
+
+        failures = self.flushLoggedErrors(expected_exception, PermanentFailure)
+        self.assertEqual(2, len(failures))
+
+
+    def test_nack_service_exception(self):
+        """
+        When `ServiceException` is raised in an outbound message handler, it
+        results in a `PermanentFailure` exception.
+        """
+        return self._test_nack_permanent(ServiceException)
+
+
+    def test_nack_policy_exception(self):
+        """
+        When `PolicyException` is raised in an outbound message handler, it
+        results in a `PermanentFailure` exception.
+        """
+        return self._test_nack_permanent(PolicyException)
+
+
+    @inlineCallbacks
+    def test_receive_sms(self):
+        """
+        When a text message is submitted to the Vumi ParlayX
+        ``notifySmsReception`` SOAP endpoint, a message is
+        published containing the message identifier, message content, from
+        address and to address that accurately match what was submitted.
+        """
+        yield self.transport.startWorker()
+        body = create_sms_reception_element(
+            '1234', 'message', '+27117654321', '54321')
+        yield perform_soap_request(self.uri, '', body)
+        [msg] = self.get_dispatched_messages()
+        self.assertEqual(
+            ('1234', 'message', '+27117654321', '54321'),
+            (msg['message_id'], msg['content'], msg['from_addr'],
+             msg['to_addr']))
+
+
+    @inlineCallbacks
+    def test_delivery_receipt(self):
+        """
+        When a delivery receipt is submitted to the Vumi ParlayX
+        ``notifySmsDeliveryReceiptResponse`` SOAP endpoint, an event is
+        published containing the message identifier and the delivery status
+        that accurately match what was submitted.
+        """
+        yield self.transport.startWorker()
+        body = create_sms_delivery_receipt(
+            '1234', '+27117654321', DeliveryStatus.DeliveredToNetwork)
+        yield perform_soap_request(self.uri, '', body)
+        [event] = self.get_dispatched_events()
+        self.assertEqual(
+            ('1234', 'delivered'),
+            (event['user_message_id'], event['delivery_status']))
