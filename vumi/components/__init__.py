066610797ee2d6d5fa1d50c142113fa456d51b03
Jeremy Thurgood <jeremy@praekelt.com>
2013-06-25 14:39:07 +0200

    Remove message_store import from components __init__.

--- vumi/components/__init__.py
+++ vumi/components/__init__.py
@@ -1,7 +1,6 @@
 """Various useful components."""
 
-__all__ = ["MessageStore", "SessionManager", "TagpoolManager"]
+__all__ = ["SessionManager", "TagpoolManager"]
 
-from vumi.components.message_store import MessageStore
 from vumi.components.session import SessionManager
 from vumi.components.tagpool import TagpoolManager
