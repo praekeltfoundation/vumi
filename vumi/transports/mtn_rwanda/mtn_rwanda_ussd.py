4645d68538dd59e90494fae2c74a18b2da074728
ashfall <ashwini.oruganti@gmail.com>
2013-06-26 18:36:48 +0530

    Initial check in of the transport skeleton, and XML-RPC Resource implementation

---
+++ vumi/transports/mtn_rwanda/mtn_rwanda_ussd.py
@@ -0,0 +1,66 @@
+from twisted.internet import reactor
+from twisted.web import xmlrpc, server
+
+from vumi import log
+from vumi.transports.base import Transport
+from vumi.config import ConfigText
+
+class MTNRwandaUSSDTransportConfig(Transport.CONFIG_CLASS):
+    """
+    MTN Rwanda USSD transport configuration.
+    """
+    # TODO: Configure elements described in chapter 8 of the spec
+
+
+class MTNRwandaUSSDTransport(Transport):
+    """
+
+    """
+
+    transport_type = 'ussd'
+
+    CONFIG_CLASS = MTNRwandaUSSDTransportConfig
+
+    def setup_transport(self):
+        """
+        Transport specific setup.
+        """
+
+    def teardown_transport(self):
+        """
+        Clean-up of setup done in setup_transport.
+        """
+
+    def handle_outbound_message(self):
+        """
+        Read outbound message and do what needs to be done with them.
+        """
+
+    def handle_raw_inbound_message(self):
+        """
+        Called by the XML-RPC server when it receives a payload that
+        needs processing.
+        """
+
+
+class MTNRwandaXMLRPCResource(xmlrpc.XMLRPC):
+    """
+    A Resource object implementing XML-RPC, can be published using
+    twisted.web.server.Site.
+    """
+
+    def __init__(self, transport):
+        self.transport = transport
+        xmlrpc.XMLRPC.__init__(self)
+
+    def xmlrpc_(self, request, request_id=None):
+        request_id = request_id or Transport.generate_message_id()
+        request.setHeader("content-type", self.transport.content_type)
+        self.transport.set_request(request_id, request)
+        self.transport.handle_raw_inbound_message(request_id, request)
+        return server.NOT_DONE_YET
+
+    def xmlrpc_heathResource(self, request):
+        request.setResponseCode(http.OK)
+        request.do_not_log = True
+        return self.transport.get_health_response()
