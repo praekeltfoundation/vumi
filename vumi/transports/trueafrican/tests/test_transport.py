c00bcbd524e1810a8c110077da150b188a08e574
Vincent Geddes <vincent@praekeltconsulting.com>
2013-10-21 11:26:47 +0200

    add more tests (currently failing!)

--- vumi/transports/trueafrican/tests/test_transport.py
+++ vumi/transports/trueafrican/tests/test_transport.py
@@ -13,13 +13,6 @@ class TestTrueAfricanUssdTransport(TransportTestCase):
     transport_name = 'trueafrican_ussd'
     transport_class = TrueAfricanUssdTransport
 
-    def get_transport_url(self, transport):
-        """
-        Get the URL for the HTTP resource. Requires the worker to be started.
-        """
-        addr = transport.web_service._getPort().getHost()
-        return "http://%s:%s/" % (addr.host, addr.port)
-
     @inlineCallbacks
     def setUp(self):
         yield super(TestTrueAfricanUssdTransport, self).setUp()
@@ -28,6 +21,55 @@ class TestTrueAfricanUssdTransport(TransportTestCase):
             'server_port': 0,
         }
         self.transport = yield self.get_transport(self.config)
+        self.service_url = self.get_service_url(self.transport)
 
-    def test_foo(self):
-        pass
+    def get_service_url(self, transport):
+        """
+        Get the URL for the HTTP resource. Requires the worker to be started.
+        """
+        addr = transport.web_service._getPort().getHost()
+        return "http://%s:%s/" % (addr.host, addr.port)
+
+    def service_client(self):
+        return Proxy(self.service_url)
+
+    def reply_to_message(self, *args, **kw):
+        """
+        Fake a response from an application worker
+        """
+        d = self.wait_for_dispatched_messages(1)
+
+        def reply(r):
+            msg = TransportUserMessage(**r[0].payload)
+            self.dispatch(msg.reply(*args, **kw))
+            return msg
+
+        return d.addCallback(reply)
+
+    @inlineCallbacks
+    def test_session_init(self):
+        client = self.service_client()
+        resp_d = client.callRemote('USSD.INIT',
+                                   {'session': '32423',
+                                    'msisdn': '+27724385170',
+                                    'shortcode': '*23#'})
+        yield self.reply_to_message("Hello!")
+        resp = yield resp_d
+        self.assertEquals(
+            resp,
+            {
+                'message': 'Hello!',
+                'session': '32423',
+                'type': 'cont'
+            }
+        )
+
+    @inlineCallbacks
+    def test_nack(self):
+        msg = self.mkmsg_out()
+        yield self.dispatch(msg)
+        [nack] = yield self.wait_for_dispatched_events(1)
+        self.assertEqual(nack['user_message_id'], msg['message_id'])
+        self.assertEqual(nack['sent_message_id'], msg['message_id'])
+        self.assertEqual(nack['nack_reason'],
+                         'Missing in_reply_to, content or session_id')
