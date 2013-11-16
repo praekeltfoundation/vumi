802e3adcbadebcf739770c2c28d1c66dce244080
Simon de Haan <simon@praekeltfoundation.org>
2013-11-16 08:48:56 +0200

    connect to redis

--- vumi/middleware/tests/test_tracing.py
+++ vumi/middleware/tests/test_tracing.py
@@ -1,13 +1,22 @@
+from twisted.internet.defer import inlineCallbacks
+
 from vumi.middleware.tracing import TracingMiddleware
-from vumi.tests.helpers import VumiTestCase
+from vumi.application.tests.utils import ApplicationTestCase
+from vumi.application.tests.test_base import DummyApplicationWorker
 
 
-class TracingMiddlewareTestCase(VumiTestCase):
+class TracingMiddlewareTestCase(ApplicationTestCase):
 
     use_riak = False
+    application_class = DummyApplicationWorker
 
+    @inlineCallbacks
     def setUp(self):
-        self.mw = TracingMiddleware("tracing", {}, object())
+        yield super(TracingMiddlewareTestCase, self).setUp()
+        dummy_worker = yield self.get_application({})
+        self.mw = TracingMiddleware(
+            "tracing", self.mk_config({}), dummy_worker)
+        yield self.mw.setup_middleware()
 
     def test_something(self):
         pass
--- vumi/middleware/tracing.py
+++ vumi/middleware/tracing.py
@@ -2,17 +2,37 @@
 
 from twisted.internet.defer import inlineCallbacks, returnValue
 
+from vumi.persist.txredis_manager import TxRedisManager
+from vumi.config import Config, ConfigDict
 from vumi.middleware.base import BaseMiddleware
 
 
-class TracingMiddleware(BaseMiddleware):
+class BaseMiddlewareWithConfig(BaseMiddleware):
+
+    CONFIG_CLASS = Config
+
+    def __init__(self, name, config, worker):
+        self.name = name
+        self.config = self.CONFIG_CLASS(config, static=True)
+        self.worker = worker
+
+
+class TracingMiddlewareConfig(BaseMiddlewareWithConfig.CONFIG_CLASS):
+
+    redis_manager = ConfigDict("Redis Config", static=True)
+
+
+class TracingMiddleware(BaseMiddlewareWithConfig):
     """
     Middleware for tracing all the hops and associated timestamps
     a message took when being routed through the system
     """
+    CONFIG_CLASS = TracingMiddlewareConfig
 
+    @inlineCallbacks
     def setup_middleware(self):
-        pass
+        self.redis = yield TxRedisManager.from_config(
+            self.config.redis_manager)
 
     def teardown_middleware(self):
         pass
