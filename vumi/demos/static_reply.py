18dee6e2cff2e165c214fca03c1598efaf06d0f2
Simon de Haan <simon@praekeltfoundation.org>
2013-11-18 16:08:55 +0200

    using dynamic config (thanks @jerith)

--- vumi/demos/static_reply.py
+++ vumi/demos/static_reply.py
@@ -1,11 +1,13 @@
 # -*- test-case-name: vumi.demos.tests.test_static_reply -*-
+from twisted.internet.defer import succeed, inlineCallbacks, returnValue
+
 from vumi.application import ApplicationWorker
 from vumi.config import ConfigText
 
 
 class StaticReplyConfig(ApplicationWorker.CONFIG_CLASS):
     reply_text = ConfigText(
-        "Reply text to send in response to inbound messages.", static=True)
+        "Reply text to send in response to inbound messages.", static=False)
 
 
 class StaticReplyApplication(ApplicationWorker):
@@ -14,8 +16,16 @@ class StaticReplyApplication(ApplicationWorker):
     """
     CONFIG_CLASS = StaticReplyConfig
 
+    def get_config(self, message, ctxt=None):
+        config = self.config.copy()
+        if 'reply_text' in config:
+            config['reply_text'] = '%s %s' % (
+                config['reply_text'], message.user())
+        return succeed(self.CONFIG_CLASS(config))
+
+    @inlineCallbacks
     def consume_user_message(self, message):
-        config = self.get_static_config()
+        config = yield self.get_config(message)
         if config.reply_text:
-            return self.reply_to(
+            yield self.reply_to(
                 message, config.reply_text, continue_session=False)
--- vumi/demos/tests/test_static_reply.py
+++ vumi/demos/tests/test_static_reply.py
@@ -6,19 +6,21 @@ from vumi.demos.static_reply import StaticReplyApplication
 
 class TestStaticReplyApplication(ApplicationTestCase):
     application_class = StaticReplyApplication
+    timeout = 1
 
     @inlineCallbacks
     def test_receive_message(self):
         yield self.get_application(config={
             'reply_text': 'Your message is important to us.',
         })
-        yield self.dispatch(self.mkmsg_in())
-        [reply] = yield self.get_dispatched_messages()
-        self.assertEqual('Your message is important to us.', reply['content'])
+        yield self.dispatch(self.mkmsg_in(from_addr='from_addr'))
+        [reply] = self.get_dispatched_messages()
+        self.assertEqual('Your message is important to us. from_addr',
+                         reply['content'])
         self.assertEqual(u'close', reply['session_event'])
 
     @inlineCallbacks
     def test_receive_message_no_reply(self):
         yield self.get_application(config={})
         yield self.dispatch(self.mkmsg_in())
-        self.assertEqual([], (yield self.get_dispatched_messages()))
+        self.assertEqual([], self.get_dispatched_messages())
