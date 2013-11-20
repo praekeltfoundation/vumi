591ee31fc8d023105ec0d9daa2ae4ed510c689c4
Simon de Haan <simon@praekeltfoundation.org>
2013-11-20 16:05:01 +0200

    tracing hops

--- vumi/middleware/tests/test_tracing.py
+++ vumi/middleware/tests/test_tracing.py
@@ -1,9 +1,10 @@
 from twisted.internet.defer import inlineCallbacks, returnValue
 from twisted.internet.task import Clock
 
-from vumi.middleware.tracing import TracingMiddleware
+from vumi.middleware.tracing import TracingMiddleware, msg_key
 from vumi.application.tests.utils import ApplicationTestCase
 from vumi.application.tests.test_base import DummyApplicationWorker
+from vumi.tests.helpers import MessageHelper
 
 
 class TracingMiddlewareTestCase(ApplicationTestCase):
@@ -18,6 +19,7 @@ class TracingMiddlewareTestCase(ApplicationTestCase):
         yield super(TracingMiddlewareTestCase, self).setUp()
         self.patch(
             TracingMiddleware, 'get_clock', lambda *a: self.clock)
+        self.msg_helper = MessageHelper()
 
     @inlineCallbacks
     def mk_mw(self, name):
@@ -30,5 +32,70 @@ class TracingMiddlewareTestCase(ApplicationTestCase):
         returnValue(mw)
 
     @inlineCallbacks
-    def test_something(self):
-        mw1 = yield self.mk_mw('app1')
+    def test_trace_mo_mt_reply(self):
+        mo = self.msg_helper.make_inbound('hello')
+        msisdn = mo['from_addr']
+        mt = self.msg_helper.make_reply(mo, 'hi there')
+        ack = self.msg_helper.make_ack(mt)
+        dr = self.msg_helper.make_delivery_report(mt)
+
+        mw = yield self.mk_mw('app1')
+        yield mw.handle_inbound(mo, 'default')
+        self.clock.advance(10)
+        yield mw.handle_outbound(mt, 'default')
+        self.clock.advance(1)
+        yield mw.handle_event(ack, 'default')
+        self.clock.advance(1)
+        mw.handle_event(dr, 'default')
+
+        mo_trace = yield mw.get_trace(mo['message_id'])
+        ttl = yield mw.redis.ttl(msg_key(mo['message_id']))
+        self.assertTrue(0 < ttl <= mw.config.lifetime)
+
+        # trace of the inbound message
+        [_mo, _mt] = mo_trace
+        self.assertEqual(_mo, {
+            'transport_name': 'app1_transport',
+            'direction': 'inbound',
+            'content': 'hello',
+            'connector_name': 'default',
+            'time': 0.0,
+            'message_type': 'user_message',
+            'message_id': mo['message_id'],
+        })
+        self.assertEqual(_mt, {
+            'transport_name': 'app1_transport',
+            'direction': 'outbound',
+            'content': 'hi there',
+            'reply_message_id': mt['message_id'],
+            'connector_name': 'default',
+            'time': 10.0,
+            'message_type': 'user_message',
+        })
+
+        mt_trace = yield mw.get_trace(mt['message_id'])
+        [_mt, _ack, _dr] = mt_trace
+        self.assertEqual(_mt, {
+            'transport_name': 'app1_transport',
+            'direction': 'outbound',
+            'content': 'hi there',
+            'connector_name': 'default',
+            'time': 10.0, 'message_type':
+            'user_message',
+            'message_id': mt['message_id'],
+        })
+        self.assertEqual(_ack, {
+            'event_id': ack['event_id'],
+            'transport_name': 'app1_transport',
+            'message_type': 'event',
+            'event_type': 'ack',
+            'time': 11.0,
+        })
+        self.assertEqual(_dr, {
+            'transport_name': 'app1_transport',
+            'event_type': 'delivery_report',
+            'event_id': dr['event_id'],
+            'time': 12.0,
+            'delivery_status': 'delivered',
+            'message_type': 'event',
+        })
--- vumi/middleware/tracing.py
+++ vumi/middleware/tracing.py
@@ -1,10 +1,12 @@
 # -*- test-case-name: vumi.middleware.tests.test_tracing -*-
+import time
+import json
 
-from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.internet.defer import inlineCallbacks
 from twisted.internet import reactor
 
 from vumi.persist.txredis_manager import TxRedisManager
-from vumi.config import Config, ConfigDict
+from vumi.config import Config, ConfigDict, ConfigInt
 from vumi.middleware.base import BaseMiddleware
 
 
@@ -21,6 +23,12 @@ class BaseMiddlewareWithConfig(BaseMiddleware):
 class TracingMiddlewareConfig(BaseMiddlewareWithConfig.CONFIG_CLASS):
 
     redis_manager = ConfigDict("Redis Config", static=True)
+    lifetime = ConfigInt('How long to keep a trace for in seconds',
+                         static=True, default=600)
+
+
+def msg_key(message_id):
+    return ':'.join(['trace', message_id])
 
 
 class TracingMiddleware(BaseMiddlewareWithConfig):
@@ -35,6 +43,8 @@ class TracingMiddleware(BaseMiddlewareWithConfig):
     def setup_middleware(self):
         self.redis = yield TxRedisManager.from_config(
             self.config.redis_manager)
+        self.transport_name = self.worker.transport_name
+        self.clock = self.get_clock()
 
     def get_clock(self):
         return self.clock
@@ -42,11 +52,65 @@ class TracingMiddleware(BaseMiddlewareWithConfig):
     def teardown_middleware(self):
         return self.redis.close()
 
+    def store_hop(self, message_id, data):
+        data.setdefault('time', self.clock.seconds())
+        data.setdefault('transport_name', self.transport_name)
+        key = msg_key(message_id)
+        d = self.redis.rpush(key, json.dumps(data))
+        d.addCallback(lambda _: self.redis.expire(key, self.config.lifetime))
+        return d
+
+    def store_msg_hop(self, message, connector_name, direction):
+        d = self.store_hop(message['message_id'], {
+            'message_id': message['message_id'],
+            'message_type': message['message_type'],
+            'direction': direction,
+            'connector_name': connector_name,
+            'content': message['content'],
+        })
+        d.addCallback(lambda _: message)
+        return d
+
+    def store_event_hop(self, event, connector_name):
+        data = {
+            'message_type': event['message_type'],
+            'event_id': event['event_id'],
+            'event_type': event['event_type'],
+        }
+
+        if data['event_type'] == 'nack':
+            data['nack_reason'] = event['nack_reason']
+        elif data['event_type'] == 'delivery_report':
+            data['delivery_status'] = event['delivery_status']
+
+        d = self.store_hop(event['user_message_id'], data)
+        d.addCallback(lambda _: event)
+        return d
+
     def handle_inbound(self, message, connector_name):
-        pass
+        return self.store_msg_hop(message, connector_name, 'inbound')
 
     def handle_outbound(self, message, connector_name):
-        pass
+        d = self.store_msg_hop(message, connector_name, 'outbound')
+        d.addCallback(self._link_reply, connector_name)
+        return d
+
+    def _link_reply(self, message, connector_name):
+        in_reply_to = message.get('in_reply_to')
+        if in_reply_to is not None:
+            return self.store_hop(in_reply_to, {
+                'message_type': message['message_type'],
+                'direction': 'outbound',
+                'connector_name': connector_name,
+                'reply_message_id': message['message_id'],
+                'content': message['content'],
+            })
+        return message
 
     def handle_event(self, message, connector_name):
-        pass
+        return self.store_event_hop(message, connector_name)
+
+    def get_trace(self, message_id):
+        d = self.redis.lrange(msg_key(message_id), 0, 10)
+        d.addCallback(lambda hops: [json.loads(hop) for hop in hops])
+        return d
