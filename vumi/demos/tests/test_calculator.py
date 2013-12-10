53f09edd321af0ab022098e41c47ab95d678fb92
Simon de Haan <simon@praekeltfoundation.org>
2013-12-10 21:33:49 +0200

    simple calculator app for testing IVR transports with

--- vumi/demos/tests/test_calculator.py
+++ vumi/demos/tests/test_calculator.py
@@ -2,11 +2,13 @@ from twisted.internet.defer import inlineCallbacks
 
 from vumi.tests.helpers import VumiTestCase
 from vumi.application.tests.helpers import ApplicationHelper
-
 from vumi.demos.calculator import CalculatorApp
+from vumi.message import TransportUserMessage
 
 
 class TestCalculatorApp(VumiTestCase):
+
+    timeout = 1
 
     @inlineCallbacks
     def setUp(self):
@@ -14,5 +16,60 @@ class TestCalculatorApp(VumiTestCase):
         self.add_cleanup(self.app_helper.cleanup)
         self.worker = yield self.app_helper.get_application({})
 
-    def test_something(self):
-        self.assertTrue(self.worker)
+    @inlineCallbacks
+    def test_session_start(self):
+        msg = yield self.app_helper.make_dispatch_inbound(
+            None, session_event=TransportUserMessage.SESSION_NEW)
+        [resp] = yield self.app_helper.wait_for_dispatched_outbound(1)
+        self.assertTrue(
+            resp['content'].startswith('What would you like to do?'))
+
+    @inlineCallbacks
+    def test_first_number(self):
+        msg = yield self.app_helper.make_dispatch_inbound(
+            '1', session_event=TransportUserMessage.SESSION_RESUME)
+        [resp] = yield self.app_helper.wait_for_dispatched_outbound(1)
+        self.assertEqual(resp['content'], 'What is the first number?')
+
+    @inlineCallbacks
+    def test_second_number(self):
+        self.worker.save_session('+41791234567', {
+            'action': 1,
+        })
+        msg = yield self.app_helper.make_dispatch_inbound(
+            '1', session_event=TransportUserMessage.SESSION_RESUME)
+        [resp] = yield self.app_helper.wait_for_dispatched_outbound(1)
+        self.assertEqual(resp['content'], 'What is the second number?')
+
+    @inlineCallbacks
+    def test_action(self):
+        self.worker.save_session('+41791234567', {
+            'action': 0,  # add
+            'first_number': 2,
+        })
+        msg = yield self.app_helper.make_dispatch_inbound(
+            '2', session_event=TransportUserMessage.SESSION_RESUME)
+        [resp] = yield self.app_helper.wait_for_dispatched_outbound(1)
+        self.assertEqual(resp['content'], 'The result is: 4.')
+        self.assertEqual(resp['session_event'],
+                         TransportUserMessage.SESSION_CLOSE)
+
+    @inlineCallbacks
+    def test_invalid_input(self):
+        self.worker.save_session('+41791234567', {
+            'action': 0,  # add
+        })
+        msg = yield self.app_helper.make_dispatch_inbound(
+            'not-an-int', session_event=TransportUserMessage.SESSION_RESUME)
+        [resp] = yield self.app_helper.wait_for_dispatched_outbound(1)
+        self.assertEqual(resp['content'], 'Sorry invalid input!')
+        self.assertEqual(resp['session_event'],
+                         TransportUserMessage.SESSION_CLOSE)
+
+    @inlineCallbacks
+    def test_invalid_action(self):
+        msg = yield self.app_helper.make_dispatch_inbound(
+            'not-an-option', session_event=TransportUserMessage.SESSION_RESUME)
+        [resp] = yield self.app_helper.wait_for_dispatched_outbound(1)
+        self.assertTrue(
+            resp['content'].startswith('Sorry invalid input!'))
