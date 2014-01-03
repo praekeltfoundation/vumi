efb0dcb96974b67cd9af94078689690c59123701
justinvdm <justinvdm.0.1.0@gmail.com>
2014-01-03 17:24:40 +0200

    Start of twitter transport changeover from twitty-twister to txTwitter (getting tracking working)

--- vumi/transports/twitter/tests/test_twitter.py
+++ vumi/transports/twitter/tests/test_twitter.py
@@ -1,6 +1,5 @@
-from twisted.internet.defer import inlineCallbacks
-from twisted.internet import defer
-from twisted.web import error
+from twisted.internet.defer import inlineCallbacks, succeed
+from txtwitter.twitter import TwitterClient
 
 from vumi.message import TransportUserMessage
 from vumi.tests.helpers import VumiTestCase
@@ -8,114 +7,70 @@ from vumi.transports.twitter import TwitterTransport
 from vumi.transports.tests.helpers import TransportHelper
 
 
-class Thing(object):
-    """This is what ruby taught me"""
-    def __init__(self, **kwargs):
-        self.kwargs = kwargs
+class FakeTwitterStreamService(object):
+    def __init__(self, delegate):
+        self.delegate = delegate
 
-    def __getattr__(self, attr):
-        return self.kwargs[attr]
+    def startService(self):
+        pass
+
+    def stopService(self):
+        pass
+
+    def respond_with(self, data):
+        self.delegate(data)
 
 
-class FakeTwitter(object):
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
+class FakeTwitterClient(TwitterClient):
+    def __init__(self, *a, **kw):
+        super(FakeTwitterClient, self).__init__(*a, **kw)
+        self.updates = []
+        self.update_response = None
+        self.stream_filters = []
+
+    def set_update_response(self, resp):
+        self.update_response = resp
 
     def update(self, content):
-        if self.raise_update_error:
-            return defer.fail(error.Error(503, 'Fail Whale'))
-        else:
-            return defer.succeed('post-id')
+        self.updates.append(content)
+        return succeed(self.update_response)
 
-    def track(self, delegate, terms):
-        self.track_delegate = delegate
-        self.track_terms = terms
+    def stream_filter(self, delegate, track=None):
+        return FakeTwitterStreamService(delegate)
 
-    def replies(self, delegate, params={}, extra_args=None):
-        self.replies_delegate = delegate
-        self.replies_params = params
-        self.replies_extra_args = extra_args
 
-    def send_fake_replies(self, *replies):
-        for reply in replies:
-            self.replies_delegate(reply)
-
-    def send_fake_track(self, *tracks):
-        for status in tracks:
-            self.track_delegate(status)
+class StubbedTwitterTransport(TwitterTransport):
+    CLIENT_CLS = FakeTwitterClient
 
 
 class TestTwitterTransport(VumiTestCase):
-
     @inlineCallbacks
     def setUp(self):
-        config = {
-            'app_name': 'testapp',
+        self.tx_helper = self.add_helper(
+            TransportHelper(StubbedTwitterTransport))
+
+        self.transport = yield self.tx_helper.get_transport({
             'consumer_key': 'consumer1',
             'consumer_secret': 'consumersecret1',
             'access_token': 'token1',
             'access_token_secret': 'tokensecret1',
             'terms': ['some', 'trending', 'topic'],
-        }
-        self.tx_helper = self.add_helper(TransportHelper(TwitterTransport))
-        self.transport = yield self.tx_helper.get_transport(
-            config, start=False)
-        self.transport._twitter_class = FakeTwitter
-        yield self.transport.startWorker()
-
-    @inlineCallbacks
-    def test_handle_replies(self):
-        reply = Thing(
-            id=1,
-            published=1,
-            text='@tweeter hi there',
-            title='tweeter',
-            author=Thing(screen_name='replier'),
-            raw={
-                'some': 'raw',
-                'fields': 'and values',
-            }
-        )
-        self.transport.twitter.send_fake_replies(reply)
-        [msg] = yield self.tx_helper.wait_for_dispatched_inbound(1)
-        self.assertEqual(msg['from_addr'], 'replier')
-        self.assertEqual(msg['to_addr'], 'tweeter')
-        self.assertEqual(msg['content'], '@tweeter hi there')
-        self.assertEqual(msg['session_event'],
-                         TransportUserMessage.SESSION_RESUME)
-        self.assertEqual(msg['message_id'], 1)
-        last_reply_timestamp = yield self.transport.redis.get(
-            'last_reply_timestamp')
-        self.assertEqual(last_reply_timestamp, '1')
+        })
 
     @inlineCallbacks
     def test_handle_track(self):
-        status = Thing(
-            id=1,
-            text='text',
-            in_reply_to_screen_name='@reply_to',
-            user=Thing(screen_name='@screen_name'),
-            raw={
-                'some': 'raw',
-                'fields': 'and values',
-            }
-        )
-        self.transport.twitter.send_fake_track(status)
+        self.transport.track_stream.respond_with({
+            'id_str': 1,
+            'text': 'text',
+            'in_reply_to_screen_name': '@reply_to',
+            'user': {'screen_name': '@screen_name'},
+        })
+
         [msg] = yield self.tx_helper.wait_for_dispatched_inbound(1)
+
         self.assertEqual(msg['from_addr'], '@screen_name')
         self.assertEqual(msg['to_addr'], '@reply_to')
         self.assertEqual(msg['content'], 'text')
         self.assertEqual(msg['message_id'], '1')
         self.assertEqual(msg['session_event'],
                          TransportUserMessage.SESSION_NONE)
-
-    @inlineCallbacks
-    def test_nack(self):
-        self.transport.twitter.raise_update_error = True
-        msg = yield self.tx_helper.make_dispatch_outbound("outbound")
-        [nack] = yield self.tx_helper.wait_for_dispatched_events(1)
-        self.assertEqual(nack['user_message_id'], msg['message_id'])
-        self.assertEqual(nack['sent_message_id'], msg['message_id'])
-        self.assertEqual(nack['nack_reason'],
-            '503 Fail Whale')
--- vumi/transports/twitter/twitter.py
+++ vumi/transports/twitter/twitter.py
@@ -4,12 +4,32 @@ from twisted.python import log
 from twisted.internet.defer import inlineCallbacks
 from twisted.internet import task
 from twisted.web import error
-from twittytwister import twitter
-from oauth import oauth
+from txtwitter.twitter import TwitterClient
 
 from vumi.transports.base import Transport
+from vumi.config import ConfigText, ConfigList, ConfigInt
 from vumi.message import TransportUserMessage
-from vumi.persist.txredis_manager import TxRedisManager
+
+
+class TwitterTransportConfig(Transport.CONFIG_CLASS):
+    consumer_key = ConfigText(
+        "The OAuth consumer key for the twitter account",
+        required=True, static=True)
+    consumer_secret = ConfigText(
+        "The OAuth consumer secret for the twitter account",
+        required=True, static=True)
+    access_token = ConfigText(
+        "The OAuth access token for the twitter account",
+        required=True, static=True)
+    access_token_secret = ConfigText(
+        "The OAuth access token secret for the twitter account",
+        required=True, static=True)
+    terms = ConfigList(
+        "A list of terms to be tracked by the transport",
+        default=[], static=True)
+    check_replies_interval = ConfigInt(
+        "How often (in seconds) to poll for replies to status updates",
+        default=0, static=True)
 
 
 class TwitterTransport(Transport):
@@ -17,100 +37,75 @@ class TwitterTransport(Transport):
 
     transport_type = 'twitter'
 
-    _twitter_class = twitter.TwitterFeed
+    CONFIG_CLASS = TwitterTransportConfig
+    ENCODING = 'utf8'
+    CLIENT_CLS = TwitterClient
 
-    def validate_config(self):
-        self.consumer_key = self.config['consumer_key']
-        self.consumer_secret = self.config['consumer_secret']
-        self.access_token = self.config['access_token']
-        self.access_token_secret = self.config['access_token_secret']
-        self.r_config = self.config.get('redis_manager', {})
-        self.r_prefix = "%(transport_name)s@%(app_name)s:replies" % self.config
-        self.terms = set(self.config.get('terms'))
-        self.check_replies_interval = int(self.config.get(
-                            'check_replies_interval', 60))
+    @classmethod
+    def decode(cls, s):
+        if not isinstance(s, basestring):
+            s = str(s)
+        return s.decode(cls.ENCODING)
 
-    @inlineCallbacks
     def setup_transport(self):
-        redis = yield TxRedisManager.from_config(self.r_config)
-        self.redis = redis.sub_manager(self.r_prefix)
-        consumer = oauth.OAuthConsumer(self.consumer_key, self.consumer_secret)
-        token = oauth.OAuthToken(self.access_token, self.access_token_secret)
-        self.twitter = self._twitter_class(consumer=consumer, token=token)
-        yield self.start_tracking_terms()
-        if self.check_replies_interval > 0:
-            self.start_checking_for_replies()
+        config = self.get_static_config()
+        self.terms = config.terms
+
+        self.client = self.CLIENT_CLS(
+            config.access_token,
+            config.access_token_secret,
+            config.consumer_key,
+            config.consumer_secret)
+
+        self.track_stream = self.client.stream_filter(
+            self.handle_track, self.terms)
+        if self.terms:
+            self.track_stream.startService()
+
+        self.check_replies = task.LoopingCall(self.check_for_replies)
+        if config.check_replies_interval > 0:
+            self.check_replies.start(config.check_replies_interval)
 
     @inlineCallbacks
-    def start_tracking_terms(self):
-        if self.terms:
-            self.stream = yield self.twitter.track(self.handle_track,
-                                                   self.terms)
-
-    def start_checking_for_replies(self):
-        self.check_replies = task.LoopingCall(self.check_for_replies)
-        self.check_replies.start(self.check_replies_interval)
-
     def teardown_transport(self):
         if self.check_replies.running:
             self.check_replies.stop()
-        return self.redis._close()
+
+        yield self.track_stream.stopService()
 
     def check_for_replies(self):
-        return self.twitter.replies(self.handle_replies)
+        #TODO
+        pass
 
     @inlineCallbacks
     def handle_outbound_message(self, message):
-        """
-        TODO:   Add in_reply_to_status_id parameter if present,
-                need access to the Twitter docs to do so at the
-                moment.
-        """
+        # TODO Use in_reply_to_status_id once txTwitter supports it
         log.msg("Twitter transport sending %r" % (message,))
-        try:
-            post_id = yield self.twitter.update(message['content'])
-            yield self.publish_ack(user_message_id=message['message_id'],
-                                sent_message_id=post_id)
-        except error.Error, e:
-            yield self.publish_nack(user_message_id=message['message_id'],
-                                sent_message_id=message['message_id'],
-                                reason=str(e))
 
-    @inlineCallbacks
-    def handle_replies(self, message):
-        """
-        handle_replies is called at a regular interval to check for replies
-        that are received on the given account. Attached the SESSION_RESUME
-        event type to the messages to keep them distinguishable from messages
-        arriving by tracking terms in realtime.
-        """
-        last_reply_timestamp = yield self.redis.get('last_reply_timestamp')
-        if (last_reply_timestamp is None or
-                message.published > last_reply_timestamp):
-            self.publish_message(
-                message_id=message.id,
-                content=message.text,
-                to_addr=message.title,
-                from_addr=message.author.screen_name,
-                session_event=TransportUserMessage.SESSION_RESUME,
-                transport_type=self.transport_type,
-                transport_metadata=message.raw,
-            )
-            yield self.redis.set('last_reply_timestamp', message.published)
+        try:
+            response = yield self.client.update(message['content'])
+
+            yield self.publish_ack(
+                user_message_id=message['message_id'],
+                sent_message_id=response['id_str'])
+        except error.Error, e:
+            yield self.publish_nack(
+                user_message_id=message['message_id'],
+                sent_message_id=message['message_id'],
+                reason=str(e))
 
     def handle_track(self, status):
         """
-        Get hits with a status update whenever a tweet matching
+        Gets called with a status update whenever a tweet matching
         a term being tracked is detected. Attached the SESSION_NONE
         event type as these messages aren't necessarily part of a
         conversation.
         """
         self.publish_message(
-            message_id=unicode(status.id),
-            content=status.text,
-            to_addr=status.in_reply_to_screen_name or '',
-            from_addr=status.user.screen_name,
+            message_id=self.decode(status['id_str']),
+            content=self.decode(status['text']),
+            to_addr=self.decode(status.get('in_reply_to_screen_name', '')),
+            from_addr=self.decode(status['user']['screen_name']),
             session_event=TransportUserMessage.SESSION_NONE,
             transport_type=self.transport_type,
-            transport_metadata=status.raw,
-        )
+            transport_metadata=status)
