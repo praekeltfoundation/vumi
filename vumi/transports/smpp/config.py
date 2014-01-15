987514b2850a947fc095f6fd8f06177e9ae473fd
Jeremy Thurgood <jeremy@praekelt.com>
2014-01-15 11:31:02 +0200

    Clean up SMPP config help text.

--- vumi/transports/smpp/config.py
+++ vumi/transports/smpp/config.py
@@ -13,58 +13,54 @@ class SmppTransportConfig(Transport.CONFIG_CLASS):
         'The SMPP endpoint to connect to.',
         required=True, static=True)
     initial_reconnect_delay = ConfigInt(
-        'How long to wait between reconnecting attempts', default=5,
-        static=True)
+        'How long (in seconds) to wait between reconnecting attempts. '
+        'Defaults to 5 seconds.', default=5, static=True)
     throttle_delay = ConfigFloat(
         "Delay (in seconds) before retrying a message after receiving "
         "`ESME_RTHROTTLED` or `ESME_RMSGQFUL`.", default=0.1, static=True)
     submit_sm_expiry = ConfigInt(
-        'How long (seconds) to wait for the SMSC to return with a '
-        '`submit_sm_resp`. Defaults to 24 hours',
+        'How long (in seconds) to wait for the SMSC to return with a '
+        '`submit_sm_resp`. Defaults to 24 hours.',
         default=(60 * 60 * 24), static=True)
     third_party_id_expiry = ConfigInt(
-        'How long (seconds) to keep 3rd party message IDs around to allow for '
-        'matching submit_sm_resp and delivery report messages. Defaults to '
-        '1 week',
+        'How long (in seconds) to keep 3rd party message IDs around to allow '
+        'for matching submit_sm_resp and delivery report messages. Defaults '
+        'to 1 week.',
         default=(60 * 60 * 24 * 7), static=True)
     redis_manager = ConfigDict(
-        'How to connect to Redis', default={}, static=True)
+        'How to connect to Redis.', default={}, static=True)
     split_bind_prefix = ConfigText(
         "This is the Redis prefix to use for storing things like sequence "
         "numbers and message ids for delivery report handling. It defaults "
         "to `<system_id>@<transport_name>`. "
-        "*ONLY* if the connection is split into two separate binds for RX "
-        "and TX then make sure this is the same value for both binds. "
-        "This _only_ needs to be done for TX & RX since messages sent via "
+        "This should *ONLY* be done for TX & RX since messages sent via "
         "the TX bind are handled by the RX bind and they need to share the "
         "same prefix for the lookup for message ids in delivery reports to "
         "work.", default='', static=True)
     delivery_report_processor = ConfigClassName(
         'Which delivery report processor to use. '
-        'Should implement `IDeliveryReportProcessor`.',
+        'MUST implement `IDeliveryReportProcessor`.',
         default=('vumi.transports.smpp.processors.'
                  'DeliveryReportProcessor'),
         static=True, implements=IDeliveryReportProcessor)
     delivery_report_processor_config = ConfigDict(
-        'The configuration for the ``delivery_report_processor``',
+        'The configuration for the `delivery_report_processor`.',
         default={}, static=True)
     deliver_short_message_processor = ConfigClassName(
         'Which deliver short message processor to use. '
-        'Should implement `IDeliverShortMessageProcessor`.',
-        default=('vumi.transports.smpp.processors.'
-                 'DeliverShortMessageProcessor'),
+        'MUST implement `IDeliverShortMessageProcessor`.',
+        default='vumi.transports.smpp.processors.DeliverShortMessageProcessor',
         static=True, implements=IDeliverShortMessageProcessor)
     deliver_short_message_processor_config = ConfigDict(
-        'The configuration for the ``deliver_short_message_processor``',
+        'The configuration for the `deliver_short_message_processor`.',
         default={}, static=True)
     submit_short_message_processor = ConfigClassName(
         'Which submit short message processor to use. '
         'Should implements `ISubmitShortMessageProcessor`.',
-        default=('vumi.transports.smpp.processors.'
-                 'SubmitShortMessageProcessor'),
+        default='vumi.transports.smpp.processors.SubmitShortMessageProcessor',
         static=True, implements=ISubmitShortMessageProcessor)
     submit_short_message_processor_config = ConfigDict(
-        'The configuration for the ``submit_short_message_processor``',
+        'The configuration for the `submit_short_message_processor`.',
         default={}, static=True)
     system_id = ConfigText(
         'User id used to connect to the SMPP server.', required=True,
@@ -78,23 +74,25 @@ class SmppTransportConfig(Transport.CONFIG_CLASS):
         "SMPP protocol version. Default is '34' (i.e. version 3.4).",
         default="34", static=True)
     service_type = ConfigText(
-        'The SMPP service type', default="", static=True)
+        'The SMPP service type.', default="", static=True)
     dest_addr_ton = ConfigInt(
-        'Destination TON (type of number)', default=0, static=True)
+        'Destination TON (type of number).', default=0, static=True)
     dest_addr_npi = ConfigInt(
         'Destination NPI (number plan identifier). '
-        'Default 1 (ISDN/E.164/E.163)', default=1, static=True)
+        'Default 1 (ISDN/E.164/E.163).', default=1, static=True)
     source_addr_ton = ConfigInt(
-        'Source TON (type of number)', default=0, static=True)
+        'Source TON (type of number).', default=0, static=True)
     source_addr_npi = ConfigInt(
-        'Source NPI (number plan identifier)', default=0, static=True)
+        'Source NPI (number plan identifier).', default=0, static=True)
     registered_delivery = ConfigBool(
-        'Whether or not to request delivery reports', default=True,
-        static=True)
+        'Whether or not to request delivery reports. Default True.',
+        default=True, static=True)
     smpp_bind_timeout = ConfigInt(
-        'How long to wait for a succesful bind', default=30, static=True)
+        'How long (in seconds) to wait for a succesful bind. Default 30.',
+        default=30, static=True)
     smpp_enquire_link_interval = ConfigInt(
-        "Number of seconds to delay before reconnecting to the server after "
-        "being disconnected. Default is 5s. Some WASPs, e.g. Clickatell "
-        "require a 30s delay before reconnecting. In these cases a 45s "
-        "initial_reconnect_delay is recommended.", default=55, static=True)
+        "How long (in seconds) to delay before reconnecting to the server "
+        "after being disconnected. Some WASPs, e.g. Clickatell require a 30s "
+        "delay before reconnecting. In these cases a 45s "
+        "`initial_reconnect_delay` is recommended. Default 55.",
+        default=55, static=True)
