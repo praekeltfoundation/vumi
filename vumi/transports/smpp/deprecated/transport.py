90f11cabf9711b910e89a3b0f61b4a7a19f46498
Simon de Haan <simon@praekeltfoundation.org>
2014-01-15 16:46:43 +0200

    log deprecation warning in old implementation

--- vumi/transports/smpp/deprecated/transport.py
+++ vumi/transports/smpp/deprecated/transport.py
@@ -1,4 +1,5 @@
 # -*- test-case-name: vumi.transports.smpp.deprecated.tests.test_smpp -*-
+import warnings
 
 from twisted.internet import reactor
 from twisted.internet.defer import inlineCallbacks, returnValue
@@ -43,6 +44,10 @@ class SmppTransport(Transport):
 
     @inlineCallbacks
     def setup_transport(self):
+        warnings.warn(
+            'This SMPP implementation is deprecated. Please use the '
+            'implementations available in vumi.transports.smpp.'
+            'smpp_transport instead.', category=DeprecationWarning)
         config = self.get_static_config()
         log.msg("Starting the SmppTransport for %s" % (
             config.twisted_endpoint))
