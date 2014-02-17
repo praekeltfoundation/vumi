9eb361f2690ec3f8e36d28ef4da99785f4dec229
Simon Cross <hodgestar@gmail.com>
2014-02-17 13:08:48 +0200

    Fix PEP8 violation (@jerith).

--- vumi/components/message_store_resource.py
+++ vumi/components/message_store_resource.py
@@ -19,7 +19,7 @@ def chunks(l, n):
     """ Yield successive n-sized chunks from l.
     """
     for i in xrange(0, len(l), n):
-        yield l[i:i+n]
+        yield l[i:i + n]
 
 
 class MessageStoreProxyResource(Resource):
