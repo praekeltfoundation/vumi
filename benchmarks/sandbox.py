84a48645c0933757be59bd7ec0d7f65e32c5c8a5
Simon Cross <hodgestar@gmail.com>
2014-07-09 14:28:17 +0200

    Fancier timer.

--- benchmarks/sandbox.py
+++ benchmarks/sandbox.py
@@ -42,8 +42,32 @@ class BenchApp(JsSandbox):
         return "DUMMY_SANDBOX_ID"
 
 
+class Timer(object):
+    def __init__(self):
+        self.current_time = None
+        self.times = []
+
+    def __enter__(self, *args, **kw):
+        assert self.current_time is None
+        self.current_time = time.time()
+
+    def __exit__(self, *args, **kw):
+        assert self.current_time is not None
+        self.times.append(time.time() - self.current_time)
+        self.current_time = None
+
+    def total(self):
+        return sum(self.times)
+
+    def loops(self):
+        return len(self.times)
+
+    def mean(self):
+        return self.total() / self.loops()
+
+
 @inlineCallbacks
-def run_bench(loops=100):
+def run_bench(loops):
     opts = VumiOptions()
     opts.postOptions()
     worker_creator = WorkerCreator(opts.vumi_options)
@@ -73,29 +97,29 @@ def run_bench(loops=100):
     })
 
     yield transport.startService()
-    print "Waiting for transport ..."
+    log.msg("Waiting for transport ...")
     yield BenchTransport.WORKER_QUEUE.get()
 
     yield app.startService()
-    print "Waiting for worker ..."
+    log.msg("Waiting for worker ...")
     yield BenchApp.WORKER_QUEUE.get()
 
-    start = time.time()
+    print "Starting %d loops ..." % (loops,)
+    timer = Timer()
     for i in range(loops):
-        transport.publish_message(
-            content="Hi!",
-            to_addr="+1234",
-            from_addr="+5678",
-            transport_type="ussd",
-        )
-        reply = yield transport.message_queue.get()
-        log.msg(reply['message_id'])
+        with timer:
+            transport.publish_message(
+                content="Hi!",
+                to_addr="+1234",
+                from_addr="+5678",
+                transport_type="ussd",
+            )
+            reply = yield transport.message_queue.get()
+            log.msg(reply['message_id'])
 
-    elapsed = time.time() - start
-
-    print "Total time: %.2f" % (elapsed,)
-    print "Time per message: %g" % (elapsed / loops)
-    print "  Messages sent: %d" % (loops,)
+    print "Total time: %.2f" % timer.total()
+    print "Time per message: %g" % timer.mean()
+    print "  Loops: %d" % timer.loops()
 
     yield transport.stopService()
     yield app.stopService()
@@ -103,6 +127,6 @@ def run_bench(loops=100):
 
 
 if __name__ == "__main__":
-    #log.startLogging(sys.stdout)
+    # log.startLogging(sys.stdout)
     reactor.callLater(0, run_bench, loops=100)
     reactor.run()
