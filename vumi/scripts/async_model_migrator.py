2f052c52ea0c266c18383071c72aca1651fec732
Jeremy Thurgood <jeremy@praekelt.com>
2014-11-07 14:30:25 +0200

    Migrate objects in batches using paginated indexes.

--- vumi/scripts/async_model_migrator.py
+++ vumi/scripts/async_model_migrator.py
@@ -1,9 +1,9 @@
 # -*- test-case-name: vumi.scripts.tests.test_model_migrator -*-
 import sys
 
-from twisted.python import usage
+from twisted.internet.defer import inlineCallbacks, gatherResults, succeed
 from twisted.internet.task import react
-from twisted.internet.defer import inlineCallbacks
+from twisted.python import usage
 
 from vumi.utils import load_class_by_string
 from vumi.persist.txriak_manager import TxRiakManager
@@ -19,6 +19,8 @@ class Options(usage.Options):
         ["keys", None, None,
          "Migrate these specific keys rather than the whole bucket."
          " E.g. --keys 'foo,bar,baz'"],
+        ["batch-size", None, "20",
+         "The number of keys to process in each concurrent batch."],
     ]
 
     optFlags = [
@@ -35,26 +37,37 @@ class Options(usage.Options):
             raise usage.UsageError("Please specify a model class.")
         if self['bucket-prefix'] is None:
             raise usage.UsageError("Please specify a bucket prefix.")
+        self['batch-size'] = int(self['batch-size'])
 
 
 class ProgressEmitter(object):
-    """Report progress as a percentage to an emitter."""
+    """Report progress as the number of items processed to an emitter."""
 
-    def __init__(self, total, emit):
+    def __init__(self, emit, batch_size):
         self.emit = emit
-        self.total = total
-        self.percentage = 0
-
-    def _calculate_percentage(self, value):
-        if value == 0:
-            return 0
-        return int(value * 100.0 / self.total)
+        self.batch_size = batch_size
+        self.processed = 0
 
     def update(self, value):
-        old_percentage = self.percentage
-        self.percentage = self._calculate_percentage(value)
-        if self.percentage != old_percentage:
-            self.emit(self.percentage)
+        if (value / self.batch_size) > (self.processed / self.batch_size):
+            self.emit(value)
+        self.processed = value
+
+
+class FakeIndexPage(object):
+    def __init__(self, keys, batch_size):
+        self._keys = keys
+        self._batch_size = batch_size
+
+    def __iter__(self):
+        return iter(self._keys[:self._batch_size])
+
+    def has_next_page(self):
+        return len(self._keys) > self._batch_size
+
+    def next_page(self):
+        return succeed(
+            type(self)(self._keys[self._batch_size:], self._batch_size))
 
 
 class ModelMigrator(object):
@@ -86,26 +99,41 @@ class ModelMigrator(object):
             self.emit("Failed to migrate key %r:" % (key,))
             self.emit("  %s: %s" % (type(e).__name__, e))
 
+    def migrate_batch(self, keys, dry_run):
+        # Depending on our Riak client, Python version, and JSON library we may
+        # get bytes or unicode here.
+        keys = [k.decode('utf-8') if isinstance(k, str) else k for k in keys]
+        return gatherResults([self.migrate_key(key, dry_run) for key in keys])
+
     @inlineCallbacks
     def run(self):
         dry_run = self.options["dry-run"]
         if self.options["keys"] is not None:
             keys = self.options["keys"].split(",")
             self.emit("Migrating %d specified keys ..." % len(keys))
+            emit_progress = lambda t: self.emit(
+                "%s of %s objects migrated." % (t, len(keys)))
+            index_page = FakeIndexPage(keys, self.options["batch-size"])
         else:
-            keys = yield self.model.all_keys()
-            self.emit("%d keys found. Migrating ..." % len(keys))
-        # Depending on our Riak client, Python version, and JSON library we may
-        # get bytes or unicode here.
-        keys = [k.decode('utf-8') if isinstance(k, str) else k for k in keys]
-        progress = ProgressEmitter(
-            len(keys),
-            lambda p: self.emit("%s%% complete." % (p,))
-        )
-        for i, key in enumerate(keys):
-            yield self.migrate_key(key, dry_run)
-            progress.update(i)
-        self.emit("Done.")
+            self.emit("Migrating ...")
+            emit_progress = lambda t: self.emit(
+                "%s object%s migrated." % (t, "" if t == 1 else "s"))
+            index_page = yield self.model.all_keys_page(
+                max_results=self.options["batch-size"])
+
+        progress = ProgressEmitter(emit_progress, self.options["batch-size"])
+        processed = 0
+        while index_page is not None:
+            if index_page.has_next_page():
+                next_page_d = index_page.next_page()
+            else:
+                next_page_d = succeed(None)
+            batch_keys = list(index_page)
+            yield self.migrate_batch(batch_keys, dry_run)
+            processed += len(batch_keys)
+            progress.update(processed)
+            index_page = yield next_page_d
+        self.emit("Done, %s objects migrated." % (processed,))
 
 
 def main(name, *args):
--- vumi/scripts/tests/test_async_model_migrator.py
+++ vumi/scripts/tests/test_async_model_migrator.py
@@ -44,9 +44,11 @@ class TestModelMigrator(VumiTestCase):
             "-b", self.expected_bucket_prefix,
         ]
 
-    def make_migrator(self, args=None):
+    def make_migrator(self, args=None, batch_size=None):
         if args is None:
             args = self.default_args
+        if batch_size is not None:
+            args.extend(["--batch-size", str(batch_size)])
         options = Options()
         options.parseOptions(args)
         return StubbedModelMigrator(self, options)
@@ -97,11 +99,9 @@ class TestModelMigrator(VumiTestCase):
             "name",
             "-m", self.model_cls_path,
             "-b", self.riak_manager.bucket_prefix)
-        self.assertEqual(sys.stdout.getvalue(), "\n".join([
-            "3 keys found. Migrating ...",
-            "33% complete.", "66% complete.",
-            "Done.", "",
-        ]))
+        self.assertEqual(
+            sys.stdout.getvalue(),
+            "Migrating ...\nDone, 3 objects migrated.\n")
 
     @inlineCallbacks
     def test_successful_migration(self):
@@ -110,9 +110,38 @@ class TestModelMigrator(VumiTestCase):
         model_migrator = self.make_migrator()
         yield model_migrator.run()
         self.assertEqual(model_migrator.output, [
-            "3 keys found. Migrating ...",
-            "33% complete.", "66% complete.",
-            "Done.",
+            "Migrating ...",
+            "Done, 3 objects migrated.",
+        ])
+        self.assertEqual(sorted(loads), [u"key-%d" % i for i in range(3)])
+        self.assertEqual(sorted(stores), [u"key-%d" % i for i in range(3)])
+
+    @inlineCallbacks
+    def test_successful_migration_small_batches(self):
+        yield self.mk_simple_models(3)
+        loads, stores = self.record_load_and_store()
+        model_migrator = self.make_migrator(batch_size=2)
+        yield model_migrator.run()
+        self.assertEqual(model_migrator.output, [
+            "Migrating ...",
+            "2 objects migrated.",
+            "Done, 3 objects migrated.",
+        ])
+        self.assertEqual(sorted(loads), [u"key-%d" % i for i in range(3)])
+        self.assertEqual(sorted(stores), [u"key-%d" % i for i in range(3)])
+
+    @inlineCallbacks
+    def test_successful_migration_tiny_batches(self):
+        yield self.mk_simple_models(3)
+        loads, stores = self.record_load_and_store()
+        model_migrator = self.make_migrator(batch_size=1)
+        yield model_migrator.run()
+        self.assertEqual(model_migrator.output, [
+            "Migrating ...",
+            "1 object migrated.",
+            "2 objects migrated.",
+            "3 objects migrated.",
+            "Done, 3 objects migrated.",
         ])
         self.assertEqual(sorted(loads), [u"key-%d" % i for i in range(3)])
         self.assertEqual(sorted(stores), [u"key-%d" % i for i in range(3)])
@@ -132,11 +161,10 @@ class TestModelMigrator(VumiTestCase):
             self.assertTrue(("Skipping tombstone key u'key-%d'." % i)
                             in model_migrator.output)
         self.assertEqual(model_migrator.output[:1], [
-            "3 keys found. Migrating ...",
+            "Migrating ...",
         ])
-        self.assertEqual(model_migrator.output[-2:], [
-            "66% complete.",
-            "Done.",
+        self.assertEqual(model_migrator.output[-1:], [
+            "Done, 3 objects migrated.",
         ])
 
     @inlineCallbacks
@@ -157,11 +185,10 @@ class TestModelMigrator(VumiTestCase):
                 "  ValueError: Failed to load.",
             ) in line_pairs)
         self.assertEqual(model_migrator.output[:1], [
-            "3 keys found. Migrating ...",
+            "Migrating ...",
         ])
-        self.assertEqual(model_migrator.output[-2:], [
-            "66% complete.",
-            "Done.",
+        self.assertEqual(model_migrator.output[-1:], [
+            "Done, 3 objects migrated.",
         ])
 
     @inlineCallbacks
@@ -173,8 +200,7 @@ class TestModelMigrator(VumiTestCase):
         yield model_migrator.run()
         self.assertEqual(model_migrator.output, [
             "Migrating 2 specified keys ...",
-            "50% complete.",
-            "Done.",
+            "Done, 2 objects migrated.",
         ])
         self.assertEqual(sorted(loads), [u"key-1", u"key-2"])
         self.assertEqual(sorted(stores), [u"key-1", u"key-2"])
@@ -186,9 +212,8 @@ class TestModelMigrator(VumiTestCase):
         model_migrator = self.make_migrator(self.default_args + ["--dry-run"])
         yield model_migrator.run()
         self.assertEqual(model_migrator.output, [
-            "3 keys found. Migrating ...",
-            "33% complete.", "66% complete.",
-            "Done.",
+            "Migrating ...",
+            "Done, 3 objects migrated.",
         ])
         self.assertEqual(sorted(loads), [u"key-%d" % i for i in range(3)])
         self.assertEqual(sorted(stores), [])
