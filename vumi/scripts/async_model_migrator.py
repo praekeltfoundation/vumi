5649740857afc9d70655c79965dc912a9e0841d5
Jeremy Thurgood <jeremy@praekelt.com>
2014-11-07 13:14:03 +0200

    Async version of model migration script.

---
+++ vumi/scripts/async_model_migrator.py
@@ -0,0 +1,125 @@
+# -*- test-case-name: vumi.scripts.tests.test_model_migrator -*-
+import sys
+
+from twisted.python import usage
+from twisted.internet.task import react
+from twisted.internet.defer import inlineCallbacks
+
+from vumi.utils import load_class_by_string
+from vumi.persist.txriak_manager import TxRiakManager
+
+
+class Options(usage.Options):
+    optParameters = [
+        ["model", "m", None,
+         "Full Python name of the model class to migrate."
+         " E.g. 'vumi.components.message_store.InboundMessage'."],
+        ["bucket-prefix", "b", None,
+         "The bucket prefix for the Riak manager."],
+        ["keys", None, None,
+         "Migrate these specific keys rather than the whole bucket."
+         " E.g. --keys 'foo,bar,baz'"],
+    ]
+
+    optFlags = [
+        ["dry-run", None, "Don't save anything back to Riak."],
+    ]
+
+    longdesc = """Offline model migrator. Necessary for updating
+                  models when index names change so that old model
+                  instances remain findable by index searches.
+                  """
+
+    def postOptions(self):
+        if self['model'] is None:
+            raise usage.UsageError("Please specify a model class.")
+        if self['bucket-prefix'] is None:
+            raise usage.UsageError("Please specify a bucket prefix.")
+
+
+class ProgressEmitter(object):
+    """Report progress as a percentage to an emitter."""
+
+    def __init__(self, total, emit):
+        self.emit = emit
+        self.total = total
+        self.percentage = 0
+
+    def _calculate_percentage(self, value):
+        if value == 0:
+            return 0
+        return int(value * 100.0 / self.total)
+
+    def update(self, value):
+        old_percentage = self.percentage
+        self.percentage = self._calculate_percentage(value)
+        if self.percentage != old_percentage:
+            self.emit(self.percentage)
+
+
+class ModelMigrator(object):
+    def __init__(self, options):
+        self.options = options
+        model_cls = load_class_by_string(options['model'])
+        riak_config = {
+            'bucket_prefix': options['bucket-prefix'],
+        }
+        manager = self.get_riak_manager(riak_config)
+        self.model = manager.proxy(model_cls)
+
+    def get_riak_manager(self, riak_config):
+        return TxRiakManager.from_config(riak_config)
+
+    def emit(self, s):
+        print s
+
+    @inlineCallbacks
+    def migrate_key(self, key, dry_run):
+        try:
+            obj = yield self.model.load(key)
+            if obj is not None:
+                if not dry_run:
+                    yield obj.save()
+            else:
+                self.emit("Skipping tombstone key %r." % (key,))
+        except Exception, e:
+            self.emit("Failed to migrate key %r:" % (key,))
+            self.emit("  %s: %s" % (type(e).__name__, e))
+
+    @inlineCallbacks
+    def run(self):
+        dry_run = self.options["dry-run"]
+        if self.options["keys"] is not None:
+            keys = self.options["keys"].split(",")
+            self.emit("Migrating %d specified keys ..." % len(keys))
+        else:
+            keys = yield self.model.all_keys()
+            self.emit("%d keys found. Migrating ..." % len(keys))
+        # Depending on our Riak client, Python version, and JSON library we may
+        # get bytes or unicode here.
+        keys = [k.decode('utf-8') if isinstance(k, str) else k for k in keys]
+        progress = ProgressEmitter(
+            len(keys),
+            lambda p: self.emit("%s%% complete." % (p,))
+        )
+        for i, key in enumerate(keys):
+            yield self.migrate_key(key, dry_run)
+            progress.update(i)
+        self.emit("Done.")
+
+
+def main(name, *args):
+    try:
+        options = Options()
+        options.parseOptions(args)
+    except usage.UsageError, errortext:
+        print '%s: %s' % (name, errortext)
+        print '%s: Try --help for usage details.' % (name,)
+        sys.exit(1)
+
+    model_migrator = ModelMigrator(options)
+    return model_migrator.run()
+
+
+if __name__ == '__main__':
+    react(main, sys.argv)
---
+++ vumi/scripts/tests/test_async_model_migrator.py
@@ -0,0 +1,194 @@
+"""Tests for vumi.scripts.model_migrator."""
+
+import sys
+from StringIO import StringIO
+
+from twisted.internet.defer import inlineCallbacks, succeed
+from twisted.python import usage
+
+from vumi.persist.model import Model
+from vumi.persist.fields import Unicode
+from vumi.scripts.async_model_migrator import ModelMigrator, Options, main
+from vumi.tests.helpers import VumiTestCase, PersistenceHelper
+
+
+class SimpleModel(Model):
+    a = Unicode()
+
+
+class StubbedModelMigrator(ModelMigrator):
+    def __init__(self, testcase, *args, **kwargs):
+        self.testcase = testcase
+        self.output = []
+        super(StubbedModelMigrator, self).__init__(*args, **kwargs)
+
+    def emit(self, s):
+        self.output.append(s)
+
+    def get_riak_manager(self, riak_config):
+        return self.testcase.get_sub_riak(riak_config)
+
+
+class TestModelMigrator(VumiTestCase):
+
+    def setUp(self):
+        self.persistence_helper = self.add_helper(
+            PersistenceHelper(use_riak=True, is_sync=False))
+        self.riak_manager = self.persistence_helper.get_riak_manager()
+        self.model = self.riak_manager.proxy(SimpleModel)
+        self.model_cls_path = ".".join([
+            SimpleModel.__module__, SimpleModel.__name__])
+        self.expected_bucket_prefix = "bucket"
+        self.default_args = [
+            "-m", self.model_cls_path,
+            "-b", self.expected_bucket_prefix,
+        ]
+
+    def make_migrator(self, args=None):
+        if args is None:
+            args = self.default_args
+        options = Options()
+        options.parseOptions(args)
+        return StubbedModelMigrator(self, options)
+
+    def get_sub_riak(self, config):
+        self.assertEqual(config.get('bucket_prefix'),
+                         self.expected_bucket_prefix)
+        return self.riak_manager
+
+    @inlineCallbacks
+    def mk_simple_models(self, n):
+        for i in range(n):
+            obj = self.model(u"key-%d" % i, a=u"value-%d" % i)
+            yield obj.save()
+
+    def record_load_and_store(self):
+        loads, stores = [], []
+        orig_load = self.riak_manager.load
+        orig_store = self.riak_manager.store
+
+        def record_load(modelcls, key, result=None):
+            loads.append(key)
+            return orig_load(modelcls, key, result=result)
+
+        def record_store(obj):
+            stores.append(obj.key)
+            return orig_store(obj)
+
+        self.patch(self.riak_manager, 'load', record_load)
+        self.patch(self.riak_manager, 'store', record_store)
+        return loads, stores
+
+    def test_model_class_required(self):
+        self.assertRaises(usage.UsageError, self.make_migrator, [
+            "-b", self.expected_bucket_prefix,
+        ])
+
+    def test_bucket_required(self):
+        self.assertRaises(usage.UsageError, self.make_migrator, [
+            "-m", self.model_cls_path,
+        ])
+
+    @inlineCallbacks
+    def test_main(self):
+        yield self.mk_simple_models(3)
+        self.patch(sys, "stdout", StringIO())
+        yield main(
+            "name",
+            "-m", self.model_cls_path,
+            "-b", self.riak_manager.bucket_prefix)
+        self.assertEqual(sys.stdout.getvalue(), "\n".join([
+            "3 keys found. Migrating ...",
+            "33% complete.", "66% complete.",
+            "Done.", "",
+        ]))
+
+    @inlineCallbacks
+    def test_successful_migration(self):
+        yield self.mk_simple_models(3)
+        loads, stores = self.record_load_and_store()
+        model_migrator = self.make_migrator()
+        yield model_migrator.run()
+        self.assertEqual(model_migrator.output, [
+            "3 keys found. Migrating ...",
+            "33% complete.", "66% complete.",
+            "Done.",
+        ])
+        self.assertEqual(sorted(loads), [u"key-%d" % i for i in range(3)])
+        self.assertEqual(sorted(stores), [u"key-%d" % i for i in range(3)])
+
+    @inlineCallbacks
+    def test_migration_with_tombstones(self):
+        yield self.mk_simple_models(3)
+
+        def tombstone_load(modelcls, key, result=None):
+            return succeed(None)
+
+        self.patch(self.riak_manager, 'load', tombstone_load)
+
+        model_migrator = self.make_migrator()
+        yield model_migrator.run()
+        for i in range(3):
+            self.assertTrue(("Skipping tombstone key u'key-%d'." % i)
+                            in model_migrator.output)
+        self.assertEqual(model_migrator.output[:1], [
+            "3 keys found. Migrating ...",
+        ])
+        self.assertEqual(model_migrator.output[-2:], [
+            "66% complete.",
+            "Done.",
+        ])
+
+    @inlineCallbacks
+    def test_migration_with_failures(self):
+        yield self.mk_simple_models(3)
+
+        def error_load(modelcls, key, result=None):
+            raise ValueError("Failed to load.")
+
+        self.patch(self.riak_manager, 'load', error_load)
+
+        model_migrator = self.make_migrator()
+        yield model_migrator.run()
+        line_pairs = zip(model_migrator.output, model_migrator.output[1:])
+        for i in range(3):
+            self.assertTrue((
+                "Failed to migrate key u'key-0':",
+                "  ValueError: Failed to load.",
+            ) in line_pairs)
+        self.assertEqual(model_migrator.output[:1], [
+            "3 keys found. Migrating ...",
+        ])
+        self.assertEqual(model_migrator.output[-2:], [
+            "66% complete.",
+            "Done.",
+        ])
+
+    @inlineCallbacks
+    def test_migrating_specific_keys(self):
+        yield self.mk_simple_models(3)
+        loads, stores = self.record_load_and_store()
+        model_migrator = self.make_migrator(
+            self.default_args + ["--keys", "key-1,key-2"])
+        yield model_migrator.run()
+        self.assertEqual(model_migrator.output, [
+            "Migrating 2 specified keys ...",
+            "50% complete.",
+            "Done.",
+        ])
+        self.assertEqual(sorted(loads), [u"key-1", u"key-2"])
+        self.assertEqual(sorted(stores), [u"key-1", u"key-2"])
+
+    @inlineCallbacks
+    def test_dry_run(self):
+        yield self.mk_simple_models(3)
+        loads, stores = self.record_load_and_store()
+        model_migrator = self.make_migrator(self.default_args + ["--dry-run"])
+        yield model_migrator.run()
+        self.assertEqual(model_migrator.output, [
+            "3 keys found. Migrating ...",
+            "33% complete.", "66% complete.",
+            "Done.",
+        ])
+        self.assertEqual(sorted(loads), [u"key-%d" % i for i in range(3)])
+        self.assertEqual(sorted(stores), [])
