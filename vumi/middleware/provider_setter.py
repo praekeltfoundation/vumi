ec75177e10f20e446d414c9d3e755c20360a2b2b
Jeremy Thurgood <jeremy@praekelt.com>
2014-11-10 19:07:45 +0200

    Simple address prefix-matching provider setting middleware

--- vumi/middleware/provider_setter.py
+++ vumi/middleware/provider_setter.py
@@ -21,3 +21,36 @@ class StaticProviderSettingMiddleware(TransportMiddleware):
 
     def handle_outbound(self, message, connector_name):
         return message
+
+
+class AddressPrefixProviderSettingMiddleware(TransportMiddleware):
+    """
+    Transport middleware that sets a `provider` on each inbound message based
+    on configured ``from_addr`` prefixes. Outbound messages are ignored.
+
+    Configuration options:
+
+    :param dict provider_prefixes:
+        Mapping from address prefix to provider value. Longer prefixes are
+        checked first to avoid ambiguity. If no prefix matches, the provider
+        value will be set to ``None``.
+    """
+    def setup_middleware(self):
+        self.provider_prefixes = []
+        prefix_mapping = self.config['provider_prefixes']
+        sorted_prefixes = sorted(prefix_mapping.keys(), key=lambda p: -len(p))
+        for prefix in sorted_prefixes:
+            self.provider_prefixes.append((prefix, prefix_mapping[prefix]))
+
+    def get_prefix(self, from_addr):
+        for prefix, provider in self.provider_prefixes:
+            if from_addr.startswith(prefix):
+                return provider
+        return None
+
+    def handle_inbound(self, message, connector_name):
+        message["provider"] = self.get_prefix(message["from_addr"])
+        return message
+
+    def handle_outbound(self, message, connector_name):
+        return message
--- vumi/middleware/tests/test_provider_setter.py
+++ vumi/middleware/tests/test_provider_setter.py
@@ -1,6 +1,7 @@
 """Tests for vumi.middleware.provider_setter."""
 
-from vumi.middleware.provider_setter import StaticProviderSettingMiddleware
+from vumi.middleware.provider_setter import (
+    StaticProviderSettingMiddleware, AddressPrefixProviderSettingMiddleware)
 from vumi.tests.helpers import VumiTestCase, MessageHelper
 
 
@@ -43,6 +44,74 @@ class TestStaticProviderSettingMiddleware(VumiTestCase):
         """
         mw = self.mk_middleware({"provider": "MY-MNO"})
         msg = self.msg_helper.make_outbound(None)
+        self.assertEqual(msg.get("provider"), None)
+        processed_msg = mw.handle_outbound(msg, "dummy_connector")
+        self.assertEqual(processed_msg.get("provider"), None)
+
+
+class TestAddressPrefixProviderSettingMiddleware(VumiTestCase):
+    def setUp(self):
+        self.msg_helper = self.add_helper(MessageHelper())
+
+    def mk_middleware(self, config):
+        dummy_worker = object()
+        mw = AddressPrefixProviderSettingMiddleware(
+            "address_prefix_provider_setter", config, dummy_worker)
+        mw.setup_middleware()
+        return mw
+
+    def test_set_provider_unique_matching_prefix(self):
+        """
+        If exactly one prefix matches the address, its corresponding provider
+        value is set on the inbound message.
+        """
+        mw = self.mk_middleware({"provider_prefixes": {
+            "+123": "MY-MNO",
+            "+124": "YOUR-MNO",
+        }})
+        msg = self.msg_helper.make_inbound(None, from_addr="+12345")
+        self.assertEqual(msg.get("provider"), None)
+        processed_msg = mw.handle_inbound(msg, "dummy_connector")
+        self.assertEqual(processed_msg.get("provider"), "MY-MNO")
+
+    def test_set_provider_longest_matching_prefix(self):
+        """
+        If more than one prefix matches the address, the provider value for
+        the longest matching prefix is set on the inbound message.
+        """
+        mw = self.mk_middleware({"provider_prefixes": {
+            "+12": "YOUR-MNO",
+            "+123": "YOUR-MNO",
+            "+1234": "YOUR-MNO",
+            "+12345": "MY-MNO",
+            "+123456": "YOUR-MNO",
+        }})
+        msg = self.msg_helper.make_inbound(None, from_addr="+12345")
+        self.assertEqual(msg.get("provider"), None)
+        processed_msg = mw.handle_inbound(msg, "dummy_connector")
+        self.assertEqual(processed_msg.get("provider"), "MY-MNO")
+
+    def test_no_provider_for_no_matching_prefix(self):
+        """
+        If no prefix matches the address, the provider value will be set to
+        ``None`` on the inbound message.
+        """
+        mw = self.mk_middleware({"provider_prefixes": {
+            "+124": "YOUR-MNO",
+            "+125": "YOUR-MNO",
+        }})
+        msg = self.msg_helper.make_inbound(None, from_addr="+12345")
+        self.assertEqual(msg.get("provider"), None)
+        processed_msg = mw.handle_inbound(msg, "dummy_connector")
+        self.assertEqual(processed_msg.get("provider"), None)
+
+    def test_do_not_set_provider_on_outbound(self):
+        """
+        Outbound messages are left as they are.
+        """
+        mw = self.mk_middleware({"provider_prefixes": {"+123": "MY-MNO"}})
+        msg = self.msg_helper.make_outbound(
+            None, to_addr="+1234567", from_addr="+12345")
         self.assertEqual(msg.get("provider"), None)
         processed_msg = mw.handle_outbound(msg, "dummy_connector")
         self.assertEqual(processed_msg.get("provider"), None)
