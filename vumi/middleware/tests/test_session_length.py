9beb6324bb2dfc7e172769b9bd641faf2ac21713
Rudi Giesler <rudi@praekelt.com>
2015-02-12 16:04:30 +0200

    Added first test

--- vumi/middleware/tests/test_session_length.py
+++ vumi/middleware/tests/test_session_length.py
@@ -1,16 +1,50 @@
 """Tests for vumi.middleware.session_length."""
 
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+from vumi.message import TransportUserMessage
 from vumi.middleware.session_length import SessionLengthMiddleware
-from vumi.tests.helpers import VumiTestCase, MessageHelper
+from vumi.tests.helpers import VumiTestCase, PersistenceHelper
+
+SESSION_NEW, SESSION_CLOSE = (
+    TransportUserMessage.SESSION_NEW, TransportUserMessage.SESSION_CLOSE)
 
 
 class TestStaticProviderSettingMiddleware(VumiTestCase):
     def setUp(self):
-        self.msg_helper = self.add_helper(MessageHelper())
+        self.persistence_helper = self.add_helper(PersistenceHelper())
 
-    def mk_middleware(self, config):
+    @inlineCallbacks
+    def mk_middleware(self, config={}):
         dummy_worker = object()
+        config = self.persistence_helper.mk_config(config)
         mw = SessionLengthMiddleware(
             "static_provider_setter", config, dummy_worker)
-        mw.setup_middleware()
-        return mw
+        yield mw.setup_middleware()
+        self.redis = mw.redis
+        self.add_cleanup(mw.teardown_middleware)
+        returnValue(mw)
+
+    def mk_msg(self, to_addr, from_addr, session_event=SESSION_NEW):
+        msg = TransportUserMessage(
+            to_addr=to_addr, from_addr=from_addr,
+            transport_name="dummy_connector",
+            transport_type="dummy_transport_type",
+            event_type=session_event)
+        return msg
+
+    @inlineCallbacks
+    def test_incoming_message_session_length(self):
+        mw = yield self.mk_middleware()
+        msg_start = self.mk_msg('+12345', '+54231')
+        msg_end = self.mk_msg('+12345', '+54231', session_event=SESSION_CLOSE)
+
+        yield mw.handle_inbound(msg_start, "dummy_connector")
+        keys = yield self.redis.keys()
+        print keys
+        value = yield self.redis.get('+54321:session_created')
+        print value
+        self.assertTrue(value is not None)
+        msg = yield mw.handle_inbound(msg_end, "dummy_connector")
+        self.assertTrue(isinstance(
+                msg['helper_metadata']['billing']['session_length'], float))
