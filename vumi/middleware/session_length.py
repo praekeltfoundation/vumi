37c65644fcbb21b4969a9c4b5fff51486c935a11
Rudi Giesler <rudi@praekelt.com>
2015-02-12 17:03:51 +0200

    handle inbound and outbound messages.

--- vumi/middleware/session_length.py
+++ vumi/middleware/session_length.py
@@ -31,18 +31,42 @@ class SessionLengthMiddleware(BaseMiddleware):
     def teardown_middleware(self):
         yield self.redis.close_manager()
 
+    def _set_message_billing_metadata(self, message, field, value):
+        if not message['helper_metadata'].get('billing'):
+            message['helper_metadata']['billing'] = {}
+        message['helper_metadata']['billing'][field] = value
+
+    def _set_session_start_time(self, message, time):
+        self._set_message_billing_metadata(message, 'session_start', time)
+
+    def _set_session_end_time(self, message, time):
+        self._set_message_billing_metadata(message, 'session_end', time)
+
+    @inlineCallbacks
+    def _process_message(self, message, redis_key):
+        if message.get('event_type') == self.SESSION_NEW:
+            start_time = time.time()
+            yield self.redis.set(redis_key, str(start_time))
+            self._set_session_start_time(message, start_time)
+        elif message.get('event_type') == self.SESSION_CLOSE:
+            self._set_session_end_time(message, time.time())
+            created_time = yield self.redis.get(redis_key)
+            if created_time:
+                self._set_session_start_time(message, float(created_time))
+                yield self.redis.delete(redis_key)
+        else:
+            created_time = yield self.redis.get(redis_key)
+            if created_time:
+                self._set_session_start_time(message, float(created_time))
+
     @inlineCallbacks
     def handle_inbound(self, message, connector_name):
         redis_key = '%s:%s' % (message.get('from_addr'), 'session_created')
-        if message.get('event_type') == self.SESSION_NEW:
-            yield self.redis.set(redis_key, str(time.time()))
-        elif message.get('event_type') == self.SESSION_CLOSE:
-            created_time = yield self.redis.get(redis_key)
-            if created_time:
-                created_time = float(created_time)
-                if not message['helper_metadata'].get('billing'):
-                    message['helper_metadata']['billing'] = {}
-                message['helper_metadata']['billing']['session_length'] = (
-                    time.time() - created_time)
-                yield self.redis.delete(redis_key)
+        yield self._process_message(message, redis_key)
+        returnValue(message)
+
+    @inlineCallbacks
+    def handle_outbound(self, message, connector_name):
+        redis_key = '%s:%s' % (message.get('to_addr'), 'session_created')
+        yield self._process_message(message, redis_key)
         returnValue(message)
