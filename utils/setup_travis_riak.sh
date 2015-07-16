5b6c89b2eed71b22196ca8c7032754f1daf57cff
Jeremy Thurgood <jeremy@praekelt.com>
2015-07-16 14:11:42 +0200

    Use Riak built by praekelt/riak-for-travis github project.

--- utils/setup_travis_riak.sh
+++ utils/setup_travis_riak.sh
@@ -1,34 +1,19 @@
 #!/bin/bash -e
 
+# We use Riak tarballs built by https://github.com/praekelt/riak-for-travis
+
 RIAK_VERSION="${1?Please provide Riak version.}"
-RIAK_FILENAME=riak-${RIAK_VERSION}-precise.tgz
+RIAK_FILENAME=riak-${RIAK_VERSION}.tar.bz2
 RIAK_DOWNLOAD=$HOME/download/$RIAK_FILENAME
+BASE_URL="https://github.com/praekelt/riak-for-travis/releases/download/riak-for-travis-0.1.0/"
 
 echo "Setting up Riak with RIAK_VERSION='${RIAK_VERSION}'"
 
 if [ ! -f $RIAK_DOWNLOAD ]; then
     mkdir -p $HOME/download
-    wget -O $RIAK_DOWNLOAD http://jerith.za.net/files/riak/$RIAK_FILENAME
+    wget -O $RIAK_DOWNLOAD $BASE_URL/$RIAK_FILENAME
 fi
 
 tar xzf $RIAK_DOWNLOAD -C $HOME/
-
-
-case "${RIAK_VERSION}" in
-    2.1.*)
-        echo "Using Riak ${RIAK_VERSION} with v2.1 config..."
-        cp utils/riak.conf $HOME/riak/etc/
-        cp utils/advanced.config $HOME/riak/etc/
-        ;;
-    1.4.*)
-        echo "Using Riak ${RIAK_VERSION} with v1.4 config..."
-        cp utils/app.config $HOME/riak/etc/
-        ;;
-    *)
-        # Unexpected version.
-        echo "I don't know how to set up Riak ${RIAK_VERSION}, sorry."
-        exit 1
-        ;;
-esac
 
 $HOME/riak/bin/riak start
