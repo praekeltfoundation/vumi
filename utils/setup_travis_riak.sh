fb6afaf14055b84a738bdfab48c915a7ef4bf07a
Jeremy Thurgood <jeremy@praekelt.com>
2015-07-16 11:44:39 +0200

    Fix Riak paths.

--- utils/setup_travis_riak.sh
+++ utils/setup_travis_riak.sh
@@ -11,17 +11,17 @@ if [ ! -f $RIAK_DOWNLOAD ]; then
     wget -O $RIAK_DOWNLOAD http://jerith.za.net/files/riak/$RIAK_FILENAME
 fi
 
-tar xzf $RIAK_DOWNLOAD
+tar xzf $RIAK_DOWNLOAD -C $HOME/
 
 
 case "${RIAK_VERSION}" in
     2.1.*)
         echo "Using Riak ${RIAK_VERSION} with v2.1 config..."
-        cp utils/advanced.config riak/etc/
+        cp utils/advanced.config $HOME/riak/etc/
         ;;
     1.4.*)
         echo "Using Riak ${RIAK_VERSION} with v1.4 config..."
-        cp utils/app.config riak/etc/
+        cp utils/app.config $HOME/riak/etc/
         ;;
     *)
         # Unexpected version.
@@ -30,4 +30,4 @@ case "${RIAK_VERSION}" in
         ;;
 esac
 
-riak/bin/riak start
+$HOME/riak/bin/riak start
