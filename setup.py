b1b7176537f765e7cec676fc3d038f67d02e6d73
Simon Cross <hodgestar@gmail.com>
2015-08-20 20:41:50 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vumi",
-    version="0.5.22",
+    version="0.5.23a",
     url='http://github.com/praekelt/vumi',
     license='BSD',
     description="Super-scalable messaging engine for the delivery of SMS, "
