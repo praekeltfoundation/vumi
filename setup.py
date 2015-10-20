dbc7e688c8276c0e1fc9b284adc2d3d3f8ef186d
justinvdm <justinvdm.0.1.0@gmail.com>
2015-10-20 15:19:45 +0200

    Post-rlease version bump to 0.5.33a

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if platform.python_implementation() == "PyPy":
 
 setup(
     name="vumi",
-    version="0.5.32a",
+    version="0.5.33a",
     url='http://github.com/praekelt/vumi',
     license='BSD',
     description="Super-scalable messaging engine for the delivery of SMS, "
