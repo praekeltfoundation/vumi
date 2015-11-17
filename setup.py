689b9db0ced3e7ea4f4b085b14c4f56cff03b658
Rudi Giesler <rudi@praekelt.com>
2015-11-17 14:21:43 +0200

    Post release version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if platform.python_implementation() == "PyPy":
 
 setup(
     name="vumi",
-    version="0.5.33",
+    version="0.5.34a",
     url='http://github.com/praekelt/vumi',
     license='BSD',
     description="Super-scalable messaging engine for the delivery of SMS, "
