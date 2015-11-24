e8aa5c54e2a719b303d32beefd1bd7fa9596733b
Simon Cross <hodgestar@gmail.com>
2015-11-24 13:47:32 +0200

    Relese 0.5.34

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if platform.python_implementation() == "PyPy":
 
 setup(
     name="vumi",
-    version="0.5.34a",
+    version="0.5.34",
     url='http://github.com/praekelt/vumi',
     license='BSD',
     description="Super-scalable messaging engine for the delivery of SMS, "
