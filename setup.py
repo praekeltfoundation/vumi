aaa44a1c7bf78a6505ce2e757a331565d5a377f0
Simon Cross <hodgestar@gmail.com>
2016-03-03 14:59:05 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if platform.python_implementation() == "PyPy":
 
 setup(
     name="vumi",
-    version="0.6.2",
+    version="0.6.3a",
     url='http://github.com/praekelt/vumi',
     license='BSD',
     description="Super-scalable messaging engine for the delivery of SMS, "
