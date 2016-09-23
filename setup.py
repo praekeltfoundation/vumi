7d1aaeabad1b2b0e068944a78ab4356f2777c631
Simon de Haan <simon@praekeltfoundation.org>
2016-09-23 11:18:37 +0200

    version bump 0.6.12

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if platform.python_implementation() == "PyPy":
 
 setup(
     name="vumi",
-    version="0.6.11",
+    version="0.6.12",
     url='http://github.com/praekelt/vumi',
     license='BSD',
     description="Super-scalable messaging engine for the delivery of SMS, "
