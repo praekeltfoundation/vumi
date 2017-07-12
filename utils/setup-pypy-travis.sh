7c32fed9f059b9e07df4bcfc3d1e74c94f1bd98c
Simon de Haan <simon@praekelt.org>
2017-07-12 11:18:21 +0200

    drop to pypy 5.6.0

--- utils/setup-pypy-travis.sh
+++ utils/setup-pypy-travis.sh
@@ -9,13 +9,6 @@ export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
-# pyenv, depending on the versions selected, suggests having these installed
-# to side step common build problems
-# See: https://github.com/pyenv/pyenv/wiki/Common-build-problems
-apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev
-
 # Install pypy and make a virtualenv for it.
 pyenv install --list
 pyenv install -s pypy-$PYPY_VERSION
