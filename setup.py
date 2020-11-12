8c26a3a1970a839971afeb42f43faed44af7ec4a
Jeremy Thurgood <jeremy@praekelt.org>
2020-11-12 14:42:33 +0200

    Increase minimum Twisted version to match other deps

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     install_requires=[
         cryptography,  # See above for pypy-version-dependent requirement.
         'zope.interface',
-        'Twisted>=13.2.0',
+        'Twisted>=16.4.0',  # Minimum version supported by recent treq.
         'txAMQP>=0.6.2',
         'PyYAML',
         'iso8601',
